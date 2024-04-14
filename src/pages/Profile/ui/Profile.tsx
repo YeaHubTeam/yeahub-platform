266fa5f12070958c98470e6f1ea06081c4a06b10
Hatamov Samir <samirhatamov45@gmail.com>
2024-04-14 23:52:09 +0400

    fix (YH-77) simplified the block component

--- src/pages/Profile/ui/Profile.tsx
+++ src/pages/Profile/ui/Profile.tsx
@@ -1,7 +1,5 @@
 import { FC } from 'react';
 
-import Photo from '@/shared/assets/icons/Photo.png';
-
 import { Block } from '@/widgets/Block';
 
 import styles from './Profile.module.css';
@@ -10,17 +8,7 @@ export const Profile: FC = () => {
 	return (
 		<>
 			<h2 className={styles.title}>Profile</h2>
-			<Block
-				title="Анастасия Заворотнюк"
-				type="profile"
-				avatar={<img src={Photo} alt="avatar" />}
-				actionSlot={<div>Редактировать</div>}
-			>
-				<div>
-					<span>UX/UI Дизайнер в Яндекс</span>
-				</div>
-			</Block>
-			<Block title="Обо мне" expandable actionSlot={<div>Редактировать</div>}>
+			<Block expandable>
 				I’m a product designer + filmmaker currently working remotely at Twitter from beautiful
 				Manchester, United Kingdom. I’m passionate about designing digital products that have a
 				positive impact on the world. For 10 years, I’ve specialised in interface, experience &
@@ -30,15 +18,10 @@ export const Profile: FC = () => {
 				positive impact on the world. For 10 years, I’ve specialised in interface, experience &
 				interaction design as well as working in user research and product strategy for product I’m
 				a product designer + filmmaker currently working remotely at Twitter from beautiful
-				Manchester, United Kingdom. I’m passionate about designing digital products that have a
-				positive impact on the world. For 10 years, I’ve specialised in interface, experience &
 				interaction design as well as working in user research and product strategy for product I’m
 				a product designer + filmmaker currently working remotely at Twitter from beautiful
-				Manchester, United Kingdom. I’m passionate about designing digital products that have a
-				positive impact on the world. For 10 years, I’ve specialised in interface, experience &
-				interaction design as well as working in user research and product strategy for product a
-				product designer + filmmaker currently working remotely at Twitter from beautiful
-				Manchester, United Kingdom. I’m passionate about designing digital products that have a
+				interaction design as well as working in user research and product strategy for product I’m
+				a product designer + filmmaker currently working remotely at Twitter from beautiful
 			</Block>
 		</>
 	);
--- src/widgets/Block/ui/Block.tsx
+++ src/widgets/Block/ui/Block.tsx
@@ -5,67 +5,54 @@ import Arrow from '@/shared/assets/icons/arrow.svg';
 import styles from './Block.module.css';
 
 interface Props {
-	title?: string | ReactNode;
-	actionSlot?: ReactNode;
-	type?: 'info' | 'profile';
-	avatar?: ReactNode;
 	children?: ReactNode;
 	expandable?: boolean;
 }
 
 /**
  * Reusable block component
- * @param { string | ReactNode } title main block title
  * @param { ReactNode } actionSlot actionSlot may include buttons (Editing, pagination, etc.)
- * @param { 'info' | 'profile' } type the block can be of two types 'profilie' (Specialized for the profile block)
- * and 'info' for other types of block
- * @param { ReactNode } avatar if the block type is profile, then you can insert the user’s avatar into this prop
  * @param { string | ReactNode } children main block content
  * @param { boolean } expandable if the flag is true then the block is expandable (an expand button appears)
  */
 
-export const Block: FC<Props> = ({
-	title = '',
-	actionSlot,
-	avatar,
-	children,
-	type = 'info',
-	expandable = false,
-}) => {
+export const Block: FC<Props> = ({ children, expandable = false }) => {
 	const blockRef = useRef<HTMLDivElement>(null);
 	const [blockHeight, setBlockHeight] = useState(0);
 	const [isExpand, setIsExpand] = useState(true);
 
 	useLayoutEffect(() => {
-		setBlockHeight((blockRef.current?.getClientRects()[0].height as number)!);
+		if (expandable) {
+			const blockHeightHandler = () => {
+				const height = (blockRef.current?.getClientRects()[0].height as number)!;
+				setBlockHeight(height);
+				if (height < 250) {
+					setIsExpand(true);
+				}
+			};
+			blockHeightHandler();
+
+			window.addEventListener('resize', blockHeightHandler);
+			return () => {
+				window.removeEventListener('resize', blockHeightHandler);
+			};
+		}
 	}, []);
 
 	const expandHandler = () => {
 		setIsExpand((prev) => !prev);
 	};
 
-	const blockContent = () => (
-		<>
-			<div className={styles['block-header']}>
-				{typeof title === 'string' ? <span>{title}</span> : title}
-				{actionSlot}
-			</div>
-			{children}
-		</>
-	);
+	const isHeightForExpand = blockHeight >= 250;
 
-	const isProfile = type === 'profile';
-	const isHeightForExpand = blockHeight > 250;
 	return (
 		<div
 			ref={blockRef}
-			className={`${styles.block} ${isProfile && styles['block-profile']} 
+			className={`${styles.block}
 			${isHeightForExpand && expandable && styles['block-expandable']} 
 			${!isExpand && styles['block-expanded']}`}
 		>
-			{isProfile && avatar}
-			{isProfile ? <div className={styles['block-wrapper']}>{blockContent()}</div> : blockContent()}
-
+			{children}
 			{expandable && isHeightForExpand && (
 				<div className={styles['block-expand']}>
 					<button onClick={expandHandler}>
