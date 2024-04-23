0d4d29a4669fbc3bf1c2742c4b2ddf2c87c39d86
Hatamov Samir <samirhatamov45@gmail.com>
2024-04-23 12:04:30 +0400

    fix (YH-77) moved the svg into a separate component

--- src/widgets/Block/ui/Block.tsx
+++ src/widgets/Block/ui/Block.tsx
@@ -15,6 +15,38 @@ interface Props {
  * @param { boolean } expandable if the flag is true then the block is expandable (an expand button appears)
  */
 
+const Expand = () => {
+	return (
+		<svg
+			className={`${styles['block-expand-svg']}`}
+			width="100%"
+			height="90"
+			viewBox="0 0 740 90"
+			preserveAspectRatio="xMidYMid slice"
+			fill="none"
+			xmlns="http://www.w3.org/2000/svg"
+		>
+			<path
+				d="M0 0H740V66C740 79.2548 729.255 90 716 90H24C10.7452 90 0 79.2548 0 66V0Z"
+				fill="url(#paint0_linear_1262_19118)"
+			/>
+			<defs>
+				<linearGradient
+					id="paint0_linear_1262_19118"
+					x1="370"
+					y1="0"
+					x2="370"
+					y2="90"
+					gradientUnits="userSpaceOnUse"
+				>
+					<stop stopColor="white" stopOpacity="0" />
+					<stop offset="0.491518" stopColor="white" />
+				</linearGradient>
+			</defs>
+		</svg>
+	);
+};
+
 export const Block: FC<Props> = ({ children, expandable = false }) => {
 	const contentRef = useRef<HTMLDivElement>(null);
 	const [isExpand, setIsExpand] = useState(false);
@@ -57,33 +89,7 @@ export const Block: FC<Props> = ({ children, expandable = false }) => {
 			<div ref={contentRef}>{children}</div>
 			{expandable && isHeightForExpand && (
 				<>
-					<svg
-						className={`${styles['block-expand-svg']}`}
-						width="100%"
-						height="90"
-						viewBox="0 0 740 90"
-						preserveAspectRatio="xMidYMid slice"
-						fill="none"
-						xmlns="http://www.w3.org/2000/svg"
-					>
-						<path
-							d="M0 0H740V66C740 79.2548 729.255 90 716 90H24C10.7452 90 0 79.2548 0 66V0Z"
-							fill="url(#paint0_linear_1262_19118)"
-						/>
-						<defs>
-							<linearGradient
-								id="paint0_linear_1262_19118"
-								x1="370"
-								y1="0"
-								x2="370"
-								y2="90"
-								gradientUnits="userSpaceOnUse"
-							>
-								<stop stopColor="white" stopOpacity="0" />
-								<stop offset="0.491518" stopColor="white" />
-							</linearGradient>
-						</defs>
-					</svg>
+					<Expand />
 					<button onClick={expandHandler} className={`${styles.button}`}>
 						{!isExpand ? 'Развернуть' : 'Свернуть'}
 						<Arrow className={`${isExpand ? styles['block-arrow-expanded'] : ''}`} />
