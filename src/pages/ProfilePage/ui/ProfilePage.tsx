11ca97dc76be8ed7f95aa8b0458d29aeb292137e
Alexander <81985267+alexandkazakov@users.noreply.github.com>
2024-05-04 10:22:18 +0300

    feature/YH-100 (#6)
    * add open/close sidebar, connect rtk query
    
    * (YH-76) add useAppSelector, new sidebar toggle button
    
    * (YH-76) add svg chevron, fix code
    
    * change chevron direction
    
    * change search class
    
    * feat (YH-100) Added scroll on main component in pages
    
    * feat (YH-100) Added scroll on main section and fix bugs from styles (transitions and colors)
    
    ---------
    
    Co-authored-by: AleksanderIazev <mr.robot88@inbox.ru>

--- src/pages/ProfilePage/ui/ProfilePage.tsx
+++ src/pages/ProfilePage/ui/ProfilePage.tsx
@@ -5,30 +5,41 @@ import { Block } from '@/widgets/Block';
 import styles from './ProfilePage.module.css';
 
 const ProfilePage: FC = () => {
+	const blockContent = `I’m a product designer + filmmaker currently working remotely at Twitter from beautiful
+	Manchester, United Kingdom. I’m passionate about designing digital products that have a
+	positive impact on the world. For 10 years, I’ve specialised in interface, experience &
+	interaction design as well as working in user research and product strategy for product I’m
+	a product designer + filmmaker currently working remotely at Twitter from beautiful
+	Manchester, United Kingdom. I’m passionate about designing digital products that have a
+	positive impact on the world. For 10 years, I’ve specialised in interface, experience &
+	interaction design as well as working in user research and product strategy for product I’m
+	a product designer + filmmaker currently working remotely at Twitter from beautiful
+	interaction design as well as working in user research and product strategy for product I’m
+	a product designer + filmmaker currently working remotely at Twitter from beautiful
+	interaction design as well as working in user research and product strategy for product I’m
+	a product designer + filmmaker currently working remotely at Twitter from beautiful
+	interaction design as well as working in user research and product strategy for product I’m
+	a product designer + filmmaker currently working remotely at Twitter from beautiful
+	interaction design as well as working in user research and product strategy for product I’m
+	a product designer + filmmaker currently working remotely at Twitter from beautiful
+	interaction design as well as working in user research and product strategy for product I’m
+	a product designer + filmmaker currently working remotely at Twitter from beautiful`;
+
 	return (
 		<>
 			<h2 className={styles.title}>Profile</h2>
-			<Block expandable>
-				I’m a product designer + filmmaker currently working remotely at Twitter from beautiful
-				Manchester, United Kingdom. I’m passionate about designing digital products that have a
-				positive impact on the world. For 10 years, I’ve specialised in interface, experience &
-				interaction design as well as working in user research and product strategy for product I’m
-				a product designer + filmmaker currently working remotely at Twitter from beautiful
-				Manchester, United Kingdom. I’m passionate about designing digital products that have a
-				positive impact on the world. For 10 years, I’ve specialised in interface, experience &
-				interaction design as well as working in user research and product strategy for product I’m
-				a product designer + filmmaker currently working remotely at Twitter from beautiful
-				interaction design as well as working in user research and product strategy for product I’m
-				a product designer + filmmaker currently working remotely at Twitter from beautiful
-				interaction design as well as working in user research and product strategy for product I’m
-				a product designer + filmmaker currently working remotely at Twitter from beautiful
-				interaction design as well as working in user research and product strategy for product I’m
-				a product designer + filmmaker currently working remotely at Twitter from beautiful
-				interaction design as well as working in user research and product strategy for product I’m
-				a product designer + filmmaker currently working remotely at Twitter from beautiful
-				interaction design as well as working in user research and product strategy for product I’m
-				a product designer + filmmaker currently working remotely at Twitter from beautiful
-			</Block>
+			<div className={styles.content}>
+				<div className={styles.left}>
+					<Block expandable>{blockContent}</Block>
+					<Block expandable>{blockContent}</Block>
+					<Block expandable>{blockContent}</Block>
+					<Block expandable>{blockContent}</Block>
+				</div>
+				<div className={styles.right}>
+					<Block expandable>{blockContent}</Block>
+					<Block expandable>{blockContent}</Block>
+				</div>
+			</div>
 		</>
 	);
 };
