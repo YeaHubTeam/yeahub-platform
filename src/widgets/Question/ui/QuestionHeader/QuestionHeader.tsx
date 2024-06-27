bffc940e77e593d6c27eb03f63d98d19131cb092
Vladimir <62429354+Buffik@users.noreply.github.com>
2024-06-27 10:43:06 +0300

    Feature/yh 191 (#18)
    * YH-191: add api for question, add question header
    
    * YH-191: fix props in QuestionHeader
    
    * YH-191: add question body layout
    
    * YH-191: add quetion actions
    
    * YH-191: add progress block
    
    * YH-191: add additional info block
    
    * YH-191: fix from comments
    
    * YH-191: revert env
    
    * YH-191: revert logic in header button login
    
    * YH-191: fix from comments

---
+++ src/widgets/Question/ui/QuestionHeader/QuestionHeader.tsx
@@ -0,0 +1,31 @@
+import QuestionImg from '@/shared/assets/images/question_img.png';
+import { Block } from '@/shared/ui/Block';
+
+import styles from './QuestionHeader.module.css';
+
+interface Props {
+	title?: string;
+	description?: string;
+	status?: string;
+}
+
+//todo доработать проброс картинки вопроса.
+
+export const QuestionHeader = ({ title, description, status }: Props) => {
+	return (
+		<Block className={styles.wrapper}>
+			<div className={styles['question-header-wrapper']}>
+				<div className={styles['image-wrapper']}>
+					<img src={QuestionImg} alt="Question" />
+				</div>
+				<div className={styles['title-wrapper']}>
+					<h2 className={styles.title}>{title}</h2>
+					<p className={styles.description}>{description}</p>
+				</div>
+				<div className={styles['label-wrapper']}>
+					<p className={styles.label}>{status}</p>
+				</div>
+			</div>
+		</Block>
+	);
+};
