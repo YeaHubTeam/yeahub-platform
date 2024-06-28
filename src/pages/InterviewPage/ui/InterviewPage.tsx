3aa7ed181f60a7a9a9bbb8b969966de9ac42398c
Дарья <106686333+sam-dash@users.noreply.github.com>
2024-06-28 21:48:43 +0300

    feat (YH-192) add InterviewPreparationBlock (#19)

--- src/pages/InterviewPage/ui/InterviewPage.tsx
+++ src/pages/InterviewPage/ui/InterviewPage.tsx
@@ -2,7 +2,11 @@ import { FC } from 'react';
 
 import { Block } from '@/shared/ui/Block';
 
+import { QuestionProgressBar } from '@/entities/interview';
+
 import { InterviewHistoryHeader, InterviewHistoryList } from '@/widgets/InterviewHistory';
+import { InterviewPreparationHeader } from '@/widgets/InterviewPreparation';
+import { QuestionSlider } from '@/widgets/InterviewPreparation';
 import { InterviewQuestionHeader, InterviewQuestionsList } from '@/widgets/InterviewQuestions';
 
 import styles from './InterviewPage.module.css';
@@ -10,7 +14,18 @@ import styles from './InterviewPage.module.css';
 const InterviewPage: FC = () => {
 	return (
 		<div className={styles.container}>
-			<Block></Block>
+			<Block>
+				<div className={styles.questions}>
+					<InterviewPreparationHeader
+						title="Подготовка к собеседованиям"
+						linkTitle="Пройти собеседование"
+					/>
+					<div className={styles['preparation-wrapper']}>
+						<QuestionProgressBar />
+						<QuestionSlider />
+					</div>
+				</div>
+			</Block>
 			<Block></Block>
 			<Block>
 				<div className={styles.questions}>
