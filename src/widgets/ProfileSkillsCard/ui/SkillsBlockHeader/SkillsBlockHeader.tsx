dea2439d45091af039cc950caca5dba870c9bdad
Ikhtiyor <60789378+beckyuldashev@users.noreply.github.com>
2024-07-29 17:10:37 +0300

    feat (YH-238) add transfer to the profile editing page (#36)
    * feat (YH-238) add transfer to the profile editing page
    
    * feat (YH-238) add internationalization to tabs

--- src/widgets/ProfileSkillsCard/ui/SkillsBlockHeader/SkillsBlockHeader.tsx
+++ src/widgets/ProfileSkillsCard/ui/SkillsBlockHeader/SkillsBlockHeader.tsx
@@ -1,12 +1,20 @@
+import { useNavigate } from 'react-router-dom';
 import { Button } from 'yeahub-ui-kit';
 
 import styles from './SkillsBlockHeader.module.css';
 
 export const SkillsBlockHeader = () => {
+	const navigate = useNavigate();
+
 	return (
 		<div className={styles['skills-header']}>
 			<h3 className={styles['skills-title']}>Навыки</h3>
-			<Button theme="link" tagName="button" className={styles['skills-edit']}>
+			<Button
+				theme="link"
+				tagName="button"
+				className={styles['skills-edit']}
+				onClick={() => navigate('edit#skills')}
+			>
 				Редактировать
 			</Button>
 		</div>
