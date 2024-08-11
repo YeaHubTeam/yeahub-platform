6858e6c861b921a4ba3bcb8c790923c0a91b2b7a
Alexander Kazakov <alexandkazakov@ya.ru>
2024-08-11 12:52:42 +0300

    feat (YH-243) Fix cert and key paths in WebpackConfig

--- config/webpack/webpackDevServer.ts
+++ config/webpack/webpackDevServer.ts
@@ -1,19 +1,18 @@
 import { readFileSync } from 'fs';
-import path from 'path';
 
 import type { Configuration as DevServerConfiguration } from 'webpack-dev-server';
 
 import { WebpackOptions } from './types/types';
 
-export const webpackDevServer = ({ port }: WebpackOptions): DevServerConfiguration => {
+export const webpackDevServer = ({ port, paths }: WebpackOptions): DevServerConfiguration => {
   return {
     port: port ?? 3001,
     open: true,
     historyApiFallback: true,
     hot: true,
     https: {
-      key: readFileSync(path.resolve(__dirname, '..', '..', 'cert', 'localhost.key')),
-      cert: readFileSync(path.resolve(__dirname, '..', '..', 'cert', 'localhost.crt')),
+      key: readFileSync(paths.httpsKey),
+      cert: readFileSync(paths.httpsCert),
     },
   };
 };
