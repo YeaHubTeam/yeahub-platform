6792dfa086c790c6ac196c569a741695377e9c2c
Vladimir <62429354+Buffik@users.noreply.github.com>
2024-08-20 15:19:59 +0300

    Add skill select to user profile form (#51)
    * feat: add skill select component
    
    * fix from comments

---
+++ src/entities/skill/ui/SkillSelect/SkillSelect.tsx
@@ -0,0 +1,83 @@
+import { useMemo, useState } from 'react';
+import { Select, Chip, Text } from 'yeahub-ui-kit';
+
+import { i18Namespace } from '@/shared/config/i18n';
+import { useI18nHelpers } from '@/shared/hooks/useI18nHelpers';
+
+import { useGetSkillsListQuery } from '../../api/skillApi';
+import { Skill } from '../../model/types/skill';
+
+import styles from './SkillSelect.module.css';
+
+type SkillSelectProps = Omit<React.ComponentProps<typeof Select>, 'options' | 'type' | 'value'> & {
+	value: number[];
+	onChange: (value: number[]) => void;
+};
+
+export const SkillSelect = ({ onChange, value }: SkillSelectProps) => {
+	const { t } = useI18nHelpers(i18Namespace.profile);
+	const { data: skills } = useGetSkillsListQuery({});
+
+	const [selectedSkills, setSelectedSkills] = useState<number[]>(value);
+
+	const handleChange = (newValue: string | undefined) => {
+		if (!newValue) return;
+		const updates = [...(selectedSkills || []), +newValue];
+		setSelectedSkills(updates);
+		onChange(updates);
+	};
+
+	const handleDeleteSkill = (id: number) => () => {
+		const updates = selectedSkills.filter((skillId) => skillId !== id);
+		setSelectedSkills(updates);
+		onChange(updates);
+	};
+
+	const options = useMemo(() => {
+		return (skills?.data || [])
+			.map((skill) => ({
+				label: skill.title,
+				value: skill.id.toString(),
+			}))
+			.filter((skill) => !selectedSkills?.includes(+skill.value));
+	}, [skills?.data, selectedSkills]);
+
+	const skillsDictionary = useMemo(() => {
+		return skills?.data?.reduce(
+			(acc, skill) => {
+				acc[skill.id] = skill;
+				return acc;
+			},
+			{} as Record<string, Skill>,
+		);
+	}, [skills]);
+
+	return (
+		<div className={styles.wrapper}>
+			<Select
+				onChange={handleChange}
+				options={options}
+				type="default"
+				placeholder={options.length ? t('skillForm.skillSelect') : t('skillForm.emptySkillSelect')}
+				className={styles.select}
+			/>
+			{Boolean(selectedSkills?.length) && (
+				<>
+					<Text text={t('skillForm.selectedSkills')} className={styles.title} />
+					<div className={styles.selection}>
+						{selectedSkills?.map((skillId) => {
+							return (
+								<Chip
+									theme="primary"
+									onDelete={handleDeleteSkill(skillId)}
+									key={skillId}
+									label={skillsDictionary?.[skillId].title}
+								/>
+							);
+						})}
+					</div>
+				</>
+			)}
+		</div>
+	);
+};
