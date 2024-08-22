d889191275a5f9f56743e23e3972dfc8046a3422
Siarhei Shapavalau <Siarhei.Shapavalau@ventionteams.com>
2024-08-22 10:34:01 +0200

    feature/yh276: refactor project entity

--- src/entities/profileProject/index.ts
+++
@@ -1,2 +0,0 @@
-export { PROJECT_LIST } from './model/data/projectsList';
-export type { ProfileProject } from './model/types/profileProject';
--- src/entities/project/index.ts
+++ src/entities/project/index.ts
@@ -1,1 +1,3 @@
-export { ProjectForm } from './ProjectsForm/ProjectsForm';
+export { ProjectForm } from './ui/ProjectsForm/ProjectsForm';
+export { PROJECT_LIST } from '../project/model/data/projectsList';
+export type { ProfileProject } from '../project/model/types/profileProject';
