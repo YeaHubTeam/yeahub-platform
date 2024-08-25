98e42912d0809d25c9310ab38748f7ce1950df9b
Alexander Kazakov <alexandkazakov@ya.ru>
2024-08-25 14:22:59 +0300

    feat (YH-269) Changed localStorage.getItem to helper

--- src/app/providers/router/ui/AuthRoute.tsx
+++ src/app/providers/router/ui/AuthRoute.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
 
+import { getAccessToken } from '@/shared/helpers/getAccessToken';
+
 type Props = {
 	children: React.ReactNode;
 };
 
 export const AuthRoute = ({ children }: Props) => {
-	const isAuth = localStorage.getItem('accessToken');
+	const isAuth = getAccessToken();
 	const replaceUrl = '/auth/login';
 
 	return isAuth ? <>{children}</> : <Navigate to={replaceUrl} />;
--- src/app/providers/router/ui/UnAuthRoute.tsx
+++ src/app/providers/router/ui/UnAuthRoute.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
 
+import { getAccessToken } from '@/shared/helpers/getAccessToken';
+
 type Props = {
 	children: React.ReactNode;
 };
 
 export const UnAuthRoute = ({ children }: Props) => {
-	const isAuth = localStorage.getItem('accessToken');
+	const isAuth = getAccessToken();
 	const replaceUrl = '/';
 
 	return !isAuth ? <>{children}</> : <Navigate to={replaceUrl} />;
