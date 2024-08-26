449b68fce3b0af41a8f32f025d8b3edfc37c3190
Beck Yuldashev <yuldashev.bek@gmail.com>
2024-08-26 22:02:31 +0300

    feat (YH-290) fix local storage functions

--- src/shared/helpers/manageLocalStorage.ts
+++ src/shared/helpers/manageLocalStorage.ts
@@ -1,10 +1,11 @@
 export const setToLS = (key: string, value: unknown) => {
-	localStorage.setItem(key, JSON.stringify(value));
+	const transformedValue = typeof value === 'string' ? value : JSON.stringify(value);
+	localStorage.setItem(key, transformedValue);
 };
 
 export const getFromLS = (key: string) => {
 	const item = localStorage.getItem(key);
-	return item ? JSON.parse(item) : null;
+	return item ? JSON.parse(JSON.stringify(item)) : null;
 };
 
 export const removeFromLS = (key: string) => {
