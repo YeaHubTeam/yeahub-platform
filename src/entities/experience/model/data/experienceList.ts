1c1faa336af1d7b7f199d801b0fd02ccfcdc6e8e
TeRovaWork <uvarov@smapo.ru>
2024-08-29 14:01:31 +0300

    YH-242 | update img in project & experience

--- src/entities/experience/model/data/experienceList.ts
+++ src/entities/experience/model/data/experienceList.ts
@@ -5,7 +5,7 @@ export const EXPERIENCE_LIST: ProfileExperience[] = [
 	{
 		id: '1',
 		imgUrl:
-			'https://s3-alpha-sig.figma.com/img/58e1/b09c/50cfd2764f457cd297c68742b11503d3?Expires=1722211200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=ImfZL-ClZcxIwaSNv6WwirjGylfhCYXt2HB4ysz25mCWfJu5jwOZO8QbEk8i5BzajuYbZKTGODL-LUPTItxSpFBDm2sScvprNCMIHpi2LJW-HbCfXp06uIIFnspuR3AU6tNIsC4f6OO2VuKeiogx3gYsu~X95VQICpVLoA4hahVCuEETi8TAVdp89rdbJhN-tGfwVfAoT5cMMACfBFQcflOYRf6TCOUUblerR1OTRXJJ5EiqZPy~zhjnx1e1si52lvfV4fsBKuvEQkRk0v8gd12Yd-t4sVMfOeRuxhGTYr39ZTGbG8BJWUzEgnjj0aCREpkXaBzg2uizHwMqkytwcQ__',
+			'https://s3-alpha-sig.figma.com/img/eae3/13a4/8883a46e7a2a60ee806e73a8052191be?Expires=1724025600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=K3te7aowP8zcQRVl2gYKAO0DLnNeiA~PXn3yZ8V0gD6hCR3iptAQts7xqdCl23bHR0qpdgF3svL5FeKJGAk29ek43CajHgDJJMLcucQMl2B8bZA7AEqFNIpV8ckaxZdR~1bjNDAM1mTdJLK2hZ20mC4syGqijclcvYFf3kxDARBVKQiy0oturv5KcHGOQw6yW5E2zN9aSacx6Hm~n~snFsROX160tRKghs9Ocd8z-iT12Usb3eCgN0z3ToEOW8nYwlx4OviGsImjZyTQi6ETLEWGfdr4WdsYkLjQFW3U7F5SK7PpvRbaDC8UXapFamh-KuQ5BAkCe3Vyy9iDEoAWcA__',
 		name: 'UX/UI дизайнер',
 		company: 'Яндекс',
 		schedule: 'Full-time',
@@ -15,7 +15,7 @@ export const EXPERIENCE_LIST: ProfileExperience[] = [
 	{
 		id: '2',
 		imgUrl:
-			'https://s3-alpha-sig.figma.com/img/58e1/b09c/50cfd2764f457cd297c68742b11503d3?Expires=1722211200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=ImfZL-ClZcxIwaSNv6WwirjGylfhCYXt2HB4ysz25mCWfJu5jwOZO8QbEk8i5BzajuYbZKTGODL-LUPTItxSpFBDm2sScvprNCMIHpi2LJW-HbCfXp06uIIFnspuR3AU6tNIsC4f6OO2VuKeiogx3gYsu~X95VQICpVLoA4hahVCuEETi8TAVdp89rdbJhN-tGfwVfAoT5cMMACfBFQcflOYRf6TCOUUblerR1OTRXJJ5EiqZPy~zhjnx1e1si52lvfV4fsBKuvEQkRk0v8gd12Yd-t4sVMfOeRuxhGTYr39ZTGbG8BJWUzEgnjj0aCREpkXaBzg2uizHwMqkytwcQ__',
+			'https://s3-alpha-sig.figma.com/img/eae3/13a4/8883a46e7a2a60ee806e73a8052191be?Expires=1724025600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=K3te7aowP8zcQRVl2gYKAO0DLnNeiA~PXn3yZ8V0gD6hCR3iptAQts7xqdCl23bHR0qpdgF3svL5FeKJGAk29ek43CajHgDJJMLcucQMl2B8bZA7AEqFNIpV8ckaxZdR~1bjNDAM1mTdJLK2hZ20mC4syGqijclcvYFf3kxDARBVKQiy0oturv5KcHGOQw6yW5E2zN9aSacx6Hm~n~snFsROX160tRKghs9Ocd8z-iT12Usb3eCgN0z3ToEOW8nYwlx4OviGsImjZyTQi6ETLEWGfdr4WdsYkLjQFW3U7F5SK7PpvRbaDC8UXapFamh-KuQ5BAkCe3Vyy9iDEoAWcA__',
 		name: 'UX/UI дизайнер',
 		company: 'Яндекс',
 		schedule: 'Full-time',
@@ -25,7 +25,7 @@ export const EXPERIENCE_LIST: ProfileExperience[] = [
 	{
 		id: '3',
 		imgUrl:
-			'https://s3-alpha-sig.figma.com/img/58e1/b09c/50cfd2764f457cd297c68742b11503d3?Expires=1722211200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=ImfZL-ClZcxIwaSNv6WwirjGylfhCYXt2HB4ysz25mCWfJu5jwOZO8QbEk8i5BzajuYbZKTGODL-LUPTItxSpFBDm2sScvprNCMIHpi2LJW-HbCfXp06uIIFnspuR3AU6tNIsC4f6OO2VuKeiogx3gYsu~X95VQICpVLoA4hahVCuEETi8TAVdp89rdbJhN-tGfwVfAoT5cMMACfBFQcflOYRf6TCOUUblerR1OTRXJJ5EiqZPy~zhjnx1e1si52lvfV4fsBKuvEQkRk0v8gd12Yd-t4sVMfOeRuxhGTYr39ZTGbG8BJWUzEgnjj0aCREpkXaBzg2uizHwMqkytwcQ__',
+			'https://s3-alpha-sig.figma.com/img/eae3/13a4/8883a46e7a2a60ee806e73a8052191be?Expires=1724025600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=K3te7aowP8zcQRVl2gYKAO0DLnNeiA~PXn3yZ8V0gD6hCR3iptAQts7xqdCl23bHR0qpdgF3svL5FeKJGAk29ek43CajHgDJJMLcucQMl2B8bZA7AEqFNIpV8ckaxZdR~1bjNDAM1mTdJLK2hZ20mC4syGqijclcvYFf3kxDARBVKQiy0oturv5KcHGOQw6yW5E2zN9aSacx6Hm~n~snFsROX160tRKghs9Ocd8z-iT12Usb3eCgN0z3ToEOW8nYwlx4OviGsImjZyTQi6ETLEWGfdr4WdsYkLjQFW3U7F5SK7PpvRbaDC8UXapFamh-KuQ5BAkCe3Vyy9iDEoAWcA__',
 		name: 'UX/UI дизайнер',
 		company: 'Яндекс',
 		schedule: 'Full-time',
