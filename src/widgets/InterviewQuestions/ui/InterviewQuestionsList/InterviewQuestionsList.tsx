42a852150780807f7fbb04ccd53e08dffccfd29a
Дарья <106686333+sam-dash@users.noreply.github.com>
2024-09-01 22:01:28 +0300

    feat (YH-285) add query to fetch all questions with parameters (#71)
    * feat (YH-285) add query to fetch all questions with parameters
    
    * feat (YH-285) rename specializationId
    
    * feat (YH-285) fix type specialization
    
    * feat (YH-285) fix http

--- src/widgets/InterviewQuestions/ui/InterviewQuestionsList/InterviewQuestionsList.tsx
+++ src/widgets/InterviewQuestions/ui/InterviewQuestionsList/InterviewQuestionsList.tsx
@@ -1,13 +1,27 @@
-import { INTERVIEW_QUESTIONS } from '@/entities/quiz';
+import { useProfileQuery } from '@/entities/auth';
+import { useGetQuestionsListQuery } from '@/entities/question';
 
 import { InterviewQuestionsItem } from '../InterviewQuestionsItem/InterviewQuestionsItem';
 
 import styles from './InterviewQuestionsList.module.css';
 
 export const InterviewQuestionsList = () => {
+	const { data: profile } = useProfileQuery();
+
+	const specializationId = profile?.profiles[0].specializationId;
+
+	const params = {
+		random: true,
+		limit: 3,
+		specialization: specializationId,
+	};
+
+	const { data: response } = useGetQuestionsListQuery(params);
+	const questions = response?.data;
+
 	return (
 		<ul className={styles.list}>
-			{INTERVIEW_QUESTIONS.map((question) => (
+			{questions?.map((question) => (
 				<InterviewQuestionsItem key={question.id} question={question} />
 			))}
 		</ul>
