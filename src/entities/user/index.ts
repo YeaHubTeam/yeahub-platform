9aa4ff5a229a2c226f8ac6901db456b3bcd3eb9c
MaxI <makcimka1477@yandex.ru>
2024-09-09 10:45:14 +0300

    feature/YH-312 delete mock user data

--- src/entities/user/index.ts
+++ src/entities/user/index.ts
@@ -1,5 +1,5 @@
 export { AboutMeForm } from './ui/AboutMeForm/AboutMeForm';
 export { PersonalInformationForm } from './ui/PersonalInformationForm/PersonalInformationForm';
-export { USER_INFO, USER_LINK } from '../user/model/data/userInfo';
-export { useUpdateProfileMutation } from '../user/api/userApi';
 export type { UserInfoProfile } from './model/types/userInfo';
+
+export { useUpdateProfileMutation } from '../user/api/userApi';
