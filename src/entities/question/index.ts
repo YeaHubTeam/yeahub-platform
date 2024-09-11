f00e7e85d59d08e148726ac547ff5dfeee752740
Denis Pereloma <den.pereloma@gmail.com>
2024-09-11 19:04:47 +0300

    YH-222: fix import

--- src/entities/question/index.ts
+++ src/entities/question/index.ts
@@ -4,7 +4,6 @@ export {
 	useGetQuestionsListQuery,
 	useGetLearnedQuestionsQuery,
 } from './api/questionApi';
-export { QuestionPreview } from './ui/QuestionPreview/QuestionPreview';
 export { ChooseQuestionComplexity } from './ui/ChooseQuestionComplexity/ChooseQuestionComplexity';
 export { ChooseQuestionCount } from './ui/ChooseQuestionCount/ChooseQuestionCount';
 export { ChooseQuestionsCategories } from './ui/ChooseQuestionsCategories/ChooseQuestionsCategories';
