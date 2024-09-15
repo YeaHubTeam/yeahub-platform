4433dd8ba1e904cfc1769f6895c8fd215132b024
osimjon <osimjon90@yandex.ru>
2024-09-15 14:13:25 +0300

    feature/YH-322: fix: sizes

--- src/pages/InterviewQuizResultPage/ui/InterviewQuizResultPage.skeleton.tsx
+++ src/pages/InterviewQuizResultPage/ui/InterviewQuizResultPage.skeleton.tsx
@@ -6,19 +6,24 @@ import styles from './InterviewResultPage.module.css';
 export const InterviewQuizResultPageSkeleton = () => {
 	return (
 		<div className={styles.container}>
-			<Block>
+			<Block className={styles.block}>
 				<Flex direction="column" align="center" className={styles.result}>
 					<Skeleton width="calc(100% - 200px)" height={30} />
 					<Flex direction="row" gap="16" align="center">
 						<Skeleton
-							width="clamp(150px, 10vw, 300px)"
-							height="clamp(150px, 10vw, 300px)"
+							width="clamp(180px, 12vw, 300px)"
+							height="clamp(180px, 12vw, 300px)"
 							borderRadius="50%"
 						/>
-
 						<Flex direction="column" gap="20" align="center">
 							{[...Array(3)].map((_, i) => (
-								<Skeleton key={i} height={'clamp(17px, 4vw, 35px)'} width={100} />
+								<Flex key={i} direction="row" gap="8" align="center">
+									<Skeleton height={'35px'} width={12} />
+									<Flex direction="column" gap="4" align="center">
+										<Skeleton height={17} width={100} />
+										<Skeleton height={17} width={100} />
+									</Flex>
+								</Flex>
 							))}
 						</Flex>
 					</Flex>
@@ -39,9 +44,9 @@ export const InterviewQuizResultPageSkeleton = () => {
 				</Block>
 				<Flex justify="between" gap="12">
 					{[...Array(4)].map((_, i) => (
-						<Block key={i}>
-							<Skeleton width="clamp(40px, 7vw, 300px)" height={17} style={{ marginBottom: 12 }} />
-							<Skeleton width="clamp(40px, 7vw, 300px)" height={17} />
+						<Block key={i} className={styles.stats}>
+							<Skeleton width="88%" height={17} style={{ marginBottom: 12, flexGrow: 1 }} />
+							<Skeleton width="88%" height={17} />
 						</Block>
 					))}
 				</Flex>
@@ -49,7 +54,10 @@ export const InterviewQuizResultPageSkeleton = () => {
 			<Block className={styles.passed}>
 				<Flex direction="column" gap="16" align="center">
 					<Skeleton width={'clamp(350px, 5vw, 600px)'} height={30} />
-					<Skeleton />
+					<Flex gap="16" align="center" justify="between">
+						<Skeleton width="12vw" />
+						<Skeleton width="calc(50vw - 100px)" />
+					</Flex>
 				</Flex>
 			</Block>
 		</div>
