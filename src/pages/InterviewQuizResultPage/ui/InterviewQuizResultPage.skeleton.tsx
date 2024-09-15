72334da5e971acabb095c1305de4d77afd0b4c14
osimjon <osimjon90@yandex.ru>
2024-09-16 00:30:43 +0300

    feature/YH-322: add: Skeleton for question list

--- src/pages/InterviewQuizResultPage/ui/InterviewQuizResultPage.skeleton.tsx
+++ src/pages/InterviewQuizResultPage/ui/InterviewQuizResultPage.skeleton.tsx
@@ -54,10 +54,20 @@ export const InterviewQuizResultPageSkeleton = () => {
 			<Block className={styles.passed}>
 				<Flex direction="column" gap="16" align="center">
 					<Skeleton width={'clamp(350px, 5vw, 600px)'} height={30} />
-					<Flex gap="16" align="center" justify="between">
-						<Skeleton width="12vw" />
-						<Skeleton width="calc(50vw - 100px)" />
-					</Flex>
+					<div className={styles['question-list']}>
+						{[...Array(6)].map((_, i) => (
+							<Flex key={i} gap="16" align="center" style={{ width: '100%' }}>
+								<Skeleton width="150px" />
+								<Flex gap="16" direction="column" style={{ width: '100%' }}>
+									<Skeleton width={'90%'} height={50} />
+									<Flex gap="8">
+										<Skeleton width="30px" height={30} />
+										<Skeleton width="55px" height={30} />
+									</Flex>
+								</Flex>
+							</Flex>
+						))}
+					</div>
 				</Flex>
 			</Block>
 		</div>
