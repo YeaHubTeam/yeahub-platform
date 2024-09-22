ca9c908df29cc403fca949a23b4ac707f1a40421
Maxim <84065692+makcimishevskih@users.noreply.github.com>
2024-09-22 12:11:53 +0300

    feature/YH-338 toast after updateProfile request (#109)
    * feature/YH-338 toast after updateProfile request
    
    * redirect and toast in updateProfile

--- src/entities/user/api/userApi.ts
+++ src/entities/user/api/userApi.ts
@@ -1,7 +1,8 @@
 import { ApiTags } from '@/shared/config/api/apiTags';
 import { baseApi } from '@/shared/config/api/baseApi';
+import { toast } from '@/shared/ui/Toast';
 
-import type { UserInfoProfile } from '../model/types/userInfo';
+import type { UserInfoProfile, ExtraArgument } from '../model/types/userInfo';
 
 export const userApi = baseApi.injectEndpoints({
 	endpoints: (build) => ({
@@ -12,6 +13,19 @@ export const userApi = baseApi.injectEndpoints({
 				method: 'PUT',
 				providesTags: [ApiTags.PROFILE_DETAIL],
 			}),
+			async onQueryStarted(_, { queryFulfilled, extra }) {
+				try {
+					await queryFulfilled;
+					const typedExtra = extra as ExtraArgument;
+
+					toast.success('Профиль был успешно изменен');
+					typedExtra.navigate('/profile');
+				} catch (err) {
+					toast.error('Профиль не удалось изменить');
+					// eslint-disable-next-line no-console
+					console.log(err);
+				}
+			},
 			invalidatesTags: (_result, _error, { id }) => [{ type: ApiTags.PROFILE_DETAIL, id }],
 		}),
 	}),
