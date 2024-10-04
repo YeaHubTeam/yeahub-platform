4fbcde49ee49932edfd45699ce6191ee9b17ef29
V4nden <vanden.gd@gmail.com>
2024-10-04 20:59:53 +0300

    YH-365: unmocking statistics info on interview page

--- src/pages/InterviewPage/ui/InterviewPage.tsx
+++ src/pages/InterviewPage/ui/InterviewPage.tsx
@@ -1,4 +1,4 @@
-import { useMemo } from 'react';
+import { useCallback, useMemo } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Button } from 'yeahub-ui-kit';
 
@@ -13,7 +13,11 @@ import { Loader } from '@/shared/ui/Loader';
 import { PassedQuestionStatInfo } from '@/shared/ui/PassedQuestionStatInfo';
 
 import { useProfileQuery } from '@/entities/auth';
-import { getActiveQuizQuestions, useGetActiveQuizQuery } from '@/entities/quiz';
+import {
+	getActiveQuizQuestions,
+	useGetActiveQuizQuery,
+	useGetProfileStatsQuery,
+} from '@/entities/quiz';
 
 import { PassedQuestionChart } from '@/widgets/Charts';
 import { InterviewHistoryList } from '@/widgets/InterviewHistory';
@@ -26,26 +30,35 @@ import styles from './InterviewPage.module.css';
 const InterviewPage = () => {
 	const { t } = useI18nHelpers(i18Namespace.interview);
 
-	const questionStats = [
-		{
-			title: 'Всего вопросов',
-			value: '0',
-		},
-		{
-			title: 'Не изучено',
-			value: '0',
-		},
-		{
-			title: 'Изучено',
-			value: '0',
-		},
-	];
-
 	const { data: profile } = useProfileQuery();
+
+	const { data: profileStats } = useGetProfileStatsQuery(profile?.profiles[0].profileId ?? '');
+
 	const { isLoading: isActiveQuizLoading } = useGetActiveQuizQuery({
 		profileId: profile?.profiles[0].profileId,
 		params: { limit: 1, page: 1 },
 	});
+
+	const questionStats = useCallback(
+		() =>
+			profileStats
+				? [
+						{
+							title: 'Всего вопросов',
+							value: String(profileStats.questionsStat.uniqueQuestionsCount),
+						},
+						{
+							title: 'Не изучено',
+							value: String(profileStats.questionsStat.unlearnedQuestionsCount),
+						},
+						{
+							title: 'Изучено',
+							value: String(profileStats.questionsStat.learnedQuestionsCount),
+						},
+					]
+				: [],
+		[profileStats],
+	);
 
 	const navigate = useNavigate();
 
@@ -145,8 +158,11 @@ const InterviewPage = () => {
 						actionDisabled={!lastActiveQuizInfo}
 					>
 						<div className={styles.statistics}>
-							<PassedQuestionChart total={0} learned={0} />
-							<PassedQuestionStatInfo stats={questionStats} />
+							<PassedQuestionChart
+								total={profileStats ? profileStats.questionsStat.uniqueQuestionsCount : 0}
+								learned={profileStats ? profileStats.questionsStat.learnedQuestionsCount : 0}
+							/>
+							<PassedQuestionStatInfo stats={questionStats()} />
 						</div>
 					</Card>
 					<Card
