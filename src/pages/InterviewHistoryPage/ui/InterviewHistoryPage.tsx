b83547e1847aeb72e60519f688505d4391dd94d8
Дарья <106686333+sam-dash@users.noreply.github.com>
2024-10-06 23:59:06 +0300

    YH-306: added skeleton in the HistoryPage (#131)
    * YH-306: added skeleton in the HistoryPage
    
    * YH-306: added skeleton block in the HistoryPage
    
    * YH-306: fix styles HistoryPageSkeleton

--- src/pages/InterviewHistoryPage/ui/InterviewHistoryPage.tsx
+++ src/pages/InterviewHistoryPage/ui/InterviewHistoryPage.tsx
@@ -4,12 +4,13 @@ import { useDispatch, useSelector } from 'react-redux';
 import { EventCalendar } from '@/shared/ui/Calendar';
 import { Value } from '@/shared/ui/Calendar/ui/EventCalendar';
 
-import { FullInterviewHistoryList } from '@/widgets/FullInterviewHistory';
+import { FullInterviewHistoryList, useGetHistory } from '@/widgets/FullInterviewHistory';
 
 import { getInterviewHistoryPageDateRange } from '../model/selectors/InterviewHistoryPageSelectors';
 import { interviewHistoryPageActions } from '../model/slices/InterviewHistoryPageSlice';
 
 import styles from './InterviewHistoryPage.module.css';
+import { InterviewHistoryPageSkeleton } from './InterviewHistoryPage.skeleton';
 
 const InterviewHistoryPage = () => {
 	const dispatch = useDispatch();
@@ -20,11 +21,15 @@ const InterviewHistoryPage = () => {
 		dispatch(interviewHistoryPageActions.setDateRange(dates));
 	};
 
+	const { isLoading } = useGetHistory();
+
 	useEffect(() => {
 		return () => {
 			dispatch(interviewHistoryPageActions.setDateRange(null));
 		};
 	}, [dispatch]);
+
+	if (isLoading) return <InterviewHistoryPageSkeleton />;
 
 	return (
 		<div className={styles.container}>
