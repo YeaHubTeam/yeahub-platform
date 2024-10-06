a4eff5972c1f4c5e853f46763b34333edf72d1a7
Daria Samodelova <sam-dash@yandex.ru>
2024-10-06 03:40:46 +0300

    YH-306: added skeleton block in the HistoryPage

--- src/pages/InterviewHistoryPage/ui/InterviewHistoryPage.skeleton.tsx
+++ src/pages/InterviewHistoryPage/ui/InterviewHistoryPage.skeleton.tsx
@@ -1,4 +1,5 @@
 import { Card } from '@/shared/ui/Card';
+import { Flex } from '@/shared/ui/Flex';
 import { Skeleton } from '@/shared/ui/Skeleton/Skeleton';
 
 import styles from './InterviewHistoryPage.module.css';
@@ -6,15 +7,51 @@ import styles from './InterviewHistoryPage.module.css';
 export const InterviewHistoryPageSkeleton = () => {
 	return (
 		<section className={styles['skeleton-wrapper']}>
-			<div>
+			<div className={styles['skeleton-card-block']}>
+				<Skeleton width="36px" height="36px" className={styles['skeleton-mobile-calendar']} />
 				{[...Array(3)].map((_, i) => (
 					<Card key={i} className={styles['skeleton-card']}>
-						<Skeleton height="200px" />
+						<Flex gap="20" justify="between" style={{ marginBottom: '48px' }}>
+							<Skeleton width="177px" height="25px" />
+							<Skeleton width="100px" height="20px" />
+						</Flex>
+						<Flex gap="24" style={{ flexWrap: 'wrap', marginBottom: '34px' }}>
+							<Skeleton width="132px" height="22px" />
+							<Skeleton width="196px" height="22px" />
+							<Skeleton width="191px" height="22px" />
+							<Skeleton width="96px" height="22px" />
+						</Flex>
+						<Skeleton width="137px" height="19px" />
+						<Flex gap="8" style={{ flexWrap: 'wrap', marginTop: '8px' }}>
+							<Skeleton width="97px" height="42px" />
+							<Skeleton width="144px" height="42px" />
+							<Skeleton width="114px" height="42px" />
+							<Skeleton width="85px" height="42px" />
+							<Skeleton width="95px" height="42px" />
+							<Skeleton width="97px" height="42px" />
+							<Skeleton width="144px" height="42px" />
+							<Skeleton width="85px" height="42px" />
+							<Skeleton width="95px" height="42px" />
+							<Skeleton width="97px" height="42px" />
+							<Skeleton width="144px" height="42px" />
+						</Flex>
 					</Card>
 				))}
 			</div>
 			<Card className={styles['skeleton-calendar']}>
-				<Skeleton width="312px" height="276px" />
+				<Flex justify="between" style={{ marginBottom: '31px' }}>
+					<Skeleton width="24px" height="25px" />
+					<Skeleton width="64px" height="25px" />
+					<Skeleton width="24px" height="24px" />
+				</Flex>
+				<Flex gap="12" direction="column">
+					<Skeleton width="311px" height="19px" style={{ marginBottom: '16px' }} />
+					<Skeleton width="312px" height="19px" />
+					<Skeleton width="312px" height="19px" />
+					<Skeleton width="312px" height="19px" />
+					<Skeleton width="312px" height="19px" />
+					<Skeleton width="150px" height="19px" />
+				</Flex>
 			</Card>
 		</section>
 	);
