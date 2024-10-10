6a2f8ba3afc7d94e7bddde3714ad6e1414753651
Fr1Day12 <almaz9061198856@gmail.com>
2024-10-10 17:22:34 +0300

    YH-399: Remove image stubs on interview and question sections

--- src/widgets/Question/ui/QuestionHeader/QuestionHeader.tsx
+++ src/widgets/Question/ui/QuestionHeader/QuestionHeader.tsx
@@ -1,4 +1,3 @@
-import QuestionImg from '@/shared/assets/images/question_img.png';
 import { Card } from '@/shared/ui/Card';
 
 import styles from './QuestionHeader.module.css';
@@ -15,9 +14,7 @@ export const QuestionHeader = ({ title, description, status }: QuestionHeaderPro
 	return (
 		<Card className={styles.wrapper}>
 			<div className={styles['question-header-wrapper']}>
-				<div className={styles['image-wrapper']}>
-					<img className={styles.img} src={QuestionImg} alt="Question" />
-				</div>
+				<div className={styles['image-wrapper']}></div>
 				<div className={styles['title-wrapper']}>
 					<h2 className={styles.title}>{title}</h2>
 					<p className={styles.description}>{description}</p>
