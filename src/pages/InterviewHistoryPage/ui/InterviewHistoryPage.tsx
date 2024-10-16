7798da933dc949a6b1413dd7cf365dc408b6d4de
Fr1Day12 <106734778+Fr1Day12@users.noreply.github.com>
2024-10-16 15:10:53 +0300

    YH-417: add InterviewHistoryPage stub (#166)
    * YH-417: add InterviewHistoryPage stub
    
    * YH-417: replace div with card and remove useless styles

--- src/pages/InterviewHistoryPage/ui/InterviewHistoryPage.tsx
+++ src/pages/InterviewHistoryPage/ui/InterviewHistoryPage.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { EventCalendar } from '@/shared/ui/Calendar';
@@ -13,12 +13,20 @@ import styles from './InterviewHistoryPage.module.css';
 import { InterviewHistoryPageSkeleton } from './InterviewHistoryPage.skeleton';
 
 const InterviewHistoryPage = () => {
+	const [selectedDates, setSelectedDates] = useState<Value>(null);
+
 	const dispatch = useDispatch();
 
 	const dateRange = useSelector(getInterviewHistoryPageDateRange);
 
 	const handleDateChange = (dates: Value) => {
+		setSelectedDates(dates);
 		dispatch(interviewHistoryPageActions.setDateRange(dates));
+	};
+
+	const handleResetFilters = () => {
+		setSelectedDates(null);
+		dispatch(interviewHistoryPageActions.setDateRange(null));
 	};
 
 	const { isLoading } = useGetHistory();
@@ -33,8 +41,8 @@ const InterviewHistoryPage = () => {
 
 	return (
 		<div className={styles.container}>
-			<FullInterviewHistoryList dateRange={dateRange} />
-			<EventCalendar onDateChange={handleDateChange} />
+			<FullInterviewHistoryList dateRange={dateRange} resetFilters={handleResetFilters} />
+			<EventCalendar onDateChange={handleDateChange} selectedDates={selectedDates} />
 		</div>
 	);
 };
