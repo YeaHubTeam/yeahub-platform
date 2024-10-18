f80ffa85a2f3d76524f55a8b21889fa8a6f10326
nick <nikita.kolymazhnov@gmail.com>
2024-10-18 14:45:02 +0300

    YH-435: fix quiz navigation (#168)
    * YH-435: fix quiz navigation
    
    * YH-435: fix complete button
    
    * YH-435: refactoring
    
    * YH-435: refactoring

--- src/app/providers/router/config/routeConfig.tsx
+++ src/app/providers/router/config/routeConfig.tsx
@@ -230,7 +230,7 @@ export const router = createBrowserRouter([
 								path: ROUTES.interview.new.route,
 								element: <InterviewQuizPage />,
 								handle: {
-									crumb: Translation.CRUMBS_INTERVIEW,
+									crumb: Translation.CRUMBS_QUIZ,
 								},
 							},
 						],
--- src/entities/quiz/model/selectors/quizSelectors.ts
+++ src/entities/quiz/model/selectors/quizSelectors.ts
@@ -3,3 +3,7 @@ import { State } from '@/shared/config/store/State';
 export const getActiveQuizQuestions = (state: State) => {
 	return state.activeQuiz.questions;
 };
+
+export const getIsAllQuestionsAnswered = (state: State) => {
+	return state.activeQuiz.questions.every((question) => question.answer !== undefined);
+};
