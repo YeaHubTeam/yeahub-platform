2927badf540ccdf3fb883602d50a5962485398fb
kirillcodes <kirillusernet@gmail.com>
2024-10-18 16:24:32 +0300

    YH-439: styles position and variables, ui

--- src/shared/ui/Modal/ui/Modal.tsx
+++ src/shared/ui/Modal/ui/Modal.tsx
@@ -1,36 +1,76 @@
+import { Button, Icon } from 'yeahub-ui-kit';
+
 import styles from './styles.module.css';
 
 type ModalProps = {
 	title: string;
-	isModal: boolean;
-	handleOverlayClick: (e: React.MouseEvent<HTMLDivElement>) => void;
-	handleKeyDown: (e: React.KeyboardEvent<HTMLDivElement>) => void;
-	children: React.ReactNode | React.ReactElement;
+	isOpen: boolean;
+	onClose: () => void;
+	children: React.ReactNode;
+	buttonPrimaryText?: string;
+	buttonOutlineText?: string;
+	buttonPrimaryClick?: () => void;
+	buttonOutlineClick?: () => void;
 };
 
 export const Modal = ({
 	title,
-	isModal,
-	handleOverlayClick,
-	handleKeyDown,
+	isOpen,
+	onClose,
+	buttonPrimaryText,
+	buttonOutlineText,
+	buttonPrimaryClick,
+	buttonOutlineClick,
 	children,
 }: ModalProps) => {
+	const handleOverlayClick = (e: React.MouseEvent<HTMLDivElement>) => {
+		const target = e.target as HTMLDivElement;
+		if (target && target.id === 'modal-overlay') {
+			onClose();
+		}
+	};
+
+	const handleKeyDown = (e: React.KeyboardEvent<HTMLDivElement>) => {
+		if (e.key === 'Escape') {
+			onClose();
+		}
+	};
+
+	const isButtons = buttonOutlineText || buttonPrimaryText;
+
 	return (
 		<div
-			id="modal"
+			id="modal-overlay"
 			role="button"
 			tabIndex={0}
-			className={styles.overlay}
+			className={`${styles.overlay} ${isOpen ? styles['modal-open'] : ''}`}
 			onKeyDown={(e) => handleKeyDown(e)}
 			onClick={(e) => handleOverlayClick(e)}
-			style={isModal ? { display: 'block' } : { display: 'none' }}
 		>
 			<div className={styles.modal}>
-				<h3 id="modal-title" className={styles.title}>
-					{title}
-				</h3>
-				<hr />
+				<Icon
+					icon="xCircle"
+					type="button"
+					className={styles['x-circle']}
+					color="--palette-ui-black-25"
+					onClick={onClose}
+				/>
+				<h3 className={styles.title}>{title}</h3>
 				<div className={styles.content}>{children}</div>
+				{isButtons && (
+					<div className={styles.buttons}>
+						{buttonPrimaryText && (
+							<Button theme="primary" onClick={buttonPrimaryClick}>
+								{buttonPrimaryText}
+							</Button>
+						)}
+						{buttonOutlineText && (
+							<Button theme="outline" onClick={buttonOutlineClick}>
+								{buttonOutlineText}
+							</Button>
+						)}
+					</div>
+				)}
 			</div>
 		</div>
 	);
