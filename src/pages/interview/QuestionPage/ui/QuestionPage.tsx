7e4aa100ff284c30e092d47b2041f61fa4e13df4
osimjon <osimjon90@yandex.ru>
2024-10-24 15:37:38 +0300

    YH-436: resolved  conflict

--- src/pages/interview/QuestionPage/ui/QuestionPage.tsx
+++ src/pages/interview/QuestionPage/ui/QuestionPage.tsx
@@ -1,33 +1,27 @@
 import classNames from 'classnames';
 import { useMemo } from 'react';
-
 import { NavLink, useNavigate, useParams } from 'react-router-dom';
-
-import { ROUTES } from '@/shared/config/router/routes';
-import { useCheckSpecialization } from '@/shared/hooks/useCheckSpecialization';
-import { useScreenSize } from '@/shared/hooks/useScreenSize';
-import { RedirectToProfile } from '@/shared/ui/RedirectToProfile';
-
-import { NavLink, useParams } from 'react-router-dom';
 import { Button } from 'yeahub-ui-kit';
 
 import { i18Namespace } from '@/shared/config/i18n';
 import { Translation } from '@/shared/config/i18n/i18nTranslations';
+import { ROUTES } from '@/shared/config/router/routes';
+import { useCheckSpecialization } from '@/shared/hooks/useCheckSpecialization';
 import { useI18nHelpers } from '@/shared/hooks/useI18nHelpers';
 import { useScreenSize } from '@/shared/hooks/useScreenSize';
 import { BackButton } from '@/shared/ui/BackButton';
 import { Flex } from '@/shared/ui/Flex';
-
+import { RedirectToProfile } from '@/shared/ui/RedirectToProfile';
 
 import { useProfileQuery } from '@/entities/auth';
 import { useGetQuestionByIdQuery } from '@/entities/question';
 
 import {
-	QuestionHeader,
-	QuestionBody,
-	QuestionActions,
-	ProgressBlock,
 	AdditionalInfo,
+	ProgressBlock,
+	QuestionActions,
+	QuestionBody,
+	QuestionHeader,
 } from '@/widgets/Question';
 
 import styles from './QuestionPage.module.css';
@@ -71,7 +65,14 @@ export const QuestionPage = ({ isAdmin }: QuestionPageProps) => {
 	if (isMobile) {
 		return (
 			<section className={classNames(styles.wrapper, styles.mobile)}>
-
+				{isAdmin && (
+					<Flex justify="between" className={styles['admin-nav']}>
+						<BackButton />
+						<NavLink style={{ marginLeft: 'auto' }} to="edit">
+							<Button>{t(Translation.EDIT)}</Button>
+						</NavLink>
+					</Flex>
+				)}
 				{isSpecializationEmpty ? (
 					<RedirectToProfile />
 				) : (
@@ -81,7 +82,7 @@ export const QuestionPage = ({ isAdmin }: QuestionPageProps) => {
 							status={question?.status}
 							title={question?.title}
 						/>
-						<ProgressBlock checksCount={question?.checksCount} />
+						{!isAdmin && <ProgressBlock checksCount={question?.checksCount} />}
 						<AdditionalInfo
 							rate={question?.rate}
 							keywords={question?.keywords}
@@ -91,63 +92,20 @@ export const QuestionPage = ({ isAdmin }: QuestionPageProps) => {
 						<p className={styles.author}>
 							Автор: <NavLink to={`#`}>{authorFullName}</NavLink>
 						</p>
-						<QuestionActions
-							profileId={profile ? profile.profiles[0].id : ''}
-							questionId={questionId ? questionId : ''}
-						/>
+						{!isAdmin && (
+							<QuestionActions
+								profileId={profile ? profile.profiles[0].id : ''}
+								questionId={questionId ? questionId : ''}
+							/>
+						)}
 						<QuestionBody shortAnswer={question?.shortAnswer} longAnswer={question?.longAnswer} />
 					</>
 				)}
-
-				{isAdmin && (
-					<Flex justify="between" className={styles['admin-nav']}>
-						<BackButton />
-
-						<NavLink style={{ marginLeft: 'auto' }} to="edit">
-							<Button>{t(Translation.EDIT)}</Button>
-						</NavLink>
-					</Flex>
-				)}
-				<QuestionHeader
-					description={question?.description}
-					status={question?.status}
-					title={question?.title}
-				/>
-				{!isAdmin && <ProgressBlock checksCount={question?.checksCount} />}
-				<AdditionalInfo
-					rate={question?.rate}
-					keywords={question?.keywords}
-					complexity={question?.complexity}
-					questionSkills={question?.questionSkills}
-				/>
-				<p className={styles.author}>
-					Автор: <NavLink to={`#`}>{authorFullName}</NavLink>
-				</p>
-				{!isAdmin && (
-					<QuestionActions
-						profileId={profile ? profile.profiles[0].id : ''}
-						questionId={questionId ? questionId : ''}
-					/>
-				)}
-				<QuestionBody shortAnswer={question?.shortAnswer} longAnswer={question?.longAnswer} />
 			</section>
 		);
 	}
 
 	return (
-
-		<section className={styles.wrapper}>
-			{isSpecializationEmpty ? (
-				<RedirectToProfile />
-			) : (
-				<>
-					<div className={styles.main}>
-						<QuestionHeader
-							description={question?.description}
-							status={question?.status}
-							title={question?.title}
-						/>
-
 		<>
 			{isAdmin && (
 				<Flex justify="between" className={styles['admin-nav']}>
@@ -159,54 +117,39 @@ export const QuestionPage = ({ isAdmin }: QuestionPageProps) => {
 				</Flex>
 			)}
 			<section className={styles.wrapper}>
-				<div className={styles.main}>
-					<QuestionHeader
-						description={question?.description}
-						status={question?.status}
-						title={question?.title}
-					/>
-					{!isAdmin && (
-
-						<QuestionActions
-							profileId={profile ? profile.profiles[0].id : ''}
-							questionId={questionId ? questionId : ''}
-						/>
-
-						<QuestionBody shortAnswer={question?.shortAnswer} longAnswer={question?.longAnswer} />
-					</div>
-					<div className={styles.additional}>
-						<ProgressBlock checksCount={question?.checksCount} />
-						<AdditionalInfo
-							rate={question?.rate}
-							keywords={question?.keywords}
-							complexity={question?.complexity}
-							questionSkills={question?.questionSkills}
-						/>
-						<p className={styles.author}>
-							Автор: <NavLink to={`#`}>{authorFullName}</NavLink>
-						</p>
-					</div>
-				</>
-			)}
-		</section>
-
-					)}
-					<QuestionBody shortAnswer={question?.shortAnswer} longAnswer={question?.longAnswer} />
-				</div>
-				<div className={styles.additional}>
-					{!isAdmin && <ProgressBlock checksCount={question?.checksCount} />}
-					<AdditionalInfo
-						rate={question?.rate}
-						keywords={question?.keywords}
-						complexity={question?.complexity}
-						questionSkills={question?.questionSkills}
-					/>
-					<p className={styles.author}>
-						Автор: <NavLink to={`#`}>{authorFullName}</NavLink>
-					</p>
-				</div>
+				{isSpecializationEmpty ? (
+					<RedirectToProfile />
+				) : (
+					<>
+						<div className={styles.main}>
+							<QuestionHeader
+								description={question?.description}
+								status={question?.status}
+								title={question?.title}
+							/>
+							{!isAdmin && (
+								<QuestionActions
+									profileId={profile ? profile.profiles[0].id : ''}
+									questionId={questionId ? questionId : ''}
+								/>
+							)}
+							<QuestionBody shortAnswer={question?.shortAnswer} longAnswer={question?.longAnswer} />
+						</div>
+						<div className={styles.additional}>
+							{!isAdmin && <ProgressBlock checksCount={question?.checksCount} />}
+							<AdditionalInfo
+								rate={question?.rate}
+								keywords={question?.keywords}
+								complexity={question?.complexity}
+								questionSkills={question?.questionSkills}
+							/>
+							<p className={styles.author}>
+								Автор: <NavLink to={`#`}>{authorFullName}</NavLink>
+							</p>
+						</div>
+					</>
+				)}
 			</section>
 		</>
-
 	);
 };
