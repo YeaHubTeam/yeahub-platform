0acce2d30f271e619fca2724d32718f42dfdefe6
Roman Druzhinin <karamount11@gmail.com>
2024-10-25 21:40:17 +0300

    YH-443: a11y update (#206)
    * Add Suspense around Sidebar in MainLayout
    * Created "a11y" jsons in public/locales
    * Put p outside ul in QuestionCategories.tsx
    * Add aria-label on buttons in QuestionNavPanel, BackButton, EventCalendar, Sidebar
    * Fixed semantics in QuestionPreview and AdditionalInfo

--- src/shared/ui/BackButton/ui/BackButton.tsx
+++ src/shared/ui/BackButton/ui/BackButton.tsx
@@ -2,10 +2,15 @@ import { memo } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { IconButton, Icon } from 'yeahub-ui-kit';
 
+import { i18Namespace } from '@/shared/config/i18n';
+import { A11y } from '@/shared/config/i18n/i18nTranslations';
+import { useI18nHelpers } from '@/shared/hooks/useI18nHelpers';
+
 import styles from './BackButton.module.css';
 
 export const BackButton = memo(() => {
 	const navigate = useNavigate();
+	const { t } = useI18nHelpers(i18Namespace.a11y);
 
 	const onReturnBack = () => {
 		navigate(-1);
@@ -14,7 +19,7 @@ export const BackButton = memo(() => {
 	return (
 		<IconButton
 			onClick={onReturnBack}
-			aria-label="back button"
+			aria-label={t(A11y.BACK_BUTTON)}
 			icon={<Icon icon="arrowLeft" size={20} />}
 			form="round"
 			theme="outline"
