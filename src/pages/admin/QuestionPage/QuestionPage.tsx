5fcfffdd1e9dd2c76a26d53d14e9d0438e1604a8
Vanden <59515058+V4nden@users.noreply.github.com>
2024-10-29 22:41:39 +0400

    YH-465: Question page admin / interview refactoring (#214)

--- src/pages/admin/QuestionPage/QuestionPage.tsx
+++ src/pages/admin/QuestionPage/QuestionPage.tsx
@@ -1,28 +1,25 @@
 import classNames from 'classnames';
 import { useMemo } from 'react';
 import { NavLink, useParams } from 'react-router-dom';
+import { Button } from 'yeahub-ui-kit';
 
+import { i18Namespace } from '@/shared/config/i18n';
+import { Translation } from '@/shared/config/i18n/i18nTranslations';
+import { useI18nHelpers } from '@/shared/hooks/useI18nHelpers';
 import { useScreenSize } from '@/shared/hooks/useScreenSize';
+import { BackHeader } from '@/shared/ui/BackHeader';
 
 import { useProfileQuery } from '@/entities/auth';
 import { useGetQuestionByIdQuery } from '@/entities/question';
 
-import {
-	QuestionHeader,
-	QuestionBody,
-	QuestionActions,
-	ProgressBlock,
-	AdditionalInfo,
-} from '@/widgets/Question';
+import { QuestionHeader, QuestionBody, AdditionalInfo } from '@/widgets/Question';
 
 import styles from './QuestionPage.module.css';
 import { QuestionPageSkeleton } from './QuestionPage.skeleton';
 
-interface QuestionPageProps {
-	isAdmin?: boolean;
-}
+export const QuestionPage = () => {
+	const { t } = useI18nHelpers(i18Namespace.translation);
 
-export const QuestionPage = ({ isAdmin }: QuestionPageProps) => {
 	const { questionId } = useParams();
 	const { isMobile } = useScreenSize();
 
@@ -50,12 +47,17 @@ export const QuestionPage = ({ isAdmin }: QuestionPageProps) => {
 	if (isMobile) {
 		return (
 			<section className={classNames(styles.wrapper, styles.mobile)}>
+				<BackHeader>
+					<NavLink style={{ marginLeft: 'auto' }} to="edit">
+						<Button>{t(Translation.EDIT)}</Button>
+					</NavLink>
+				</BackHeader>
+
 				<QuestionHeader
 					description={question?.description}
 					status={question?.status}
 					title={question?.title}
 				/>
-				<ProgressBlock checksCount={question?.checksCount} />
 				<AdditionalInfo
 					rate={question?.rate}
 					keywords={question?.keywords}
@@ -66,11 +68,6 @@ export const QuestionPage = ({ isAdmin }: QuestionPageProps) => {
 					Автор: <NavLink to={`#`}>{authorFullName}</NavLink>
 				</p>
 
-				<QuestionActions
-					profileId={profile ? profile.profiles[0].id : ''}
-					questionId={questionId ? questionId : ''}
-				/>
-
 				<QuestionBody shortAnswer={question?.shortAnswer} longAnswer={question?.longAnswer} />
 			</section>
 		);
@@ -78,6 +75,12 @@ export const QuestionPage = ({ isAdmin }: QuestionPageProps) => {
 
 	return (
 		<>
+			<BackHeader>
+				<NavLink style={{ marginLeft: 'auto' }} to="edit">
+					<Button>{t(Translation.EDIT)}</Button>
+				</NavLink>
+			</BackHeader>
+
 			<section className={styles.wrapper}>
 				<div className={styles.main}>
 					<QuestionHeader
@@ -85,16 +88,9 @@ export const QuestionPage = ({ isAdmin }: QuestionPageProps) => {
 						status={question?.status}
 						title={question?.title}
 					/>
-					{!isAdmin && (
-						<QuestionActions
-							profileId={profile ? profile.profiles[0].id : ''}
-							questionId={questionId ? questionId : ''}
-						/>
-					)}
 					<QuestionBody shortAnswer={question?.shortAnswer} longAnswer={question?.longAnswer} />
 				</div>
 				<div className={styles.additional}>
-					{!isAdmin && <ProgressBlock checksCount={question?.checksCount} />}
 					<AdditionalInfo
 						rate={question?.rate}
 						keywords={question?.keywords}
