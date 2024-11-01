d4db5bebfa308f9c7a0113633291b0c0c01006de
nick <nikita.kolymazhnov@gmail.com>
2024-11-01 15:39:37 +0300

    Feature/yh 501 (#228)
    * YH-501: add button to add Keywords
    
    * YH-501: add translation

--- src/features/skill/editSkill/model/lib/validation/skillEditSchema.ts
+++ src/features/skill/editSkill/model/lib/validation/skillEditSchema.ts
@@ -3,8 +3,8 @@ import * as yup from 'yup';
 export const skillEditSchema = yup.object().shape({
 	id: yup.number().required(),
 	title: yup.string().required(),
-	description: yup.string().required(),
-	imageSrc: yup.string().required(),
-	createdAt: yup.string().required(),
-	updatedAt: yup.string().required(),
+	description: yup.string(),
+	imageSrc: yup.string(),
+	createdAt: yup.string(),
+	updatedAt: yup.string(),
 });
