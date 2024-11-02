ce6046153c1a6c884a18834e0498d6f6c5dc215e
Maxim <84065692+makcimishevskih@users.noreply.github.com>
2024-11-02 14:16:08 +0300

    YH-487: transfer Button component from UiKit (#221)
    * YH-487: transfer Button component from UiKit
    
    * YH-487: css-vars to button,refactor
    
    * YH-487: add: getter function,prop destructive , remove: tagName prop

---
+++ src/shared/ui/Button/ui/Button.tsx
@@ -0,0 +1,50 @@
+import classnames from 'classnames';
+import React, { forwardRef } from 'react';
+
+import styles from './Button.module.css';
+import { getTagName } from './model/helpers';
+import { ButtonProps } from './model/types';
+
+export const Button = forwardRef<HTMLButtonElement | HTMLAnchorElement, ButtonProps>(
+	(
+		{
+			size = 'M',
+			variant = 'primary',
+			className = '',
+			fullWidth = false,
+			destructive = false,
+			children,
+			preffix,
+			suffix,
+			badge,
+			...props
+		},
+		ref,
+	): JSX.Element => {
+		const tagName = getTagName(variant);
+
+		const Component = tagName as React.ElementType;
+
+		return (
+			<Component
+				ref={ref}
+				className={classnames(
+					styles[tagName],
+					fullWidth ? styles['button-full'] : styles[`${tagName}-${size.toLowerCase()}`],
+					destructive && tagName === 'a'
+						? styles['a-link-destructive']
+						: styles[`${tagName}-${variant}`],
+					className,
+				)}
+				{...props}
+			>
+				{preffix}
+				{children}
+				{suffix}
+				{badge && badge !== '0' ? <div className={styles['button-badge']}>{badge}</div> : null}
+			</Component>
+		);
+	},
+);
+
+Button.displayName = 'Button';
