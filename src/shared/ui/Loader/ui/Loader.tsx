e215535105d8eb259ea45722bb18724431689b43
AndreyZhivilo <stargazer2017@yandex.ru>
2024-11-03 19:10:16 +0300

    YH-494: only files changed left

--- src/shared/ui/Loader/ui/Loader.tsx
+++ src/shared/ui/Loader/ui/Loader.tsx
@@ -1,17 +1,24 @@
+import { CSSProperties } from 'react';
+
 import { useI18nHelpers } from '@/shared/hooks/useI18nHelpers';
 import { Card } from '@/shared/ui/Card';
 
 import styles from './Loader.module.css';
 
-export const Loader = () => {
+interface LoaderProps {
+	hasText?: boolean;
+	style?: CSSProperties;
+}
+
+export const Loader = ({ hasText = true, style }: LoaderProps) => {
 	const { t } = useI18nHelpers();
 
 	return (
-		<div className={styles.wrapper}>
+		<div className={styles.wrapper} style={style}>
 			<Card className={styles.block}>
 				<div className={styles.content}>
 					<span className={styles.loader}></span>
-					<span className={styles.text}>{t('loading')}</span>
+					{hasText && <span className={styles.text}>{t('loading')}</span>}
 				</div>
 			</Card>
 		</div>
