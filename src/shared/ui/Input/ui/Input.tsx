f48dbba075b04c19b1a321da93d2d00b0e5c39d2
Fr1Day12 <almaz9061198856@gmail.com>
2024-11-07 12:17:12 +0300

    YH-489: add magnifier svg and aria labels

--- src/shared/ui/Input/ui/Input.tsx
+++ src/shared/ui/Input/ui/Input.tsx
@@ -1,45 +1,77 @@
 import classNames from 'classnames';
+import { useRef } from 'react';
 
+import Magnifer from '@/shared/assets/icons/Magnifer.svg';
 import { Size } from '@/shared/ui/Input/model/types/InputTypes';
 
 import styles from './Input.module.css';
 
-interface InputProps {
-	prefix?: React.ReactNode;
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+	customPrefix?: React.ReactNode;
 	suffix?: React.ReactNode;
-	size?: Size;
+	customSize?: Size;
 	className?: string;
 	disabled?: boolean;
 	placeholder?: string;
 	error?: boolean;
+	label?: string;
 }
 
 export const Input = ({
-	prefix,
+	customPrefix,
 	suffix,
-	size = 'L',
+	customSize = 'L',
 	disabled = false,
 	className,
 	placeholder = '',
 	error = false,
+	label = '',
 	...props
 }: InputProps) => {
+	const inputRef = useRef<HTMLInputElement>(null);
+
 	const wrapperClasses = classNames(
 		styles.wrapper,
-		{ [styles['wrapper-disabled']]: disabled },
-		{ [styles['wrapper-error']]: error },
-		{ [styles[`wrapper-${size.toLowerCase()}`]]: size },
+		{
+			[styles['wrapper-disabled']]: disabled,
+			[styles['wrapper-error']]: error,
+			[styles[`wrapper-${customSize.toLowerCase()}`]]: customSize,
+		},
+		className,
 	);
 
+	const handleClick = () => {
+		if (inputRef.current) inputRef.current.focus();
+	};
+
+	const handleKeyDown = (e: React.KeyboardEvent<HTMLDivElement>) => {
+		if (e.key === 'Enter' || e.key === ' ') {
+			handleClick();
+		}
+	};
+
 	return (
-		<div className={wrapperClasses}>
-			{prefix && <span className={styles['input-prefix']}>{prefix}</span>}
+		<div
+			className={wrapperClasses}
+			onClick={handleClick}
+			onFocus={handleClick}
+			onKeyDown={handleKeyDown}
+			role="button"
+			tabIndex={disabled ? -1 : 0}
+		>
+			{
+				<span className={styles['input-prefix']}>
+					{customPrefix || <Magnifer className={styles.prefix} />}
+				</span>
+			}
 			<input
+				ref={inputRef}
 				placeholder={placeholder}
-				tabIndex={disabled ? -1 : 0}
 				type="text"
-				className={classNames(styles.input, className)}
+				className={styles.input}
 				disabled={disabled}
+				aria-invalid={error}
+				aria-labelledby={label}
 				{...props}
 			/>
 			{suffix && <span className={styles['input-suffix']}>{suffix}</span>}
