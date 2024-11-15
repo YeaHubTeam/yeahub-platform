744eba6efa4264d00aecd99d159f51231000b708
Nikita Kolymazhnov <nikita.kolymazhnov@gmail.com>
2024-11-15 12:54:56 +0300

    YH-546: add story to Accordion

---
+++ src/shared/ui/Accordion/ui/Accordion.stories.tsx
@@ -0,0 +1,29 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Accordion } from './Accordion';
+
+const meta = {
+	title: 'shared/ui/Accordion',
+	component: Accordion,
+	parameters: {
+		layout: 'centered',
+	},
+	tags: ['autodocs'],
+	argTypes: {},
+} satisfies Meta<typeof Accordion>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const WithChildren: Story = {
+	args: {
+		title: 'Как выглядит базовая структура HTML страницы?',
+		children: <div>Базовая структура состоит из DOCTYPE, html, head, body</div>,
+	},
+};
+
+export const WithoutChildren: Story = {
+	args: {
+		title: 'Как выглядит базовая структура HTML страницы?',
+	},
+};
--- src/shared/ui/Accordion/ui/Accordion.tsx
+++ src/shared/ui/Accordion/ui/Accordion.tsx
@@ -5,10 +5,22 @@ import Arrow from '@/shared/assets/icons/arrow.svg';
 import styles from './Accordion.module.css';
 
 interface AccordionProps {
+	/**
+	 *The part of the content that remains visible when closed
+	 */
 	title: string;
+	/**
+	 *The part of content that appear when open
+	 */
 	children?: ReactNode;
 }
 
+/**
+ * This component can hide and show the content
+ * @param title
+ * @param children
+ * @constructor
+ */
 export const Accordion = ({ title, children }: AccordionProps) => {
 	const [isOpen, setisOpen] = useState(false);
 	const contentRef = useRef<HTMLDivElement>(null);
