024e5504dbca90277083838b0d5886fdf8723530
Maxim <84065692+makcimishevskih@users.noreply.github.com>
2024-11-17 22:34:39 +0300

    YH-555: !isEmailVerified block btns, refactor p nesting error (#290)

--- src/shared/ui/TextHtml/ui/TextHtml.tsx
+++ src/shared/ui/TextHtml/ui/TextHtml.tsx
@@ -1,16 +1,18 @@
+import classNames from 'classnames';
 import DOMPurify from 'dompurify';
 
 import styles from './TextHtml.module.css';
 
 interface TextHtmlProps {
 	html: string;
+	className?: string;
 }
 
-export const TextHtml = ({ html }: TextHtmlProps) => {
+export const TextHtml = ({ className, html }: TextHtmlProps) => {
 	const sanitizedHtmlContent = DOMPurify.sanitize(html);
 
 	return (
-		<pre className={styles.text}>
+		<pre className={classNames(styles.text, className)}>
 			<div dangerouslySetInnerHTML={{ __html: sanitizedHtmlContent }} />
 		</pre>
 	);
