5e96485ed8311b4448045b935641be0e0b5724ee
Denis Pereloma <den.pereloma@gmail.com>
2024-11-20 14:06:59 +0300

    YH-582: add logout, settings to sidebar

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -1,6 +1,7 @@
 import classNames from 'classnames';
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
+import ChatIcon from '@/shared/assets/icons/chat.svg';
 import LeftChevron from '@/shared/assets/icons/leftChevron.svg';
 import { i18Namespace } from '@/shared/config/i18n';
 import { A11y } from '@/shared/config/i18n/i18nTranslations';
@@ -10,6 +11,9 @@ import { useScreenSize } from '@/shared/hooks/useScreenSize';
 import { AppLogo } from '@/shared/ui/AppLogo';
 import { Button } from '@/shared/ui/Button';
 import { Flex } from '@/shared/ui/Flex';
+import { SignOutIcon } from '@/shared/ui/Icons/SignOutIcon';
+
+import { useLazyLogoutQuery } from '@/entities/auth';
 
 import { MenuItem } from '../../model/types/sidebar';
 import { SidebarMenuList } from '../SidebarMenuList/SidebarMenuList';
@@ -36,6 +40,7 @@ export const Sidebar = ({ menuItems, isMobileSidebar = false }: SidebarProps) =>
 	const { isMobile } = useScreenSize();
 	const { t } = useI18nHelpers([i18Namespace.translation, i18Namespace.a11y]);
 	const [isOpenNavSidebar, setIsOpenNavSidebar] = useState<boolean>(false);
+	const [logout] = useLazyLogoutQuery();
 
 	useEffect(() => {
 		if (!isMobileSidebar) {
@@ -48,6 +53,8 @@ export const Sidebar = ({ menuItems, isMobileSidebar = false }: SidebarProps) =>
 	};
 
 	const openSupportTab = () => window.open('https://t.me/yeahub_support', '_blank');
+
+	const onLogout = () => logout();
 
 	return (
 		<aside
@@ -76,15 +83,29 @@ export const Sidebar = ({ menuItems, isMobileSidebar = false }: SidebarProps) =>
 				<div className={styles.menu}>
 					<SidebarMenuList fullWidth={isOpenNavSidebar} menuItems={menuItems} />
 				</div>
-				<Button
-					className={classNames(styles['support-button'], {
-						[styles['support-button-hide']]: isOpenNavSidebar,
-					})}
-					size="L"
-					onClick={openSupportTab}
-				>
-					{t(Translation.SUPPORT, { ns: i18Namespace.translation })}
-				</Button>
+				<Flex direction="column" gap="8" className={styles['bottom-actions']}>
+					<Button
+						className={classNames(styles['sidebar-bottom-button'], {
+							[styles['sidebar-bottom-button-hide']]: isOpenNavSidebar,
+						})}
+						size="L"
+						onClick={openSupportTab}
+						preffix={<ChatIcon />}
+					>
+						<span>{t(Translation.SUPPORT, { ns: i18Namespace.translation })}</span>
+					</Button>
+					<Button
+						className={classNames(styles['sidebar-bottom-button'], {
+							[styles['sidebar-bottom-button-hide']]: isOpenNavSidebar,
+						})}
+						size="L"
+						onClick={onLogout}
+						preffix={<SignOutIcon isCurrentColor />}
+						variant="destructive"
+					>
+						<span>{t(Translation.USERPREFERENCES_LOGOUT, { ns: i18Namespace.translation })}</span>
+					</Button>
+				</Flex>
 			</Flex>
 		</aside>
 	);
