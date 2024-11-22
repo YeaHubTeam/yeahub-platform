9d70a47fd1d1b14730ee46111add10420b4e9e8e
Vitaliy Shevtsov <57325046+vvs-jkr@users.noreply.github.com>
2024-11-22 18:14:21 +0300

    YH-545: fix validation errors on questions page (#315)
    * YH-545: fix validation errors on questions page
    
    * YH-545: fixed validation keywords

--- src/features/question/createQuestion/model/lib/validation/questionCreateSchema.ts
+++ src/features/question/createQuestion/model/lib/validation/questionCreateSchema.ts
@@ -1,11 +1,14 @@
 import * as yup from 'yup';
 
+import i18n from '@/shared/config/i18n/i18n';
+import { Translation } from '@/shared/config/i18n/i18nTranslations';
+
 export const questionCreateSchema = yup.object().shape({
 	status: yup.string(),
-	title: yup.string().required(),
-	description: yup.string().required(),
-	shortAnswer: yup.string().required(),
-	longAnswer: yup.string().required(),
+	title: yup.string().required(i18n.t(Translation.VALIDATION_REQUIRED)),
+	description: yup.string().required(i18n.t(Translation.VALIDATION_REQUIRED)),
+	shortAnswer: yup.string().required(i18n.t(Translation.VALIDATION_REQUIRED)),
+	longAnswer: yup.string().required(i18n.t(Translation.VALIDATION_REQUIRED)),
 	imageSrc: yup.string(),
 	rate: yup
 		.number()
@@ -15,6 +18,11 @@ export const questionCreateSchema = yup.object().shape({
 		.number()
 		.transform((value) => (Number.isNaN(value) ? null : value))
 		.required(),
-	skills: yup.array().required(),
-	specializations: yup.array().required(),
+	skills: yup.array().required(i18n.t(Translation.VALIDATION_SKILLS_REQUIRED)),
+	specializations: yup.array().required(i18n.t(Translation.VALIDATION_SPECIALIZATION_REQUIRED)),
+	keywords: yup
+		.array()
+		.of(yup.string().required(i18n.t(Translation.VALIDATION_REQUIRED)))
+		.min(1, i18n.t(Translation.VALIDATION_REQUIRED))
+		.required(i18n.t(Translation.VALIDATION_REQUIRED)),
 });
