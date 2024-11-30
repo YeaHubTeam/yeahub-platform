1e22306bcba190266d946e1c5851ebc71da63a46
nick <nikita.kolymazhnov@gmail.com>
2024-11-30 15:01:34 +0300

    Feature/yh 596 (#340)
    * YH-596: add adaptive for interview result page
    
    * YH-596: add scroll to questions list
    
    * YH-596: fix diagram fontSize

--- src/widgets/Charts/ui/PassedInterviewStat/PassedInterviewStat.tsx
+++ src/widgets/Charts/ui/PassedInterviewStat/PassedInterviewStat.tsx
@@ -1,5 +1,6 @@
 import ReactECharts from 'echarts-for-react';
 
+import { useScreenSize } from '@/shared/hooks/useScreenSize';
 import { AttemptInfoItemProps, AttemptInfoItem } from '@/shared/ui/AttemptInfoItem';
 
 import { options } from '../../model/constants/options';
@@ -22,6 +23,9 @@ export const PassedInterviewStat = ({
 	pieOption.series[0].padAngle = attemptData.some((item) => item.value === 100) ? 0 : 15;
 	pieOption.series[0].data = attemptData;
 
+	const { isMobile } = useScreenSize();
+	const fontSize = isMobile ? 'var(--font-size-p-s)' : 'var(--font-size-p-l)';
+
 	return (
 		<div className={styles.wrapper}>
 			<ReactECharts
@@ -33,7 +37,7 @@ export const PassedInterviewStat = ({
 					borderRadius: '50%',
 					width: 'clamp(192px, calc(192px + 9.98vw - 57.6px), 307px)',
 					height: 'clamp(192px, calc(192px + 9.98vw - 57.6px), 307px)',
-					fontSize: 'var(--font-size-p-l)',
+					fontSize,
 					fontWeight: 'var(--font-weight-600)',
 					color: 'var(--text-color-light)',
 				}}
