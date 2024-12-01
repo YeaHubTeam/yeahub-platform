db15744794d843d33a7dee2374aa9589caba2c54
Sabina <gareevasaab@mail.ru>
2024-12-01 14:41:20 +0500

    YH-597: сalendar component is wrapped in a popover

--- src/shared/ui/Calendar/ui/EventCalendar.tsx
+++ src/shared/ui/Calendar/ui/EventCalendar.tsx
@@ -1,14 +1,16 @@
-import { useEffect, useState } from 'react';
 import { Calendar } from 'react-calendar';
 import { Icon } from 'yeahub-ui-kit';
 
-import { Card } from '../../Card';
 import './EventCalendar.css';
 
 import 'react-calendar/dist/Calendar.css';
 import { i18Namespace } from '@/shared/config/i18n';
 import { A11y } from '@/shared/config/i18n/i18nTranslations';
 import { useI18nHelpers } from '@/shared/hooks/useI18nHelpers';
+import { IconButton } from '@/shared/ui/IconButton';
+import { Popover } from '@/shared/ui/Popover';
+
+import { Card } from '../../Card';
 
 type ValuePiece = Date | null;
 export type Value = ValuePiece | [ValuePiece, ValuePiece];
@@ -19,7 +21,6 @@ interface EventCalendarProps {
 }
 
 export const EventCalendar = ({ onDateChange, selectedDates }: EventCalendarProps) => {
-	const [isOpenCalendar, setOpenCalendar] = useState<boolean>(false);
 	const { t } = useI18nHelpers(i18Namespace.a11y);
 
 	const PREV_LABEL = (
@@ -39,33 +40,52 @@ export const EventCalendar = ({ onDateChange, selectedDates }: EventCalendarProp
 		/>
 	);
 
-	const handleClick = (): void => {
-		setOpenCalendar((prev) => !prev);
-	};
-
-	useEffect(() => {
-		if (selectedDates) {
-			setOpenCalendar((prev) => !prev);
-		}
-	}, [selectedDates]);
-
 	return (
 		<>
-			<button className="mobile-calendar" onClick={handleClick}>
-				<Icon icon="calendar" color="--palette-ui-black-700" />
-			</button>
-			<Card className={isOpenCalendar ? 'calendar-block  active' : 'calendar-block '}>
-				<Calendar
-					onChange={onDateChange}
-					showNeighboringMonth={false}
-					prevLabel={PREV_LABEL}
-					nextLabel={NEXT_LABEL}
-					prev2Label={null}
-					next2Label={null}
-					selectRange={true}
-					value={selectedDates}
-				/>
-			</Card>
+			<div className="popover-additional">
+				<Popover
+					body={
+						<Card className={'calendar-block '}>
+							<Calendar
+								onChange={onDateChange}
+								showNeighboringMonth={false}
+								prevLabel={PREV_LABEL}
+								nextLabel={NEXT_LABEL}
+								prev2Label={null}
+								next2Label={null}
+								selectRange={true}
+								value={selectedDates}
+							/>
+						</Card>
+					}
+				>
+					{({ isOpen, onToggle }) => (
+						<IconButton
+							className={isOpen ? 'active' : ''}
+							aria-label="go to filters"
+							form="square"
+							icon={<Icon icon="calendar" />}
+							size="S"
+							variant={'tertiary'}
+							onClick={onToggle}
+						/>
+					)}
+				</Popover>
+			</div>
+			<div className={'additional-info-wrapper'}>
+				<Card className={'calendar-block'}>
+					<Calendar
+						onChange={onDateChange}
+						showNeighboringMonth={false}
+						prevLabel={PREV_LABEL}
+						nextLabel={NEXT_LABEL}
+						prev2Label={null}
+						next2Label={null}
+						selectRange={true}
+						value={selectedDates}
+					/>
+				</Card>
+			</div>
 		</>
 	);
 };
