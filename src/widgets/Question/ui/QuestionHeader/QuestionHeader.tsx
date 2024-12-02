814aa7e1031e8d94b861e5c35bc96d3744b1e467
Vitaliy Shevtsov <57325046+vvs-jkr@users.noreply.github.com>
2024-12-02 18:06:56 +0300

    YH-593: update adaptive for a detailed question (#332)
    * YH-593: update adaptive for a detailed question
    
    * YH-593: add popover menu
    
    * YH-593: rerurn sidebar height
    
    * YH-593: fix adaptive for a detailed question
    
    * YH-593: fix popover
    
    * YH-593: fix popover style
    
    * YH-593: fix popover
    
    * YH-593: fix ImageWithWrapper
    
    * YH-593: fix question image
    
    * YH-593: fix ImageWithWrapper
    
    * YH-593: fix ImageWithWrapper on Interview page
    
    * YH-593: fix ImageWithWrapper style

--- src/widgets/Question/ui/QuestionHeader/QuestionHeader.tsx
+++ src/widgets/Question/ui/QuestionHeader/QuestionHeader.tsx
@@ -1,3 +1,4 @@
+import { useScreenSize } from '@/shared/hooks/useScreenSize';
 import { Card } from '@/shared/ui/Card';
 import { ImageWithWrapper } from '@/shared/ui/ImageWithWrapper';
 
@@ -12,19 +13,31 @@ interface QuestionHeaderProps {
 }
 
 export const QuestionHeader = ({ title, description, status }: QuestionHeaderProps) => {
+	const { isDesktop, isMobile } = useScreenSize();
+
+	const imageClassName = isMobile ? styles['image-mobile'] : styles['image-default'];
+
 	return (
 		<Card withOutsideShadow>
 			<div className={styles['question-header-wrapper']}>
-				<div className={styles['image-wrapper']}>
-					<ImageWithWrapper className={styles.image} />
-				</div>
+				{isDesktop ? (
+					<div
+						className={`${styles['image-wrapper']} ${
+							isMobile ? styles['image-wrapper-mobile'] : ''
+						}`}
+					>
+						<ImageWithWrapper className={imageClassName} src={''} />
+					</div>
+				) : null}
 				<div className={styles['title-wrapper']}>
 					<h2 className={styles.title}>{title}</h2>
 					<p className={styles.description}>{description}</p>
 				</div>
-				<div className={styles['label-wrapper']}>
-					<p className={styles.label}>{questionStatuses[status]}</p>
-				</div>
+				{isDesktop && (
+					<div className={styles['label-wrapper']}>
+						<p className={styles.label}>{questionStatuses[status]}</p>
+					</div>
+				)}
 			</div>
 		</Card>
 	);
