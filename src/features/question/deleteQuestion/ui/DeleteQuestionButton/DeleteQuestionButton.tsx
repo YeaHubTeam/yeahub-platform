d081424e1358a6155f25ec1e3c23bc3940fc0ca2
Danil Sveshnikov <bloodshoww@mail.ru>
2024-12-11 17:16:33 +0300

    feature/YH-611 (#371)
    * YH-611: added blocker in question, skill and specialization
    
    * YH-611: fix blocker
    
    * YH-611: fixed the components
    
    * YH-611: fixed constants

--- src/features/question/deleteQuestion/ui/DeleteQuestionButton/DeleteQuestionButton.tsx
+++ src/features/question/deleteQuestion/ui/DeleteQuestionButton/DeleteQuestionButton.tsx
@@ -1,16 +1,15 @@
-import classNames from 'classnames';
+import { useState } from 'react';
 import { Icon } from 'yeahub-ui-kit';
 
 import { i18Namespace } from '@/shared/config/i18n';
 import { Translation } from '@/shared/config/i18n/i18nTranslations';
 import { useI18nHelpers } from '@/shared/hooks/useI18nHelpers';
+import { BlockerDialog } from '@/shared/ui/BlockerDialogModal';
 import { Button } from '@/shared/ui/Button';
 
 import { Question } from '@/entities/question';
 
 import { useDeleteQuestionMutation } from '../../api/deleteQuestionApi';
-
-import styles from './DeleteQuestionButton.module.css';
 
 interface DeleteQuestionButtonProps {
 	questionId: Question['id'];
@@ -22,21 +21,36 @@ export const DeleteQuestionButton = ({
 	isDetailPage = false,
 }: DeleteQuestionButtonProps) => {
 	const [deleteQuestionMutation] = useDeleteQuestionMutation();
+  
 	const { t } = useI18nHelpers(i18Namespace.translation);
+	const [isDeleteModalOpen, setIsModalOpen] = useState(false);
+
+	const onCloseDeleteModal = () => {
+		setIsModalOpen((prev) => !prev);
+	};
 
 	const onDeleteQuestion = async () => {
 		await deleteQuestionMutation(questionId);
 	};
 
 	return (
-		<Button
-			aria-label="Delete"
-			className={classNames({ [styles['detail-button']]: !isDetailPage })}
-			preffix={!isDetailPage && <Icon icon="trash" size={24} color="--palette-ui-red-600" />}
-			variant={isDetailPage ? 'destructive' : 'tertiary'}
-			onClick={onDeleteQuestion}
-		>
-			{t(Translation.DELETE)}
-		</Button>
+		<>
+			<Button
+				aria-label="Large"
+				style={{ width: 'auto', justifyContent: isDetailPage ? 'center' : 'flex-start' }}
+				preffix={!isDetailPage && <Icon icon="trash" size={20} color="--palette-ui-red-600" />}
+				variant={isDetailPage ? 'destructive' : 'tertiary'}
+				onClick={onCloseDeleteModal}
+			>
+				{t(Translation.DELETE)}
+			</Button>
+			{isDeleteModalOpen && (
+				<BlockerDialog
+					onOk={onDeleteQuestion}
+					onCancel={() => setIsModalOpen(false)}
+					message={Translation.BLOCKMODAL_CONFIRM_DELETE}
+				/>
+			)}
+		</>
 	);
 };
--- src/features/skill/deleteSkill/ui/DeleteSkillButton/DeleteSkillButton.tsx
+++ src/features/skill/deleteSkill/ui/DeleteSkillButton/DeleteSkillButton.tsx
@@ -1,42 +1,61 @@
-import classNames from 'classnames';
+import { useState } from 'react';
 import { Icon } from 'yeahub-ui-kit';
 
 import { i18Namespace } from '@/shared/config/i18n';
 import { Translation } from '@/shared/config/i18n/i18nTranslations';
 import { useI18nHelpers } from '@/shared/hooks/useI18nHelpers';
+import { BlockerDialog } from '@/shared/ui/BlockerDialogModal';
 import { Button } from '@/shared/ui/Button';
 
 import { Skill } from '@/entities/skill';
 
 import { useDeleteSkillMutation } from '../../api/deleteSkillApi';
 
-import styles from './DeleteSkillButton.module.css';
-
 interface DeleteSkillButtonProps {
 	skillId: Skill['id'];
 	isDetailPage?: boolean;
 }
 
-export const DeleteSkillButton = ({ skillId, isDetailPage = false }: DeleteSkillButtonProps) => {
+export const DeleteSkillButton = ({ 
+  skillId, 
+  isDetailPage = false, 
+}: DeleteSkillButtonProps) => {
 	const [deleteSkillMutation] = useDeleteSkillMutation();
 
 	const { t } = useI18nHelpers(i18Namespace.translation);
+  const [isDeleteModalOpen, setIsModalOpen] = useState(false);
+
+	const onCloseDeleteModal = () => {
+		setIsModalOpen((prev) => !prev);
+	};
 
 	const onDeleteSkill = async () => {
 		await deleteSkillMutation(skillId);
 	};
 
 	return (
-		<Button
-			aria-label="Delete"
-			className={classNames({ [styles['detail-button']]: !isDetailPage })}
-			preffix={
-				!isDetailPage ? <Icon icon="trash" size={24} color="--palette-ui-red-600" /> : undefined
-			}
-			variant={isDetailPage ? 'destructive' : 'tertiary'}
-			onClick={onDeleteSkill}
-		>
-			{t(Translation.DELETE)}
-		</Button>
+		<>
+			<Button
+				aria-label="Large"
+				style={{
+					width: isDetailPage ? 'auto' : '100%',
+					justifyContent: isDetailPage ? 'center' : 'flex-start',
+				}}
+				variant={isDetailPage ? 'destructive' : 'tertiary'}
+				onClick={onCloseDeleteModal}
+				preffix={
+					isDetailPage ? undefined : <Icon icon="trash" size={20} color="--palette-ui-red-600" />
+				}
+			>
+				{t(Translation.DELETE)}
+			</Button>
+			{isDeleteModalOpen && (
+				<BlockerDialog
+					onOk={onDeleteSkill}
+					onCancel={() => setIsModalOpen(false)}
+					message={Translation.BLOCKMODAL_CONFIRM_DELETE}
+				/>
+			)}
+		</>
 	);
 };
