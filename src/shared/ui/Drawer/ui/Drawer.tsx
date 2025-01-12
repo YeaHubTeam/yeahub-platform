947c99ab5804bd4a4394b40fd8895b9586316077
Wayface <wayfacee@gmail.com>
2025-01-13 00:32:04 +0600

    [fix/YH-683] - Drawer problem

--- src/shared/ui/Drawer/ui/Drawer.tsx
+++ src/shared/ui/Drawer/ui/Drawer.tsx
@@ -35,22 +35,24 @@ export const Drawer = ({
 	const portalRootRef = useRef(document.getElementById('drawer-root') || createPortalRoot());
 	const documentRootName = rootName === 'mainLayout' ? 'main' : 'body';
 	const renderRootRef = useRef(document.querySelector(documentRootName)!);
+	const rootEl = renderRootRef.current;
 
 	useEffect(() => {
-		const mainContainer = document.querySelector('#main-container') as HTMLElement;
-		renderRootRef.current.style.overflow = isOpen ? 'hidden' : '';
-		mainContainer.style.paddingRight = isOpen ? '8px' : ''; // 8px = scrollbar width
+		rootEl.style.overflow = isOpen ? 'hidden' : '';
+
+		return () => {
+			rootEl.style.overflow = '';
+		};
 	}, [isOpen]);
 
 	useEffect(() => {
 		if (isOpen) {
-			renderRootRef.current.appendChild(portalRootRef.current);
+			rootEl.appendChild(portalRootRef.current);
 			const portal = portalRootRef.current;
-			const bodyEl = renderRootRef.current;
 
 			return () => {
 				portal.remove();
-				bodyEl.style.overflow = '';
+				rootEl.style.overflow = '';
 			};
 		}
 	}, [isOpen]);
