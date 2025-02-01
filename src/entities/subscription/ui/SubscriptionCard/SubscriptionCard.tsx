86be06509070625830e3e2d23834aa1a4d7a00a8
nick <nikita.kolymazhnov@gmail.com>
2025-02-01 15:35:25 +0300

    YH-699: add subscriptionCards widget (#422)
    * YH-699: add subscriptionCards widget
    
    * YH-699: refactoring

--- src/entities/subscription/ui/SubscriptionCard/SubscriptionCard.tsx
+++ src/entities/subscription/ui/SubscriptionCard/SubscriptionCard.tsx
@@ -1,11 +1,13 @@
+import classNames from 'classnames';
 import { useTranslation } from 'react-i18next';
 
 import IconCheck from '@/shared/assets/icons/iconCheck.svg';
 import { i18Namespace } from '@/shared/config/i18n';
 import { SubscriptionCard as SubscriptionCardI18 } from '@/shared/config/i18n/i18nTranslations';
+import { useScreenSize } from '@/shared/hooks/useScreenSize';
+import { Card } from '@/shared/ui/Card';
 import { Flex } from '@/shared/ui/Flex';
-
-import { SubscribeButton } from '@/features/subscription/SubscribeButton/ui/SubscribeButton';
+import { Text } from '@/shared/ui/Text';
 
 import { Subscription } from '../../model/types/subscription';
 
@@ -13,32 +15,73 @@ import styles from './SubscriptionCard.module.css';
 
 interface SubscriptionCardProps {
 	subscription: Subscription;
+	className?: string;
+	renderSubscribeButton: () => React.ReactNode;
 }
 
-export const SubscriptionCard = ({ subscription }: SubscriptionCardProps) => {
+export const SubscriptionCard = ({
+	subscription,
+	className,
+	renderSubscribeButton,
+}: SubscriptionCardProps) => {
 	const { t } = useTranslation(i18Namespace.subscriptionCard);
+	const { isMobile, isTablet } = useScreenSize();
+
+	const titleVariant = isMobile ? 'body4' : isTablet ? 'body5-accent' : 'body6';
+	const actualPrice = subscription.discountedPrice
+		? subscription.discountedPrice
+		: subscription.price;
 
 	return (
-		<div className={styles['subscription']}>
-			<Flex gap="8" className={styles['subscription-header']}>
-				<subscription.icon style={{ width: '39px', height: '39px' }} />
-				<p className={styles['subscription-name']}>{subscription.name}</p>
-			</Flex>
-			<p className={styles['subscription-description']}>{subscription.description}</p>
-			<p className={styles['subscription-price']}>
-				{subscription.price === 0
-					? t(SubscriptionCardI18.SUBSCRIPTION_CARD_FREE)
-					: t(SubscriptionCardI18.SUBSCRIPTION_CARD_PRICE, { price: subscription.price })}
-			</p>
-			<SubscribeButton className={styles['subscription-button']} />
-			<Flex componentType="ul" direction="column" gap="14">
-				{subscription.advantages.map((advantage) => (
-					<Flex key={advantage} align="center" gap="8">
-						<IconCheck className={styles['subscription-check']} />
-						{advantage}
+		<Card withOutsideShadow className={classNames(styles['subscription'], className)}>
+			<Flex direction="column" gap="14">
+				<Flex direction="column" gap="14">
+					<Flex gap="8" className={styles['subscription-header']}>
+						{subscription.icon}
+						<Text variant={titleVariant} className={styles['subscription-name']}>
+							{subscription.name}
+						</Text>
 					</Flex>
-				))}
+					<Text variant="body2" className={styles['subscription-description']}>
+						{subscription.description}
+					</Text>
+				</Flex>
+				<Flex gap="8" align="center" className={styles['subscription-price-container']}>
+					<Text variant="body3-accent" className={styles['subscription-price']}>
+						{t(SubscriptionCardI18.SUBSCRIPTION_CARD_PRICE, { price: actualPrice })}
+					</Text>
+					{subscription.discountedPrice && (
+						<Text variant="body2" className={styles['crossed-out-price']}>
+							{t(SubscriptionCardI18.SUBSCRIPTION_CARD_PRICE, { price: subscription.price })}
+						</Text>
+					)}
+				</Flex>
 			</Flex>
-		</div>
+			<Flex
+				className={styles['advantages-and-button-container']}
+				align={isMobile || isTablet ? 'normal' : 'start'}
+			>
+				<Flex componentType="ul" gap="14" wrap="wrap" className={styles['advantages-list']}>
+					{subscription.advantages.map((advantage) => (
+						<Flex key={advantage.title} align="center" gap="8" className={styles.advantage}>
+							<IconCheck
+								className={
+									advantage.isActive
+										? styles['subscription-check-active']
+										: styles['subscription-check-inactive']
+								}
+							/>
+							<Text
+								variant="body2"
+								className={classNames({ [styles['crossed-out-text']]: !advantage.isActive })}
+							>
+								{advantage.title}
+							</Text>
+						</Flex>
+					))}
+				</Flex>
+				{subscription.hasSubscribeButton && renderSubscribeButton()}
+			</Flex>
+		</Card>
 	);
 };
---
+++ src/features/subscription/ui/AgreementForm/AgreementForm.tsx
@@ -0,0 +1,179 @@
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useForm, FormProvider } from 'react-hook-form';
+import { useTranslation } from 'react-i18next';
+
+import FreeSubIcon from '@/shared/assets/icons/free-sub.svg';
+import ProSubIcon from '@/shared/assets/icons/pro-sub.svg';
+import { i18Namespace } from '@/shared/config/i18n';
+import { SubscriptionCard as SubscriptionCardI18 } from '@/shared/config/i18n/i18nTranslations';
+import { ROUTES } from '@/shared/config/router/routes';
+import { useScreenSize } from '@/shared/hooks/useScreenSize';
+import { Checkbox } from '@/shared/ui/Checkbox';
+import { Flex } from '@/shared/ui/Flex';
+import { FormControl } from '@/shared/ui/FormControl';
+import { Text } from '@/shared/ui/Text';
+import { parseI18nText } from '@/shared/utils/parseI18nText';
+
+import { SubscriptionCard } from '@/entities/subscription';
+
+import { SubscriptionAgreeFormValues } from '../../model/types/subscriptionAgreeTypes';
+import { subscriptionAgreeSchema } from '../../model/validation/subscriptionAgreeSchema';
+import { SubscribeButton } from '../SubscribeButton';
+
+import styles from './AgreementForm.module.css';
+
+export const AgreementForm = () => {
+	const { t } = useTranslation(i18Namespace.subscriptionCard);
+	const { isMobile } = useScreenSize();
+
+	const subscriptionMethods = useForm<SubscriptionAgreeFormValues>({
+		resolver: yupResolver(subscriptionAgreeSchema),
+		mode: 'onTouched',
+	});
+
+	const subscriptions = [
+		{
+			id: 1,
+			icon: <FreeSubIcon className={styles['free-sub-icon']} />,
+			name: t(SubscriptionCardI18.SUBSCRIPTION_CARD_FREE_TITLE),
+			description: t(SubscriptionCardI18.SUBSCRIPTION_CARD_FREE_DESCRIPTION),
+			price: 0,
+			hasSubscribeButton: false,
+			advantages: [
+				{
+					title: t(SubscriptionCardI18.SUBSCRIPTION_CARD_ADVANTAGES_FIRST),
+					isActive: true,
+				},
+				{
+					title: t(SubscriptionCardI18.SUBSCRIPTION_CARD_ADVANTAGES_FOURTH_FREE),
+					isActive: true,
+				},
+				{
+					title: t(SubscriptionCardI18.SUBSCRIPTION_CARD_ADVANTAGES_SECOND),
+					isActive: false,
+				},
+				{
+					title: t(SubscriptionCardI18.SUBSCRIPTION_CARD_ADVANTAGES_THIRD),
+					isActive: false,
+				},
+			],
+		},
+		{
+			id: 2,
+			icon: <ProSubIcon className={styles['premium-sub-icon']} />,
+			name: t(SubscriptionCardI18.SUBSCRIPTION_CARD_PREMIUM_TITLE),
+			description: t(SubscriptionCardI18.SUBSCRIPTION_CARD_PREMIUM_DESCRIPTION),
+			price: 400,
+			discountedPrice: 200,
+			hasSubscribeButton: true,
+			advantages: [
+				{
+					title: t(SubscriptionCardI18.SUBSCRIPTION_CARD_ADVANTAGES_FIRST),
+					isActive: true,
+				},
+				{
+					title: t(SubscriptionCardI18.SUBSCRIPTION_CARD_ADVANTAGES_SECOND),
+					isActive: true,
+				},
+				{
+					title: t(SubscriptionCardI18.SUBSCRIPTION_CARD_ADVANTAGES_THIRD),
+					isActive: true,
+				},
+				{
+					title: t(SubscriptionCardI18.SUBSCRIPTION_CARD_ADVANTAGES_FOURTH_PAID),
+					isActive: true,
+				},
+			],
+		},
+	];
+
+	const { control } = subscriptionMethods;
+
+	const offerAgreementParts = parseI18nText(
+		t(SubscriptionCardI18.SUBSCRIPTION_CARD_PRIVACY_OFFER_AGREEMENT),
+	);
+	const consentParts = parseI18nText(t(SubscriptionCardI18.SUBSCRIPTION_CARD_PRIVACY_CONSENT));
+
+	return (
+		<FormProvider {...subscriptionMethods}>
+			<Flex direction="column">
+				<Flex
+					direction="column"
+					gap={isMobile ? '8' : '12'}
+					className={styles['subscription-info']}
+				>
+					<Text variant={isMobile ? 'body5-accent' : 'head3'} className={styles.title}>
+						{t(SubscriptionCardI18.SUBSCRIPTION_TITLE)}
+					</Text>
+					<Text variant={isMobile ? 'body2' : 'body3'}>
+						{t(SubscriptionCardI18.SUBSCRIPTION_DESCRIPTION)}
+					</Text>
+				</Flex>
+				<Flex gap="20" className={styles['subscription-cards']}>
+					<SubscriptionCard
+						subscription={subscriptions[0]}
+						renderSubscribeButton={() => (
+							<SubscribeButton id={subscriptions[0].id} className={styles['subscription-button']} />
+						)}
+						className={styles.free}
+					/>
+					<SubscriptionCard
+						subscription={subscriptions[1]}
+						renderSubscribeButton={() => (
+							<SubscribeButton id={subscriptions[1].id} className={styles['subscription-button']} />
+						)}
+						className={styles.premium}
+					/>
+				</Flex>
+				<Flex direction="column" gap="16">
+					<Text variant="body1" color="black-600">
+						{t(SubscriptionCardI18.SUBSCRIPTION_CARD_PRIVACY_TITLE)}
+					</Text>
+					<Flex direction="column" gap="8">
+						<FormControl name="isOfferAgreed" control={control} className={styles['form-control']}>
+							{(field) => (
+								<Checkbox
+									{...field}
+									label={
+										<Text variant="body1" color="black-600">
+											{offerAgreementParts[0]}
+											<a href={ROUTES.docs.page} target="_blank" rel="noopener noreferrer">
+												{' '}
+												{offerAgreementParts[1]}
+											</a>
+											{offerAgreementParts[2]}
+											<a href={ROUTES.docs.page} target="_blank" rel="noopener noreferrer">
+												{' '}
+												{offerAgreementParts[3]}
+											</a>
+										</Text>
+									}
+								/>
+							)}
+						</FormControl>
+						<FormControl
+							name="isConsentAgreed"
+							control={control}
+							className={styles['form-control']}
+						>
+							{(field) => (
+								<Checkbox
+									{...field}
+									label={
+										<Text variant="body1" color="black-600">
+											{consentParts[0]}
+											<a href={ROUTES.docs.page} target="_blank" rel="noopener noreferrer">
+												{' '}
+												{consentParts[1]}
+											</a>
+										</Text>
+									}
+								/>
+							)}
+						</FormControl>
+					</Flex>
+				</Flex>
+			</Flex>
+		</FormProvider>
+	);
+};
