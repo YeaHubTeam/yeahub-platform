85ddbb35941517cf9b45f5b25cd6458ce0f2fb1c
Osimjons <74949726+Osimjons@users.noreply.github.com>
2025-03-02 00:32:41 +0300

    YH-736: add User status label (#453)
    * YH-736: add User status label
    
    * YH-736: rename and relocate UserRolesList and etc.

--- src/widgets/Header/ui/UserPreferencesHeader/UserPreferencesHeader.tsx
+++ src/widgets/Header/ui/UserPreferencesHeader/UserPreferencesHeader.tsx
@@ -1,7 +1,11 @@
 import { useTranslation } from 'react-i18next';
 
+import ProSubIcon from '@/shared/assets/icons/pro-sub.svg';
+import { i18Namespace } from '@/shared/config/i18n';
 import { Translation } from '@/shared/config/i18n/i18nTranslations';
+import { User } from '@/shared/config/i18n/i18nTranslations';
 import { AvatarWithoutPhoto } from '@/shared/ui/AvatarWithoutPhoto';
+import { Button } from '@/shared/ui/Button';
 import { Flex } from '@/shared/ui/Flex';
 import { Text } from '@/shared/ui/Text';
 
@@ -9,9 +13,13 @@ import { useProfileQuery } from '@/entities/auth';
 
 import styles from './UserPreferencesHeader.module.css';
 
-export const UserPreferencesHeader = () => {
+interface UserPreferencesHeaderProps {
+	isPremiumUser: boolean;
+}
+
+export const UserPreferencesHeader = ({ isPremiumUser }: UserPreferencesHeaderProps) => {
 	const { data: profile, isSuccess: isSuccessGetProfile } = useProfileQuery();
-	const { t } = useTranslation();
+	const { t } = useTranslation(i18Namespace.user);
 
 	return (
 		<div className={styles.container}>
@@ -37,6 +45,13 @@ export const UserPreferencesHeader = () => {
 								{profile.email}
 							</Text>
 						</div>
+						<Button
+							className={styles[isPremiumUser ? 'candidate-premium' : 'candidate-free']}
+							fullWidth
+							preffix={isPremiumUser && <ProSubIcon className={styles['premium-icon']} />}
+						>
+							{t(isPremiumUser ? User.SUBSCRIPTION_PREMIUM : User.SUBSCRIPTION_FREE)}
+						</Button>
 					</Flex>
 				</>
 			)}
