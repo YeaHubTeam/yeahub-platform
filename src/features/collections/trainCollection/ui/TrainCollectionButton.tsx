f7b398a886255516dc9d9a945cb4a9ec469d101c
Dinysha <143814669+Dinysha@users.noreply.github.com>
2025-03-07 08:51:13 +0300

    YH-767: create a quiz for the collection

--- src/features/collections/trainCollection/ui/TrainCollectionButton.tsx
+++ src/features/collections/trainCollection/ui/TrainCollectionButton.tsx
@@ -1,14 +1,19 @@
 import { useTranslation } from 'react-i18next';
+import { useNavigate } from 'react-router-dom';
 import { Icon } from 'yeahub-ui-kit';
 
 import { i18Namespace } from '@/shared/config/i18n';
 import { Collections } from '@/shared/config/i18n/i18nTranslations';
+import { ROUTES } from '@/shared/config/router/routes';
 import { Button } from '@/shared/ui/Button';
+
+import { useLazyCreateNewQuizQuery } from '@/entities/quiz';
 
 import styles from './TrainCollectionButton.module.css';
 
 interface TrainCollectionProps {
 	collectionId?: string;
+	profileId: string;
 	isDisabled: boolean;
 	variant?: 'tertiary' | 'link-gray';
 	onSuccess?: () => void;
@@ -16,13 +21,29 @@ interface TrainCollectionProps {
 
 export const TrainCollectionButton = ({
 	collectionId,
+	profileId,
 	isDisabled,
 	variant = 'tertiary',
 }: TrainCollectionProps) => {
 	const { t } = useTranslation(i18Namespace.collection);
-	const handleTrainCollection = () => {
-		// eslint-disable-next-line no-console
-		console.log('Коллекция изучена успешно', collectionId);
+	const navigate = useNavigate();
+
+	const [createNewQuiz] = useLazyCreateNewQuizQuery();
+
+	const handleTrainCollection = async () => {
+		if (!collectionId || !profileId) return;
+
+		try {
+			await createNewQuiz({
+				profileId,
+				collection: Number(collectionId),
+				mode: 'RANDOM',
+			}).unwrap();
+			navigate(ROUTES.interview.new.page);
+		} catch (error) {
+			// eslint-disable-next-line no-console
+			console.error('Ошибка при создании квиза:', error);
+		}
 	};
 
 	return (
