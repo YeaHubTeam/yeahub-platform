37503c1c12f1bcb688e4a16d8c1299bac1f6eea6
Dmitriy Zakharov <dzaharov794@gmail.com>
2025-04-03 10:00:11 +0300

    YH-859: remove test onClick

--- src/shared/ui/Button/ui/Button.test.tsx
+++ src/shared/ui/Button/ui/Button.test.tsx
@@ -1,24 +1,13 @@
-import { fireEvent, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 
 import { renderComponent } from '@/shared/libs/jest/renderComponent/renderComponent';
 
 import { Button } from './Button';
 
 describe('Button', () => {
-	beforeEach(() => {
-		renderComponent(<Button />);
-	});
-
 	test('render', () => {
+		renderComponent(<Button />);
 		const button = screen.getByTestId('Button');
 		expect(button).toBeInTheDocument();
-	});
-
-	test('button handle click', () => {
-		const handleClick = jest.fn();
-		const button = screen.getByTestId('Button');
-
-		fireEvent.click(button);
-		expect(handleClick).toHaveBeenCalledTimes(1);
 	});
 });
