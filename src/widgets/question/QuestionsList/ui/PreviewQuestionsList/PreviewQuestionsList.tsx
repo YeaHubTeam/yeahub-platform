1c163452d4b900bbadce3a835c43b8a15ded12e1
Dmitriy Shilkin <160278878+Dima-Shilkin@users.noreply.github.com>
2025-04-26 16:15:30 +0300

    YH-826: fix aligned titles to the left (#592)

--- src/widgets/question/QuestionsList/ui/PreviewQuestionsList/PreviewQuestionsList.tsx
+++ src/widgets/question/QuestionsList/ui/PreviewQuestionsList/PreviewQuestionsList.tsx
@@ -3,7 +3,7 @@ import { useTranslation } from 'react-i18next';
 import { i18Namespace } from '@/shared/config/i18n';
 import { Questions } from '@/shared/config/i18n/i18nTranslations';
 import { ROUTES } from '@/shared/config/router/routes';
-import { useScreenSize, useAppSelector } from '@/shared/hooks';
+import { useAppSelector } from '@/shared/hooks';
 import { Card } from '@/shared/ui/Card';
 import { Flex } from '@/shared/ui/Flex';
 import { Text } from '@/shared/ui/Text';
@@ -30,8 +30,6 @@ export const PreviewQuestionsList = ({ className }: PreviewQuestionsListProps) =
 		specialization: specializationId,
 	});
 
-	const { isMobile } = useScreenSize();
-
 	const questions = response?.data ?? [];
 
 	const isEmptyData = isSuccess && questions.length === 0;
@@ -51,7 +49,6 @@ export const PreviewQuestionsList = ({ className }: PreviewQuestionsListProps) =
 			actionTitle={t(Questions.PREVIEW_LINK)}
 			actionRoute={ROUTES.interview.questions.page}
 			withShadow
-			isTitleCenter={isMobile}
 		>
 			<Flex componentType="ul" direction="column" gap="12" className={styles.list}>
 				{questions.map((question) => (
