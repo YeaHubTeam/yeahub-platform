769485b47606081dcb207a13278ba8e66347791f
Ilya Sinyakin <arqu1t@mail.ru>
2025-04-28 17:05:56 +0300

    YH-902: Fix calendar skeleton layout

--- src/shared/ui/Calendar/EventCalendar.skeleton.tsx
+++ src/shared/ui/Calendar/EventCalendar.skeleton.tsx
@@ -1,28 +1,48 @@
 import { CardSkeleton } from '@/shared/ui/Card';
 import { Flex } from '@/shared/ui/Flex';
 import { IconSkeleton } from '@/shared/ui/Icon';
-import { TextSkeleton } from '@/shared/ui/Text';
+import { Skeleton } from '@/shared/ui/Skeleton';
+
+import styles from './EventCalendar.skeleton.module.css';
 
 export const EventCalendarSkeleton = () => {
 	return (
 		<div className={'additional-info-wrapper'}>
 			<CardSkeleton className={'calendar-block'}>
-				<Flex direction="column" gap="16">
-					<Flex justify="between">
-						<IconSkeleton size={24} />
-						<TextSkeleton width={60} variant="body4" />
-						<IconSkeleton size={24} />
+				<Flex direction="column">
+					<Flex justify="between" className="react-calendar__navigation">
+						<IconSkeleton size={24} className={styles['element-wrapper']} />
+						<Skeleton
+							width={124}
+							height={24}
+							borderRadius={4}
+							className={styles['element-wrapper']}
+						/>
+						<IconSkeleton size={24} className={styles['element-wrapper']} />
 					</Flex>
 					<Flex justify="between">
 						{[...Array(7)].map((_, i) => (
-							<TextSkeleton key={i} variant="body2-accent" width={20} />
+							<Flex key={i} justify="center" maxWidth>
+								<Skeleton
+									width={24}
+									height={20}
+									borderRadius={4}
+									className={styles['element-wrapper']}
+								/>
+							</Flex>
 						))}
 					</Flex>
-					<Flex direction="column" gap="12">
+					<Flex direction="column">
 						{[...Array(5)].map((_, index) => (
 							<Flex key={index} justify="between">
 								{[...Array(7)].map((_, i) => (
-									<TextSkeleton key={i} variant="body2-accent" width={20} />
+									<Skeleton
+										key={i}
+										width={32}
+										height={24}
+										borderRadius={4}
+										className={styles['element-wrapper']}
+									/>
 								))}
 							</Flex>
 						))}
