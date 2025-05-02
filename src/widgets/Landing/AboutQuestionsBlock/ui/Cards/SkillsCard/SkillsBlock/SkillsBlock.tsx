d6334fe31ec16ddcdf4df9dc7a265b9185c9e847
Grigory <grigorydobrenko97@gmail.com>
2025-05-02 10:38:22 +0300

    YH-917: add skills block (#593)
    * YH-917: add skills block
    
    * YH-917: refactor block, add FiltersCard
    
    * YH-917: add sliders for InterviewMaterials cards and SkillsListTicker
    
    * YH-917: Add Chip variants and hover style, fix delete icon color

---
+++ src/widgets/Landing/AboutQuestionsBlock/ui/Cards/SkillsCard/SkillsBlock/SkillsBlock.tsx
@@ -0,0 +1,27 @@
+import { Flex } from '@/shared/ui/Flex';
+
+import { skills } from '../../../../model/constants';
+import { SkillChip } from '../../../SkillChip/SkillChip';
+import { CardBlockLayout } from '../../CardBlockLayout/CardBlockLayout';
+
+export const SkillsBlock = () => {
+	return (
+		<CardBlockLayout>
+			<Flex gap={'16'}>
+				<SkillChip src={skills.swift.src} alt={skills.swift.alt} />
+				<SkillChip src={skills.python.src} alt={skills.python.alt} />
+			</Flex>
+			<Flex gap={'16'}>
+				<SkillChip src={skills.git.src} alt={skills.git.alt} />
+				<SkillChip src={skills.react.src} alt={skills.react.alt} />
+				<SkillChip src={skills.html.src} alt={skills.html.alt} />
+			</Flex>
+			<Flex gap={'16'}>
+				<SkillChip src={skills.java.src} alt={skills.java.alt} />
+				<SkillChip src={skills.docker.src} alt={skills.docker.alt} />
+				<SkillChip src={skills.javascript.src} alt={skills.javascript.alt} />
+				<SkillChip src={skills.typescript.src} alt={skills.typescript.alt} />
+			</Flex>
+		</CardBlockLayout>
+	);
+};
---
+++ src/widgets/Landing/AboutQuestionsBlock/ui/Cards/SpecializationCard/SpecializationCard.tsx
@@ -0,0 +1,18 @@
+import { useTranslation } from 'react-i18next';
+
+import { i18Namespace } from '@/shared/config/i18n';
+import { Landing } from '@/shared/config/i18n/i18nTranslations';
+
+import { CardLayout } from './../CardLayout/CardLayout';
+import { SpecializationBlock } from './SpecializationBlock/SpecializationBlock';
+
+export const SpecializationCard = () => {
+	const { t } = useTranslation(i18Namespace.landing);
+	return (
+		<CardLayout
+			contentSlot={<SpecializationBlock />}
+			title={t(Landing.SPECIALIZATION_TITLE)}
+			description={t(Landing.SPECIALIZATION_DESCRIPTION)}
+		/>
+	);
+};
