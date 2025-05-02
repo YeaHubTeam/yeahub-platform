501b0e7f9591bf0d350de125e62e743f3c402d24
Vitaliy Shevtsov <57325046+vvs-jkr@users.noreply.github.com>
2025-05-02 11:22:31 +0300

    YH-931: fix company & collection validation. Fix toast translate (#602)

--- src/features/company/createCompany/ui/CompanyCreateFormHeader/CompanyCreateFormHeader.tsx
+++ src/features/company/createCompany/ui/CompanyCreateFormHeader/CompanyCreateFormHeader.tsx
@@ -1,22 +1,27 @@
 import { useFormContext } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
 
+import { i18Namespace } from '@/shared/config/i18n';
 import { Translation } from '@/shared/config/i18n/i18nTranslations';
 import { BackButton } from '@/shared/ui/BackButton';
 import { Button } from '@/shared/ui/Button';
 import { Flex } from '@/shared/ui/Flex';
 
-import { useCreateCompanyMutation } from '@/features/company/createCompany/api/createCompanyApi';
-
+import { useCreateCompanyMutation } from '../../api/createCompanyApi';
 import { CreateCompanyFormValues } from '../../model/types/companyCreateTypes';
 
 export const CompanyCreateFormHeader = () => {
 	const [createCompanyMutation, { isLoading }] = useCreateCompanyMutation();
 	const { handleSubmit } = useFormContext<CreateCompanyFormValues>();
-	const { t } = useTranslation(['company', 'translation']);
+	const { t } = useTranslation(i18Namespace.translation);
 
 	const onCreateCompany = async (data: CreateCompanyFormValues) => {
-		await createCompanyMutation(data);
+		try {
+			await createCompanyMutation(data).unwrap();
+		} catch (error) {
+			// eslint-disable-next-line no-console
+			console.log('Mutation error:', error);
+		}
 	};
 
 	return (
