029961e694ff6d302b0bbf93ee67f3e5e75d0082
Nikita Aristarkhov <aristarhovnikita1@gmail.com>
2025-05-07 00:40:42 +0700

    YH-951: add table skeleton, fix /admin/user route

--- src/pages/admin/SkillsPage/index.ts
+++ src/pages/admin/SkillsPage/index.ts
@@ -1,3 +1,4 @@
 export { SkillsPage } from './ui/SkillsPage/SkillsPage.lazy';
 export type { SkillsPageState } from './model/types/skillsPageTypes';
 export { skillsPageReducer } from './model/slices/skillsPageSlice';
+export { SkillsPageSkeleton } from './ui/SkillsPage/SkillsPage.skeleton';
