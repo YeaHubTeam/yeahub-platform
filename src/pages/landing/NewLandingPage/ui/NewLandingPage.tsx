b47b10f83a8dfea83a1298a23aa03fa2c41e6fa6
joyart78 <97617959+joyart78@users.noreply.github.com>
2025-05-13 23:08:43 +0500

    YH-916: add new block to landing (#605)
    * YH-916: add new block to landing
    
    * YH-916: fix comms
    
    * YH-916: fix comms

--- src/pages/landing/NewLandingPage/ui/NewLandingPage.tsx
+++ src/pages/landing/NewLandingPage/ui/NewLandingPage.tsx
@@ -1,7 +1,13 @@
 import { BannerBlock } from '@/widgets/NewLanding/BannerBlock';
+import { SpecializationBlock } from '@/widgets/NewLanding/SpecialityBlock';
 
 const NewLandingPage = () => {
-	return <BannerBlock />;
+	return (
+		<>
+			<BannerBlock />
+			<SpecializationBlock />
+		</>
+	);
 };
 
 export default NewLandingPage;
