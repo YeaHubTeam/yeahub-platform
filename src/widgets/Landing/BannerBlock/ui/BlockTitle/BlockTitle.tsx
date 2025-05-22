743de309051042b44d930b0e74bbc1b05dc39c41
Kirill <110499345+Kirillz8@users.noreply.github.com>
2025-05-22 14:44:35 +0300

    YH-990: fix font-size on titles for Landing page (#650)
    * YH-990: fix font-size on titles for Landing page
    
    * YH-990: fix BlockTitle for Landing page

--- src/widgets/Landing/BannerBlock/ui/BlockTitle/BlockTitle.tsx
+++ src/widgets/Landing/BannerBlock/ui/BlockTitle/BlockTitle.tsx
@@ -3,27 +3,21 @@ import { useTranslation } from 'react-i18next';
 
 import { i18Namespace } from '@/shared/config/i18n';
 import { Landing } from '@/shared/config/i18n/i18nTranslations';
+import { useScreenSize } from '@/shared/hooks';
 import { Text } from '@/shared/ui/Text';
 
 import styles from './BlockTitle.module.css';
 
 export const BlockTitle = () => {
 	const { t } = useTranslation(i18Namespace.landing);
+	const { isMobile } = useScreenSize();
 
 	return (
-		<>
-			<Text variant="head2" className={classNames(styles.title, styles.desktop)}>
-				{t(Landing.BANNER_TITLE)}
-			</Text>
-			{/*Нет такого варианта текста*/}
-
-			<Text variant="head3" className={classNames(styles.title, styles.tablet)}>
-				{t(Landing.BANNER_TITLE)}
-			</Text>
-			{/*Нет такого варианта текста*/}
-			<Text variant="head3" className={classNames(styles.title, styles.mobile)}>
-				{t(Landing.BANNER_TITLE)}
-			</Text>
-		</>
+		<Text
+			variant={isMobile ? 'body5-accent' : 'head3'}
+			className={classNames(styles.title, styles.desktop)}
+		>
+			{t(Landing.BANNER_TITLE)}
+		</Text>
 	);
 };
--- src/widgets/Landing/InterviewTrainerBlock/ui/InterviewTrainerBlock/InterviewTrainerBlock.tsx
+++ src/widgets/Landing/InterviewTrainerBlock/ui/InterviewTrainerBlock/InterviewTrainerBlock.tsx
@@ -21,7 +21,7 @@ export const InterviewTrainerBlock = () => {
 	return (
 		<section className={styles['interview-trainer']}>
 			<div className={styles['title-block']}>
-				<Text variant="head2" className={styles.title}>
+				<Text variant={isMobile ? 'body5-accent' : 'head3'} className={styles.title}>
 					{t(Landing.TRAINING_INTERVIEW_TITLE).toUpperCase()}
 				</Text>
 				<Text variant="body3" className={styles.subtitle}>
