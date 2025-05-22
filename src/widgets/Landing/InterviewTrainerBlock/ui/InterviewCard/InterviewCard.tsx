1e7718559ab8b4f1c4e29cace392a2d2a037d752
Gray7410 <56793367+Gray7410@users.noreply.github.com>
2025-05-22 11:08:42 +0500

    YH-991: Change button link (#649)

--- src/widgets/Landing/InterviewTrainerBlock/ui/InterviewCard/InterviewCard.tsx
+++ src/widgets/Landing/InterviewTrainerBlock/ui/InterviewCard/InterviewCard.tsx
@@ -5,8 +5,6 @@ import { useNavigate } from 'react-router-dom';
 import { i18Namespace } from '@/shared/config/i18n';
 import { Landing } from '@/shared/config/i18n/i18nTranslations';
 import { ROUTES } from '@/shared/config/router/routes';
-import { LS_ACCESS_TOKEN_KEY } from '@/shared/constants/authConstants';
-import { getFromLS } from '@/shared/helpers/manageLocalStorage';
 import { useScreenSize } from '@/shared/hooks';
 import { Button } from '@/shared/ui/Button';
 import { Flex } from '@/shared/ui/Flex';
@@ -25,11 +23,6 @@ export const InterviewCard = ({ img, text, iconType }: InterviewCardProps) => {
 	const navigate = useNavigate();
 	const { isMobile } = useScreenSize();
 
-	const handleNavigate = () => {
-		const path = getFromLS(LS_ACCESS_TOKEN_KEY) ? ROUTES.quiz.page : ROUTES.auth.login.page;
-		navigate(path);
-	};
-
 	return (
 		<Flex
 			gap="14"
@@ -45,7 +38,9 @@ export const InterviewCard = ({ img, text, iconType }: InterviewCardProps) => {
 					<Icon color={iconType === 'settings' ? 'yellow-900' : 'green-900'} icon={iconType} />
 				</div>
 				<Text variant="body3">{text}</Text>
-				<Button onClick={handleNavigate}>{t(Landing.TRAINING_INTERVIEW_LINK)}</Button>
+				<Button onClick={() => navigate(ROUTES.quiz.page)}>
+					{t(Landing.TRAINING_INTERVIEW_LINK)}
+				</Button>
 			</Flex>
 		</Flex>
 	);
