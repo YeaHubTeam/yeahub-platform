1bb9e2971fdcc6633d18d14bfb4ceefe7e8ec490
Denis Pereloma <den.pereloma@gmail.com>
2025-05-26 14:49:49 +0300

    YH-741: add limit content fo free users

--- src/widgets/question/QuestionActions/QuestionActions.tsx
+++ src/widgets/question/QuestionActions/QuestionActions.tsx
@@ -1,5 +1,4 @@
 import { useTranslation } from 'react-i18next';
-import { Link } from 'react-router-dom';
 
 import { i18Namespace } from '@/shared/config/i18n';
 import { Subscription } from '@/shared/config/i18n/i18nTranslations';
@@ -8,15 +7,11 @@ import { useAppSelector } from '@/shared/hooks/useAppSelector';
 import { useScreenSize } from '@/shared/hooks/useScreenSize';
 import { Card } from '@/shared/ui/Card';
 import { Flex } from '@/shared/ui/Flex';
-import { Icon } from '@/shared/ui/Icon';
-import { Text } from '@/shared/ui/Text';
 
 import { getFullProfile, getHasPremiumAccess } from '@/entities/profile';
 
 import { LearnQuestionButton } from '@/features/quiz/learnQuestion';
 import { ResetQuestionStudyProgressButton } from '@/features/quiz/resetQuestionStudyProgress';
-
-import styles from './QuestionActions.module.css';
 
 interface QuestionActionsProps {
 	profileId: number | string;
@@ -33,24 +28,17 @@ export const QuestionActions = ({ profileId, questionId, checksCount }: Question
 	const buttonVariant = isMobile || isTablet ? 'link-gray' : 'tertiary';
 	const isEmailVerified = profile?.isEmailVerified;
 
-	const linkContent = (
-		<>
-			<Text variant="body3-strong" color="purple-700">
-				{t(Subscription.CHANGE_TARIFF_PLAN, { ns: i18Namespace.subscription })}
-			</Text>
-			<Icon icon="arrowRight" size={24} color="purple-700" className={styles.icon} />
-		</>
-	);
-
 	return (
 		<Flex direction="column" gap="20">
-			<Card className={styles.card}>
+			<Card
+				actionRoute={!hasPremiumAccess && !isMobileS ? ROUTES.settings.page : undefined}
+				actionTitle={
+					!hasPremiumAccess && !isMobileS
+						? t(Subscription.CHANGE_TARIFF_PLAN, { ns: i18Namespace.subscription })
+						: undefined
+				}
+			>
 				<Flex direction="column" gap="24" align="center">
-					{!hasPremiumAccess && !isMobileS && (
-						<Link to={ROUTES.settings.page} className={styles['desktop-link']}>
-							{linkContent}
-						</Link>
-					)}
 					<Flex justify="center" gap="40" align="center">
 						<LearnQuestionButton
 							profileId={profileId}
@@ -67,11 +55,6 @@ export const QuestionActions = ({ profileId, questionId, checksCount }: Question
 					</Flex>
 				</Flex>
 			</Card>
-			{!hasPremiumAccess && isMobileS && (
-				<Link to={ROUTES.settings.page} className={styles.link}>
-					{linkContent}
-				</Link>
-			)}
 		</Flex>
 	);
 };
