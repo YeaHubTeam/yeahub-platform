47fd59ed6d97e46c51b6f07690555bffacd38f50
George Skornyakov <George.SGN@yandex.ru>
2025-06-04 19:20:32 +0300

    YH-1022: scaffold marketplace placeholders

--- src/pages/marketplace/MarketplacePage/MarketplacePage.tsx
+++ src/pages/marketplace/MarketplacePage/MarketplacePage.tsx
@@ -1,8 +1,19 @@
 import { memo } from 'react';
 
+import { Card } from '@/shared/ui/Card';
+import { Flex } from '@/shared/ui/Flex';
+
 import cls from './MarketplacePage.module.css';
 
-const MarketplacePage = () => <div className={cls.marketplacePage}>TODO: Marketplace content</div>;
+const MarketplacePage = () => (
+	<Flex gap="20" align="start" className={cls.wrapper}>
+		{/* левая колонка со списком ресурсов */}
+		<Card className={cls['list-placeholder']}>TODO: resources list placeholder</Card>
+
+		{/* правая колонка с фильтрами */}
+		<Card className={cls['filter-placeholder']}>TODO: filters placeholder</Card>
+	</Flex>
+);
 
 export default memo(MarketplacePage);
 export { MarketplacePage };
