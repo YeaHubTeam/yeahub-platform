2400ebdb02f578bb081db88742366054dde7bb71
George Skornyakov <George.SGN@yandex.ru>
2025-06-09 20:21:02 +0300

    YH-1022: rename cls to styles in MarketplacePage

--- src/pages/marketplace/MarketplacePage/ui/MarketplacePage.tsx
+++ src/pages/marketplace/MarketplacePage/ui/MarketplacePage.tsx
@@ -1,15 +1,15 @@
 import { Card } from '@/shared/ui/Card';
 import { Flex } from '@/shared/ui/Flex';
 
-import cls from './MarketplacePage.module.css';
+import styles from './MarketplacePage.module.css';
 
 const MarketplacePage = () => (
-	<Flex gap="20" align="start" className={cls.wrapper}>
+	<Flex gap="20" align="start" className={styles.wrapper}>
 		{/* левая колонка со списком ресурсов */}
-		<Card className={cls['list-placeholder']}>TODO: resources list placeholder</Card>
+		<Card className={styles['list-placeholder']}>TODO: resources list placeholder</Card>
 
 		{/* правая колонка с фильтрами */}
-		<Card className={cls['filter-placeholder']}>TODO: filters placeholder</Card>
+		<Card className={styles['filter-placeholder']}>TODO: filters placeholder</Card>
 	</Flex>
 );
 
