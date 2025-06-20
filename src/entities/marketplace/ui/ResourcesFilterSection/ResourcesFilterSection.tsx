1e03d2f0b847d1ddecadf468bab9fbfe16bff6b2
thekinaway <thekinaway@gmail.com>
2025-06-20 19:22:35 +0500

    YH-1042: feat: hide "Show All" button when all resources are displayed

--- src/entities/marketplace/ui/ResourcesFilterSection/ResourcesFilterSection.tsx
+++ src/entities/marketplace/ui/ResourcesFilterSection/ResourcesFilterSection.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import { i18Namespace } from '@/shared/config/i18n';
@@ -23,14 +23,7 @@ export const ResourcesFilterSection = ({
 }: ResourcesFilterSectionProps) => {
 	const { t } = useTranslation(i18Namespace.marketplace);
 	const [showAll, setShowAll] = useState(false);
-	const [limit, setLimit] = useState(resourceLimit || MAX_LIMIT_RESOURCES);
-
-	// TODO: Implement useGetResourcesListQuery
-	limit;
-	// const { data: resources } = useGetResourcesListQuery({
-	// 	limit,
-	// 	resources: [selectedResources],
-	// });
+	// const [limit, setLimit] = useState(resourceLimit || MAX_LIMIT_RESOURCES);
 
 	const resources = {
 		total: 4,
@@ -42,19 +35,27 @@ export const ResourcesFilterSection = ({
 		],
 	};
 
+	const hasHiddenResources = resources.total > (resourceLimit || MAX_LIMIT_RESOURCES);
+
+	// TODO: Implement useGetResourcesListQuery
+	// const { data: resources } = useGetResourcesListQuery({
+	// 	limit,
+	// 	resources: [selectedResources],
+	// });
+
+	// useEffect(() => {
+	// 	if (showAll) {
+	// 		setLimit(resources?.total ?? (resourceLimit || MAX_LIMIT_RESOURCES));
+	// 	} else {
+	// 		setLimit(resourceLimit || MAX_LIMIT_RESOURCES);
+	// 	}
+	// }, [resources?.total, showAll, resourceLimit]);
+
 	const toggleShowAll = () => {
 		setShowAll(!showAll);
 	};
 
-	useEffect(() => {
-		if (showAll) {
-			setLimit(resources?.total ?? (resourceLimit || MAX_LIMIT_RESOURCES));
-		} else {
-			setLimit(resourceLimit || MAX_LIMIT_RESOURCES);
-		}
-	}, [resources?.total, showAll, resourceLimit]);
-
-	const prepareData = resources?.data.map((resource) => ({
+	const preparedData = resources?.data.map((resource) => ({
 		...resource,
 		active: selectedResources?.includes(resource.id),
 	}));
@@ -71,13 +72,15 @@ export const ResourcesFilterSection = ({
 	return (
 		<div className={styles.wrapper}>
 			<BaseFilterSection
-				data={prepareData}
+				data={preparedData}
 				title={t(Marketplace.RESOURCES_TITLE)}
 				onClick={onChooseResource}
 			/>
-			<Button className={styles.button} variant="link" onClick={toggleShowAll}>
-				{!showAll ? t(Marketplace.SHOW_ALL) : t(Marketplace.HIDE)}
-			</Button>
+			{hasHiddenResources && (
+				<Button className={styles.button} variant="link" onClick={toggleShowAll}>
+					{!showAll ? t(Marketplace.SHOW_ALL) : t(Marketplace.HIDE)}
+				</Button>
+			)}
 		</div>
 	);
 };
