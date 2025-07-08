0af7cf97167080244dff19fa58471126f19855d3
thekinaway <thekinaway@gmail.com>
2025-07-08 19:53:40 +0500

    YH-1134: feature: add ResourceForm and FormSelect,ResourceSelect for using in it

---
+++ src/entities/resource/ui/ResourceSelect/ResourceSelect.tsx
@@ -0,0 +1,130 @@
+import { useMemo, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+
+import { i18Namespace } from '@/shared/config/i18n';
+import { Marketplace } from '@/shared/config/i18n/i18nTranslations';
+import { Dropdown, Option } from '@/shared/ui/Dropdown';
+import { SelectWithChips } from '@/shared/ui/SelectWithChips';
+
+type ResourcesSelectProps = Omit<
+	React.ComponentProps<typeof Dropdown>,
+	'options' | 'type' | 'value' | 'onChange' | 'children'
+> & {
+	value: number | number[];
+	onChange: (value: number[] | number) => void;
+	hasMultiple?: boolean;
+	disabled?: boolean;
+};
+
+type ResourceType = {
+	id: number;
+	title: string;
+};
+
+export const ResourcesSelect = ({
+	onChange,
+	value,
+	hasMultiple,
+	disabled,
+}: ResourcesSelectProps) => {
+	const { t } = useTranslation(i18Namespace.marketplace);
+	// const { data: resourcesTypes } = useGetResourcesTypesListQuery({ limit: 100 });
+
+	//TODO: Implement useGetResourcesTypesListQuery
+	const resourcesTypes = {
+		total: 4,
+		data: [
+			{ id: 1, title: t(Marketplace.RESOURCES_REPOSITORY) },
+			{ id: 2, title: t(Marketplace.RESOURCES_VIDEO) },
+			{ id: 3, title: t(Marketplace.RESOURCES_ARTICLE) },
+			{ id: 4, title: t(Marketplace.RESOURCES_COURSE) },
+		],
+	};
+
+	const [selectedResources, setSelectedResources] = useState<number[]>(
+		Array.isArray(value) ? value : value !== undefined ? [value] : [],
+	);
+
+	const handleChange = (newValue: string | undefined) => {
+		if (disabled || !newValue) return;
+		const numValue = +newValue;
+
+		if (hasMultiple) {
+			const updates = [...selectedResources, numValue];
+			setSelectedResources(updates);
+			onChange(updates);
+		} else {
+			setSelectedResources([numValue]);
+			onChange([numValue]);
+		}
+	};
+
+	const handleDeleteResource = (id: number) => () => {
+		if (disabled) return;
+		const updates = selectedResources.filter((resourceId) => resourceId !== id);
+		setSelectedResources(updates);
+		onChange(updates);
+	};
+
+	const options = useMemo(() => {
+		if (hasMultiple) {
+			return (resourcesTypes?.data || [])
+				.map((resource) => ({
+					label: resource.title,
+					value: resource.id.toString(),
+					limit: 100,
+				}))
+				.filter((resource) => !selectedResources?.includes(+resource.value));
+		} else {
+			return (resourcesTypes?.data || []).map((resource) => ({
+				label: resource.title,
+				value: resource.id.toString(),
+				limit: 100,
+			}));
+		}
+	}, [selectedResources, resourcesTypes]);
+
+	const resourcesDictionary = useMemo(() => {
+		const emptyResource: ResourceType = {
+			id: 0,
+			title: t(Marketplace.SELECT_CHOOSE),
+		};
+		return (resourcesTypes?.data || []).reduce(
+			(acc, resource) => {
+				acc[resource.id] = resource;
+				return acc;
+			},
+			{ 0: emptyResource } as Record<number, ResourceType>,
+		);
+	}, [resourcesTypes]);
+
+	if (!hasMultiple) {
+		return (
+			<>
+				<Dropdown
+					label={options.length ? t(Marketplace.SELECT_CHOOSE) : t(Marketplace.SELECT_EMPTY)}
+					disabled={disabled}
+					value={resourcesDictionary[selectedResources[0] || 0]?.title ?? ''}
+					onSelect={(val) => handleChange(String(val))}
+				>
+					{options.map((option) => (
+						<Option value={option.value} label={option.label} key={option.label} />
+					))}
+				</Dropdown>
+			</>
+		);
+	}
+
+	return (
+		<SelectWithChips
+			title={t(Marketplace.SELECT_SELECTED)}
+			options={options}
+			onChange={handleChange}
+			selectedItems={selectedResources}
+			handleDeleteItem={handleDeleteResource}
+			itemsDictionary={resourcesDictionary}
+			placeholder={options.length ? t(Marketplace.SELECT_CHOOSE) : t(Marketplace.SELECT_EMPTY)}
+			disabled={disabled}
+		/>
+	);
+};
