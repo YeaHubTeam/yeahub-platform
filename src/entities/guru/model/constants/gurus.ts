1699998d34f2e6a2a86323c8d139ffd094d5444d
it_lilya <105752516+it-Lilya@users.noreply.github.com>
2025-07-29 22:36:18 +0300

    YH-1230: added go guru (#776)

--- src/entities/guru/model/constants/gurus.ts
+++ src/entities/guru/model/constants/gurus.ts
@@ -36,4 +36,15 @@ export const gurus: Guru[] = [
 			profileId: 'f917b016-280f-4127-8d4b-149a3c1aec71',
 		},
 	},
+	{
+		title: 'Golang Guru',
+		name: 'Maxim Lukyanov',
+		specializationId: 23,
+		image: 'https://e5e684b1-4a6a-4be5-b7ee-b2b678239d61.selstorage.ru/gurus/Автостоп.jpg',
+		socials: {
+			telegram: 'https://t.me/Avtostopom_po_G0',
+			youtube: 'https://www.youtube.com/@Avtostopom_po_Go',
+			profileId: '2350f2b4-d06d-4a8b-b6a6-742c8e0df414',
+		},
+	},
 ];
