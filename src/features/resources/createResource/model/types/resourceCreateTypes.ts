b8170d231e5e3727b269a09ab9e6811dc43dd4fb
Dmytro Paramonov <dmytro.paramonov@gmail.com>
2025-08-13 16:11:25 +0300

    YH-1256: fix(landing): create resource request — add DTO + mapper, remove obsolete 'types' validation

--- src/features/resources/createResource/model/types/resourceCreateTypes.ts
+++ src/features/resources/createResource/model/types/resourceCreateTypes.ts
@@ -1,6 +1,22 @@
-import { CreateOrEditResourceFormValues, Resource } from '@/entities/resource';
+import {
+	CreateOrEditResourceFormValues,
+	Resource,
+	ResourceAccessCategory,
+} from '@/entities/resource';
 
 export type CreateResourceFormValues = Omit<CreateOrEditResourceFormValues, 'id'>;
 
-export type CreateResourceBodyRequest = CreateResourceFormValues;
+export type CreateResourceBodyRequest = {
+	product: {
+		name: string;
+		description: string;
+		provider: string;
+		accessCategory: ResourceAccessCategory;
+		iconBase64: string | null;
+	};
+	skills: number[];
+	specializations: number[];
+	keywords: string[];
+	isActive: boolean;
+};
 export type CreateResourceResponse = Resource;
