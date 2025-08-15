3f26cf3c04344c4ba7633cbfaffd089fa7a8d135
Darya898 <43304776+Darya898@users.noreply.github.com>
2025-08-14 23:39:50 -0700

    YH-1269: redirect to email veritify (#806)

--- src/features/subscriptions/trial/ui/TrialButton.tsx
+++ src/features/subscriptions/trial/ui/TrialButton.tsx
@@ -1,9 +1,17 @@
 import classNames from 'classnames';
+import toast from 'react-hot-toast';
 import { useTranslation } from 'react-i18next';
+import { useNavigate } from 'react-router-dom';
 
 import { i18Namespace } from '@/shared/config/i18n';
+import i18n from '@/shared/config/i18n/i18n';
+import { Translation } from '@/shared/config/i18n/i18nTranslations';
 import { Subscription } from '@/shared/config/i18n/i18nTranslations';
+import { EMAIL_VERIFY_SETTINGS_TAB } from '@/shared/constants/customRoutes';
+import { useAppSelector } from '@/shared/hooks/useAppSelector';
 import { Button } from '@/shared/ui/Button';
+
+import { getIsEmailVerified } from '@/entities/profile';
 
 import { useLazyGetTrialQuery } from '../api/getTrial';
 
@@ -16,12 +24,24 @@ interface TrialButtonProps {
 export const TrialButton = ({ className }: TrialButtonProps) => {
 	const { t } = useTranslation(i18Namespace.subscription);
 
+	const navigate = useNavigate();
+
+	const isEmailVerified = useAppSelector(getIsEmailVerified);
+
 	const [getTrial, { isLoading }] = useLazyGetTrialQuery();
 
 	const onGetTrial = async (event: React.MouseEvent<HTMLButtonElement>) => {
 		event.preventDefault();
-
-		await getTrial();
+		if (!isEmailVerified) {
+			navigate(EMAIL_VERIFY_SETTINGS_TAB);
+			return;
+		}
+		try {
+			await getTrial();
+		} catch (err) {
+			toast.error(i18n.t(Translation.TOAST_SUBSCRIPTIONS_TRIAL_FAILED));
+			console.error('error:', err);
+		}
 	};
 
 	return (
