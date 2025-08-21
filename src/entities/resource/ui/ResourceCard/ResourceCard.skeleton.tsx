a22fa5c9855d8b41a61d9c225db6bdde58ddda1d
Emin14 <emain-agjaev@mail.ru>
2025-08-21 13:43:49 +0300

    YH-1273: Made the necessary corrections after the review

--- src/entities/resource/ui/ResourceCard/ResourceCard.skeleton.tsx
+++ src/entities/resource/ui/ResourceCard/ResourceCard.skeleton.tsx
@@ -1,7 +1,7 @@
 import { Card } from '@/shared/ui/Card';
 import { Flex } from '@/shared/ui/Flex';
-import { TextSkeleton } from '@/shared/ui/Text';
 import { ImageWithWrapperSkeleton } from '@/shared/ui/ImageWithWrapper';
+import { TextSkeleton } from '@/shared/ui/Text';
 
 import styles from './ResourceCard.module.css';
 
@@ -11,9 +11,9 @@ export const ResourceCardSkeleton = () => {
 			<Flex className={styles.wrapper}>
 				<ImageWithWrapperSkeleton className={styles['image-wrapper']} />
 				<Flex direction="column" gap="12" maxWidth={true}>
-					<TextSkeleton variant="body2" width={'100%'} className={styles.hostname}/>
-					<TextSkeleton variant="body3-accent" width={'100%'} className={styles.title}/>
-					<TextSkeleton variant="body2" width={'100%'}/>
+					<TextSkeleton variant="body2" width={'100%'} className={styles.hostname} />
+					<TextSkeleton variant="body3-accent" width={'100%'} className={styles.title} />
+					<TextSkeleton variant="body2" width={'100%'} />
 				</Flex>
 			</Flex>
 		</Card>
