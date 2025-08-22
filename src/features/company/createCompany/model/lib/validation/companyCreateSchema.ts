071069ff3ca576ae5e1894d99d8bc66f4cef1011
Dmitry <104439884+dimaparamonovv@users.noreply.github.com>
2025-08-22 17:57:50 +0300

    YH-1281: creating company, fixed submitting through refactoring schema and types (#817)

--- src/features/company/createCompany/model/lib/validation/companyCreateSchema.ts
+++ src/features/company/createCompany/model/lib/validation/companyCreateSchema.ts
@@ -8,17 +8,11 @@ import { CreateCompanyFormValues } from '../../types/companyCreateTypes';
 export const companyCreateSchema: yup.ObjectSchema<CreateCompanyFormValues> = yup.object().shape({
 	id: yup.string(),
 	title: yup.string().required(i18n.t(Translation.VALIDATION_REQUIRED)),
-	legalName: yup.string(),
-	description: yup.string(),
-	imageSrc: yup.string().notRequired(),
-	companyImage: yup.string().notRequired(),
-	inn: yup.string(),
-	kpp: yup.string(),
-	createdBy: yup
-		.object()
-		.shape({
-			id: yup.string().required(),
-			username: yup.string().required(),
-		})
-		.optional(),
+	legalName: yup.string().optional(),
+	description: yup.string().optional(),
+	imageSrc: yup.string().nullable().optional(),
+	companyImage: yup.string().nullable().optional(),
+	inn: yup.string().optional(),
+	kpp: yup.string().optional(),
+	createdBy: yup.mixed().strip(true).optional(),
 });
