2f038b5f5b9319c198b08b819791eee90ec1540c
Владислав <77724398+vladich21@users.noreply.github.com>
2025-08-22 17:55:02 +0300

    YH-1267: Added delete button to ResourcesTablePage (#814)
    * YH-1267: Added delete button to ResourcesTablePage
    
    * YH-1267: fixed constants url and added absolute import
    
    * YH-1267: fix duplicate telegram endpoint from develop
    
    * YH-1267: Update import for DeleteResourceButton

--- src/widgets/resources/ResourcesTable/ui/ResourcesTable.tsx
+++ src/widgets/resources/ResourcesTable/ui/ResourcesTable.tsx
@@ -1,20 +1,31 @@
 import { useTranslation } from 'react-i18next';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 import { i18Namespace } from '@/shared/config/i18n';
-import { Resources } from '@/shared/config/i18n/i18nTranslations';
+import { Resources, Translation } from '@/shared/config/i18n/i18nTranslations';
 import { ROUTES } from '@/shared/config/router/routes';
 import { route } from '@/shared/helpers/route';
+import { Flex } from '@/shared/ui/Flex';
+import { Icon } from '@/shared/ui/Icon';
+import { IconButton } from '@/shared/ui/IconButton';
+import { Popover, PopoverMenuItem } from '@/shared/ui/Popover';
 import { Table } from '@/shared/ui/Table';
 import { Text } from '@/shared/ui/Text';
 
 import { Resource } from '@/entities/resource';
 
+import { DeleteResourceButton } from '@/features/resources/deleteResource';
+
+interface UIResource extends Resource {
+	disabled?: boolean;
+}
+
 interface ResourcesTableProps {
-	resources?: Resource[];
+	resources?: UIResource[];
 }
 
 export const ResourcesTable = ({ resources }: ResourcesTableProps) => {
+	const navigate = useNavigate();
 	const { t } = useTranslation([i18Namespace.resources, i18Namespace.translation]);
 
 	const renderTableHeader = () => {
@@ -36,21 +47,61 @@ export const ResourcesTable = ({ resources }: ResourcesTableProps) => {
 			author: resource.provider,
 		};
 
-		return Object.entries(columns)?.map(([k, v]) => {
-			return (
-				<td key={k}>
-					{k === 'title' ? (
-						<Link to={route(ROUTES.admin.resources.details.route, resource.id)}>
-							<Text variant={'body3'} color={'purple-700'}>
-								{v}
-							</Text>
-						</Link>
-					) : (
-						v
+		return Object.entries(columns)?.map(([k, v]) => (
+			<td key={k}>
+				{k === 'title' ? (
+					<Link to={route(ROUTES.admin.resources.details.route, resource.id)}>
+						<Text variant={'body3'} color={'purple-700'}>
+							{v}
+						</Text>
+					</Link>
+				) : (
+					v
+				)}
+			</td>
+		));
+	};
+
+	const renderActions = (resource: UIResource) => {
+		const menuItems: PopoverMenuItem[] = [
+			{
+				icon: <Icon icon="eye" size={24} />,
+				title: t(Translation.SHOW, { ns: i18Namespace.translation }),
+				onClick: () => {
+					navigate(route(ROUTES.admin.resources.details.route, resource.id));
+				},
+			},
+			{
+				icon: <Icon icon="pen" size={24} />,
+				title: t(Translation.EDIT, { ns: i18Namespace.translation }),
+				disabled: resource.disabled,
+				onClick: () => {
+					navigate(route(ROUTES.admin.resources.edit.route, resource.id));
+				},
+			},
+			{
+				renderComponent: () => (
+					<DeleteResourceButton resourceId={resource.id} disabled={resource.disabled} />
+				),
+			},
+		];
+
+		return (
+			<Flex gap="4">
+				<Popover menuItems={menuItems}>
+					{({ onToggle }) => (
+						<IconButton
+							aria-label="go to details"
+							form="square"
+							icon={<Icon icon="dotsThreeVertical" size={20} />}
+							size="medium"
+							variant="tertiary"
+							onClick={onToggle}
+						/>
 					)}
-				</td>
-			);
-		});
+				</Popover>
+			</Flex>
+		);
 	};
 
 	if (!resources) {
@@ -61,6 +112,7 @@ export const ResourcesTable = ({ resources }: ResourcesTableProps) => {
 		<Table
 			renderTableHeader={renderTableHeader}
 			renderTableBody={renderTableBody}
+			renderActions={renderActions}
 			items={resources}
 		/>
 	);
