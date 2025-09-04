c76e8e35655f85fc5a783e235f74c097001e96fe
Buffik <som16@mail.ru>
2025-09-04 17:51:21 +0300

    YH-1302: fix from comments

--- src/features/quiz/cloneQuiz/ui/CloneQuizButton/CloneQuizButton.tsx
+++ src/features/quiz/cloneQuiz/ui/CloneQuizButton/CloneQuizButton.tsx
@@ -4,17 +4,15 @@ import { useParams } from 'react-router-dom';
 
 import { i18Namespace } from '@/shared/config/i18n';
 import { InterviewQuizResult } from '@/shared/config/i18n/i18nTranslations';
-import { useAppSelector, useAppDispatch, useModal } from '@/shared/hooks';
+import { useAppSelector, useModal } from '@/shared/hooks';
 import { Button } from '@/shared/ui/Button';
 
 import { getProfileId } from '@/entities/profile';
 import { useGetActiveQuizQuery, useLazyCloneQuizQuery } from '@/entities/quiz';
-import { clearActiveQuizState } from '@/entities/quiz/model/slices/activeQuizSlice';
 
 const ResetActiveQuizModal = lazy(() => import('../ResetActiveQuizModal/ResetActiveQuizModal'));
 
 export const CloneQuizButton = () => {
-	const dispatch = useAppDispatch();
 	const { quizId = '' } = useParams<{ quizId?: string }>();
 	const { isOpen, onToggle, onClose } = useModal();
 
@@ -44,7 +42,6 @@ export const CloneQuizButton = () => {
 	};
 
 	const onResetActiveQuizOk = () => {
-		dispatch(clearActiveQuizState(profileId));
 		cloneQuiz(quizId);
 	};
 
