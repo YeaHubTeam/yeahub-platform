f5d63b38eb7f6d6afd7a54fdadb5592d86aee75b
Vladimir <62429354+Buffik@users.noreply.github.com>
2025-09-04 12:35:39 +0300

    YH-1306: add Resources page to dashboard (#845)
    * YH-1306: add sidebar menu item
    
    * YH-1306: add page to router
    
    * YH-1306: add page files
    
    * YH-1306: fix from comments

--- src/shared/config/i18n/i18nTranslations.ts
+++ src/shared/config/i18n/i18nTranslations.ts
@@ -33,6 +33,8 @@ export enum Translation {
 	SIDEBAR_MENU_USERS = 'sidebar.menu.users',
 	SIDEBAR_MENU_EDUCATION_TITLE = 'sidebar.menu.education.title',
 	SIDEBAR_MENU_EDUCATION_INTERVIEW = 'sidebar.menu.education.interview',
+	SIDEBAR_MENU_WIKI_TITLE = 'sidebar.menu.wiki.title',
+	SIDEBAR_MENU_WIKI_RESOURCES_TITLE = 'sidebar.menu.wiki.resources',
 	SIDEBAR_MENU_QUESTIONS = 'sidebar.menu.questions',
 	SIDEBAR_MENU_RESOURCES = 'sidebar.menu.resources',
 	SIDEBAR_MENU_SKILLS = 'sidebar.menu.skills',
@@ -69,6 +71,8 @@ export enum Translation {
 	CRUMBS_COLLECTIONS_LIST = 'crumbs.collections.list',
 	CRUMBS_COLLECTIONS_DETAIL = 'crumbs.collection.detail',
 	CRUMBS_QUIZ = 'crumbs.quiz',
+	CRUMBS_WIKI = 'crumbs.wiki',
+	CRUMBS_RESOURCES = 'crumbs.resources',
 
 	/* Validation */
 	VALIDATION_REQUIRED = 'validation.required',
