733067a1dbad74d41bcecfe765391be2c48649c9
Vladimir <62429354+Buffik@users.noreply.github.com>
2025-09-05 14:13:04 +0300

    YH-1000: add skeleton to collection block, connect skeleton to landing page (#847)
    * YH-1000: add skeleton to collection block, connect skeleton to landing page
    
    * YH-1000: fix from comments

---
+++ src/widgets/Landing/CollectionBlock/ui/FilterChip/FilterChip.skeleton.tsx
@@ -0,0 +1,16 @@
+import { ChipSkeleton } from '@/shared/ui/Chip';
+import { IconSkeleton } from '@/shared/ui/Icon';
+
+import styles from './FilterChip.module.css';
+
+export const FilterChipSkeleton = () => {
+	return (
+		<ChipSkeleton
+			variant={'big'}
+			className={styles.chip}
+			label="..."
+			withText={60}
+			prefix={<IconSkeleton size={20} />}
+		/>
+	);
+};
