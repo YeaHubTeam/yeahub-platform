4220772e21f69b9f8f2aea2fbcc6951d65815b9b
Denis Pereloma <den.pereloma@gmail.com>
2025-09-16 13:18:53 +0300

    YH-1380: refactor select specializations for filters

--- src/entities/specialization/index.ts
+++ src/entities/specialization/index.ts
@@ -11,8 +11,8 @@ export { SpecializationForm } from './ui/SpecializationForm/SpecializationForm';
 export { SpecializationSelect } from './ui/SpecializationSelect/SpecializationSelect';
 export { SpecializationSelectSkeleton } from './ui/SpecializationSelect/SpecializationSelect.skeleton';
 export { SpecializationsList } from './ui/SpecializationsList/SpecializationsList';
-export { SpecializationsListField } from '@/entities/specialization/ui/SpecializationsListField/SpecializationsListField';
-export { SpecializationsListFieldSkeleton } from '@/entities/specialization/ui/SpecializationsListField/SpecializationsListField.skeleton';
+export { SpecializationsListField } from './ui/SpecializationsListField/SpecializationsListField';
+export { SpecializationsListFieldSkeleton } from './ui/SpecializationsListField/SpecializationsListField.skeleton';
 
 export { specializationHandlers } from './api/__mocks__';
 
