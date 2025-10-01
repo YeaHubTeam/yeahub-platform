28f34a878cfee9fd3bdf37b5225d36bad293fc3e
Denis Pereloma <den.pereloma@gmail.com>
2025-10-01 19:13:15 +0300

    delete unused files

--- src/pages/admin/ResourcesRequestsTablePage/model/types/resourcesRequestsTablePageTypes.ts
+++
@@ -1,7 +0,0 @@
-import { SelectedResourceRequestEntities } from '@/entities/resource';
-
-export interface ResourcesRequestsTablePageState {
-	page: number;
-	selectedResourcesRequests: SelectedResourceRequestEntities;
-	search?: string;
-}
