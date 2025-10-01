daeee3cccb725999cf58eeb76632b3eb9618d677
OlgaBul <131461462+OlgaBul@users.noreply.github.com>
2025-10-01 19:30:45 +0300

    Feature/YH-1345 (#893)
    * YH-1345: updated table design
    
    * YH-1345: Added missing file
    
    * YH-1345:  apply review comments
    
    * YH-1345: apply review comments
    
    * YH-1345: apply review comments

--- src/widgets/CompaniesTable/ui/CompaniesTable.tsx
+++ src/widgets/CompaniesTable/ui/CompaniesTable.tsx
@@ -71,9 +71,7 @@ export const CompaniesTable = ({
 			<td key={k}>
 				{k === 'title' ? (
 					<Link to={route(ROUTES.admin.companies.details.route, company.id)}>
-						<Text variant={'body3'} color={'purple-700'}>
-							{v}
-						</Text>
+						<Text variant={'body3-accent'}>{v}</Text>
 					</Link>
 				) : (
 					v
