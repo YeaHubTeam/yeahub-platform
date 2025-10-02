7d5934166a1bc832d382d43e438ded62acc9c8f1
ilmir_ahtyamov <ilmir_ahtyamov>
2025-10-02 23:18:29 +0300

    YH-1375 переработан виджет, добавлен перевод

--- src/pages/analytics/AnalyticsPage/ui/AnalyticsPage.tsx
+++ src/pages/analytics/AnalyticsPage/ui/AnalyticsPage.tsx
@@ -1,20 +1,6 @@
 import { InterviewPopularQuestions } from '@/widgets/interview/InterviewPopularQuestions';
 
 export const AnalyticsPage = () => {
-	const popularQuestions = [
-		{
-			title: 'Что такое Virtual DOM, и как он работает?',
-			frequencyStat: 82,
-		},
-		{
-			title: 'Что такое Virtual DOM, и как он работает?',
-			frequencyStat: 72,
-		},
-		{
-			title: 'Что такое Virtual DOM, и как он работает?',
-			frequencyStat: 62,
-		},
-	];
-	return <InterviewPopularQuestions topStat={popularQuestions} />;
+	return <InterviewPopularQuestions />;
 };
 export default AnalyticsPage;
--- src/widgets/interview/InterviewPopularQuestions/ui/InterviewPopularQuestions.tsx
+++ src/widgets/interview/InterviewPopularQuestions/ui/InterviewPopularQuestions.tsx
@@ -2,53 +2,69 @@ import { Card } from '@/shared/ui/Card';
 import { Flex } from '@/shared/ui/Flex';
 import { Text } from '@/shared/ui/Text';
 import { ReactIcon } from '@/shared/ui/Icons/ReactIcon';
-
-import { StatusIcon } from '@storybook/icons';
-
+import { Link } from 'react-router-dom';
+import { GradeChip } from '@/shared/ui/GradeChip';
+import { InterviewQuizResult } from '@/shared/config/i18n/i18nTranslations';
 import styles from './InterviewPopularQuestions.module.css';
+
+import { useTranslation } from 'react-i18next';
+import { i18Namespace } from '@/shared/config/i18n';
 
 interface InterviewPopularQuestionsProps {
 	topStat: {
+		questionId: number;
 		title: string;
 		frequencyStat: number;
 	}[];
 }
 
-export const InterviewPopularQuestions = ({ topStat }: InterviewPopularQuestionsProps) => {
+const popularQuestions: InterviewPopularQuestionsProps = [
+	{
+		questionId: 1,
+		title: 'Что такое Virtual DOM, и как он работает?',
+		frequencyStat: 82,
+	},
+	{
+		questionId: 2,
+		title: 'Что такое Virtual DOM, и как он работает?',
+		frequencyStat: 72,
+	},
+	{
+		questionId: 3,
+		title: 'Что такое Virtual DOM, и как он работает?',
+		frequencyStat: 62,
+	},
+];
+
+export const InterviewPopularQuestions = () => {
+	const { t } = useTranslation(i18Namespace.interviewQuizResult);
 	return (
 		<Card
-			title={'Самые частые вопросы в подборках'}
-			isActionPositionBottom={true}
+			isActionPositionBottom
+			withOutsideShadow
+			title={t(InterviewQuizResult.TITLE_QUESTIONS_LIST)}
 			actionTitle={'Подробнее'}
 			actionRoute={'_blank'}
-			size={'medium'}
+			size={'small'}
 		>
 			<Flex direction="column" gap={'24'}>
-				{topStat.map((item, index) => (
-					<Flex className={styles.item} key={index} direction={'row'} gap={'12'}>
-						<div className={styles.icon}>
-							<ReactIcon />
-						</div>
-
-						<Flex direction={'column'} gap={'4'}>
-							<Flex direction={'row'} gap={'4'} align={'center'}>
-								<StatusIcon color={'blue'}></StatusIcon>
-								<Text variant={'head5'}>{`${item.title}`}</Text>
+				{popularQuestions.map((item) => (
+					<Card withOutsideShadow size="small" key={item.questionId}>
+						<Link to={'_blank'} className={styles.link}>
+							<Flex className={styles.image}>
+								<ReactIcon />
 							</Flex>
-
-							<Flex direction={'row'} align={'center'} gap={'8'}>
-								<Text variant={'body2'} className={styles.populariry}>
-									Популярность:
+							<Flex direction="column" gap="8" justify={'center'} align={'start'}>
+								<Text variant="body3-accent" maxRows={1} className={styles.title}>
+									{/*{t(InterviewStatistics.QUESTION_STATS_TITLE_SHORT)}*/}
+									{item.title}
 								</Text>
-
-								<div className={styles.freq}>
-									<Text variant={'body1-accent'} color={'white-900'}>
-										{item.frequencyStat}%
-									</Text>
-								</div>
+								<Flex className={styles.populariry}>
+									<GradeChip label={'популярность'} value={item.frequencyStat} size="small" />
+								</Flex>
 							</Flex>
-						</Flex>
-					</Flex>
+						</Link>
+					</Card>
 				))}
 			</Flex>
 		</Card>
