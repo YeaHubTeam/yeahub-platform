66902740fd30cee098ac636d73ffcf8008033d7c
dennew052 <42219192+dennew052@users.noreply.github.com>
2025-10-03 18:00:43 +0500

    YH-1391: Add verification via Telegram (#908)
    * YH-1349: Add verification via Telegram
    
    * YH-1349: Fix bugs
    
    * YH-1391: Fix bugs by comments
    
    * YH-1349: Fix bugs
    
    * YH-1391: Fix bugs
    
    * YH-1391: Fix bugs
    
    * YH-1391: Fix bugs
    
    * YH-1391: Fix bugs

---
+++ src/features/authentication/telegramLink/api/telegramLinkApi.ts
@@ -0,0 +1,35 @@
+import { ApiTags } from '@/shared/config/api/apiTags';
+import { baseApi } from '@/shared/config/api/baseApi';
+import i18n from '@/shared/config/i18n/i18n';
+import { Profile } from '@/shared/config/i18n/i18nTranslations';
+import { toast } from '@/shared/ui/Toast';
+
+import { TelegramLoginBodyRequest, TelegramLoginResponse } from '@/entities/auth';
+
+import { linkTelegramApiUrls } from '../model/constants/telegramLinkConstants';
+
+export const telegramLinkApi = baseApi.injectEndpoints({
+	endpoints: (build) => ({
+		linkTelegramAccount: build.mutation<TelegramLoginResponse, TelegramLoginBodyRequest>({
+			query: (body) => ({
+				url: linkTelegramApiUrls.linkTelegram,
+				method: 'POST',
+				body,
+			}),
+			async onQueryStarted(_, { queryFulfilled, dispatch }) {
+				try {
+					await queryFulfilled;
+					toast.success(i18n.t(Profile.TELEGRAM_VERIFICATION_TOAST_TELEGRAM_LINK_SUCCESS));
+					dispatch(baseApi.util.invalidateTags([ApiTags.PROFILE]));
+				} catch (error) {
+					toast.error(i18n.t(Profile.TELEGRAM_VERIFICATION_TOAST_TELEGRAM_LINK_ERROR));
+					// eslint-disable-next-line no-console
+					console.error(error);
+				}
+			},
+		}),
+	}),
+	overrideExisting: false,
+});
+
+export const { useLinkTelegramAccountMutation } = telegramLinkApi;
--- src/pages/profile/SettingsProfilePage/ui/tabs/EmailConfirmationTab/EmailConfirmationTab.tsx
+++ src/pages/profile/SettingsProfilePage/ui/tabs/EmailConfirmationTab/EmailConfirmationTab.tsx
@@ -1,5 +1,13 @@
+import { Flex } from '@/shared/ui/Flex';
+
+import { ConfirmationTelegram } from '@/widgets/ConfirmationTelegram';
 import { EmailVerification } from '@/widgets/EmailVerification';
 
 export const EmailConfirmationTab = () => {
-	return <EmailVerification />;
+	return (
+		<Flex direction="column" gap="20">
+			<EmailVerification />
+			<ConfirmationTelegram />
+		</Flex>
+	);
 };
