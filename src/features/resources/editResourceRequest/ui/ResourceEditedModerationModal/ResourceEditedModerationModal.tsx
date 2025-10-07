30cc493a64ac914d568c627507a27c3f14f1e5fd
Vika <94902517+Viktoriyadegt@users.noreply.github.com>
2025-10-07 21:52:30 +0300

    YH-1394: moved moderation modal to shared and reused it

--- src/features/resources/editResourceRequest/ui/ResourceEditedModerationModal/ResourceEditedModerationModal.tsx
+++ src/features/resources/editResourceRequest/ui/ResourceEditedModerationModal/ResourceEditedModerationModal.tsx
@@ -1,13 +1,8 @@
 import { useTranslation } from 'react-i18next';
 
-import moderationModal from '@/shared/assets/images/emailModal.avif';
 import { i18Namespace } from '@/shared/config/i18n';
 import { Resources } from '@/shared/config/i18n/i18nTranslations';
-import { Flex } from '@/shared/ui/Flex';
-import { Modal } from '@/shared/ui/Modal';
-import { Text } from '@/shared/ui/Text';
-
-import styles from './ResourceEditedModerationModal.module.css';
+import { NotificationModal } from '@/shared/ui/NotificationModal';
 
 interface ResourceEditedModerationModalProps {
 	isOpen: boolean;
@@ -19,23 +14,13 @@ export const ResourceEditedModerationModal = ({
 	onClose,
 }: ResourceEditedModerationModalProps) => {
 	const { t } = useTranslation([i18Namespace.resources]);
+
 	return (
-		<Modal
+		<NotificationModal
 			isOpen={isOpen}
 			onClose={onClose}
-			className={styles['moderation-content']}
-			view="notification"
-		>
-			<Flex justify="center" align="center" direction="column" className={styles.modal}>
-				<img src={moderationModal} alt="email icon" loading="lazy" />
-				<Text variant="body6" className={styles['modal-title']}>
-					{t(Resources.MODAL_RESOURCE_EDIT_MODERATION_TITLE)}
-				</Text>
-
-				<Text variant="body3-accent" className={styles['modal-subtitle']}>
-					{t(Resources.MODAL_RESOURCE_EDIT_MODERATION_MESSAGE)}
-				</Text>
-			</Flex>
-		</Modal>
+			notificationTitle={t(Resources.MODAL_RESOURCE_EDIT_MODERATION_TITLE)}
+			notificationMessage={t(Resources.MODAL_RESOURCE_EDIT_MODERATION_MESSAGE)}
+		/>
 	);
 };
