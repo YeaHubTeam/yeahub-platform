30bd4085ac7a44f9a96acf71d940cd3d67685140
Anton Demidov <antondemidov811@gmail.com>
2025-10-07 21:08:27 +0300

    YH-1388: change api url

--- src/entities/skill/model/constants/skillConstants.ts
+++ src/entities/skill/model/constants/skillConstants.ts
@@ -3,5 +3,5 @@ export const MAX_SHOW_LIMIT_SKILLS = 5;
 export const skillApiUrls = {
 	getSkillsList: 'skills',
 	getSkillById: 'skills/:skillId',
-	popularSkills: 'skills/popular',
+	popularSkills: 'questions-stats/skil-quiz-popularity',
 };
