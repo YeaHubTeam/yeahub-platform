f1b93771e63c96e130604cac6238b11294315c65
toki <94902517+Viktoriyadegt@users.noreply.github.com>
2025-10-14 09:14:24 +0300

    YH-1392: add telegram unlink from account (#916)
    * YH-1392: add telegram unlink from account
    
    * YH-1392: change custom routes
    
    * YH-1392: undo name changes
    
    * YH-1392: fix response type
    
    * YH-1392: fix after merge
    
    * YH-1392: fix errors
    
    ---------
    
    Co-authored-by: Denis Pereloma <den.pereloma@gmail.com>

--- src/app/providers/router/config/routeConfig.tsx
+++ src/app/providers/router/config/routeConfig.tsx
@@ -9,7 +9,7 @@ import EducationIcon from '@/shared/assets/icons/education.svg';
 import Home from '@/shared/assets/icons/home.svg';
 import InterviewIcon from '@/shared/assets/icons/interview.svg';
 import MainIcon from '@/shared/assets/icons/main.svg';
-// import AnalyticsIcon from '@/shared/assets/icons/pieChart.svg';
+import AnalyticsIcon from '@/shared/assets/icons/pieChart.svg';
 import ProfileIcon from '@/shared/assets/icons/profile.svg';
 import QuestionsIcon from '@/shared/assets/icons/questions.svg';
 import SettingsIcon from '@/shared/assets/icons/settings.svg';
@@ -43,6 +43,8 @@ import { QuestionsTablePage } from '@/pages/admin/QuestionsTablePage';
 import { ResourceCreatePage } from '@/pages/admin/ResourceCreatePage';
 import { ResourceEditPage } from '@/pages/admin/ResourceEditPage';
 import { ResourcePage } from '@/pages/admin/ResourcePage';
+import { ResourcesPage as AdminResourcesPage } from '@/pages/admin/ResourcesPage';
+import { ResourceRequestViewPage } from '@/pages/admin/ResourceViewPage';
 import { SkillCreatePage } from '@/pages/admin/SkillCreatePage';
 import { SkillDetailPage } from '@/pages/admin/SkillDetailPage';
 import { SkillEditPage } from '@/pages/admin/SkillEditPage';
@@ -83,6 +85,7 @@ import { PublicQuestionPage } from '@/pages/landing/PublicQuestionPage';
 import { PublicQuestionsPage } from '@/pages/landing/PublicQuestionsPage';
 import { PublicQuizPage } from '@/pages/landing/PublicQuizPage';
 import { PublicQuizResultPage } from '@/pages/landing/PublicQuizResultPage';
+import { PublicResourcesPage } from '@/pages/landing/PublicResourcesPage';
 import { EditProfilePage } from '@/pages/profile/EditProfilePage';
 import { ProfilePage } from '@/pages/profile/ProfilePage';
 import { SettingsProfilePage } from '@/pages/profile/SettingsProfilePage';
@@ -103,10 +106,6 @@ import { UnAuthRoute } from '../ui/UnAuthRoute';
 import { VerifiedEmailRoute } from '../ui/VerifiedEmailRoute';
 
 import '../../../styles/App.css';
-
-import { PublicResourcesPage } from '@/pages/landing/PublicResourcesPage';
-import { ResourcesPage as AdminResourcesPage } from '@/pages/admin/ResourcesPage';
-import { ResourceRequestViewPage } from '@/pages/admin/ResourceViewPage';
 
 export const allRoles: RoleName[] = [
 	'guest',
@@ -175,13 +174,13 @@ const mainLayoutMenuItems: MenuItem[] = [
 		],
 		roles: allRoles,
 	},
-	// {
-	// 	type: 'single',
-	// 	route: ROUTES.analytics.route,
-	// 	title: i18n.t(Translation.SIDEBAR_MENU_ANALYTICS),
-	// 	icon: AnalyticsIcon,
-	// 	roles: allRoles,
-	// },
+	{
+		type: 'single',
+		route: ROUTES.analytics.route,
+		title: i18n.t(Translation.SIDEBAR_MENU_ANALYTICS),
+		icon: AnalyticsIcon,
+		roles: allRoles,
+	},
 ];
 
 const adminLayoutMenuItems: MenuItem[] = [
--- src/pages/profile/SettingsProfilePage/ui/tabs/EmailConfirmationTab/EmailConfirmationTab.tsx
+++ src/pages/profile/SettingsProfilePage/ui/tabs/EmailConfirmationTab/EmailConfirmationTab.tsx
@@ -1,13 +1,19 @@
+import { useAppSelector } from '@/shared/hooks';
 import { Flex } from '@/shared/ui/Flex';
+
+import { getFullProfile } from '@/entities/profile';
 
 import { ConfirmationTelegram } from '@/widgets/ConfirmationTelegram';
 import { EmailVerification } from '@/widgets/EmailVerification';
+import { TelegramVerifiedSection } from '@/widgets/TelegramVerifiedSection';
 
 export const EmailConfirmationTab = () => {
+	const { telegramUsername } = useAppSelector(getFullProfile);
+
 	return (
 		<Flex direction="column" gap="20">
 			<EmailVerification />
-			<ConfirmationTelegram />
+			{telegramUsername ? <TelegramVerifiedSection /> : <ConfirmationTelegram />}
 		</Flex>
 	);
 };
