90913a392cd90323620e01a260cfa62e05882208
Daud <daud_edilbiev@mail.ru>
2025-10-30 09:02:36 +0300

    YH-1455: restrict resources viewing access for unverified users

--- src/pages/wiki/RequestResourceCreatePage/ui/RequestResourceCreatePage.tsx
+++ src/pages/wiki/RequestResourceCreatePage/ui/RequestResourceCreatePage.tsx
@@ -1,5 +1,25 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { ROUTES } from '@/shared/config/router/routes';
+import { useAppSelector } from '@/shared/hooks';
+
+import { getIsEmailVerified } from '@/entities/profile';
+
 import { ResourceRequestCreateForm } from '@/features/resources/createResourceRequest';
 
-const RequestResourceCreatePage = () => <ResourceRequestCreateForm />;
+const RequestResourceCreatePage = () => {
+	const navigate = useNavigate();
+
+	const isEmailVerified = useAppSelector(getIsEmailVerified);
+
+	useEffect(() => {
+		if (!isEmailVerified) {
+			navigate(ROUTES.wiki.resources.page);
+		}
+	}, [isEmailVerified, navigate]);
+
+	return <ResourceRequestCreateForm />;
+};
 
 export default RequestResourceCreatePage;
