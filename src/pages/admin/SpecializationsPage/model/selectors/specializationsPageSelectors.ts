e5016e830a86f5f00e17b06b8f13ef0ce9c33daa
Denis Pereloma <den.pereloma@gmail.com>
2025-11-04 20:59:37 +0300

    YH-1483: refactor specializations filters

--- src/pages/admin/SpecializationsPage/model/selectors/specializationsPageSelectors.ts
+++ src/pages/admin/SpecializationsPage/model/selectors/specializationsPageSelectors.ts
@@ -1,8 +1,4 @@
 import { State } from '@/shared/config/store/State';
 
-export const getSpecializationsPageNum = (state: State) => state.specializationsPage.page || 1;
-export const getSpecializationsSearch = (state: State) => state.specializationsPage.search || '';
-export const getSpecializationsAuthor = (state: State) => state.specializationsPage.author || '';
-
 export const getSelectedSpecializations = (state: State) =>
 	state.specializationsPage.selectedSpecializations || [];
