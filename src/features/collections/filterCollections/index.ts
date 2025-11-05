5b5428e6ad2ead49958455083916e7fdcd086bec
Denis Pereloma <den.pereloma@gmail.com>
2025-11-05 16:00:35 +0300

    YH-1485: refactor filter collections

---
+++ src/features/collections/filterCollections/index.ts
@@ -0,0 +1,3 @@
+export { CollectionsFilters } from './ui/CollectionsFilters/CollectionsFilters';
+export { CollectionsFiltersSkeleton } from './ui/CollectionsFilters/CollectionsFilters.skeleton';
+export { useCollectionsFilters } from './model/hooks/useCollectionsFilters';
