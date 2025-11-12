2c05cb81392f5e9a369bddf7bc149a81a254cd54
Maksim <grigoriev.ma2014@yandex.ru>
2025-11-12 22:19:23 +0300

    YH-1497: add i18n keys

--- src/shared/config/i18n/i18nTranslations.ts
+++ src/shared/config/i18n/i18nTranslations.ts
@@ -364,6 +364,7 @@ export enum Specializations {
 	PROGRESS_TABLE_QUESTIONS = 'progress.table.questions',
 	PROGRESS_TABLE_PROGRESS = 'progress.table.progress',
 	PROGRESS_TABLE_NUMBER = 'progress.table.number',
+	SORT_AUTH_TITLE = 'sort.auth.title',
 }
 
 export enum Analytics {
