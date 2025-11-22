57866e764ada6ff4450800f1325c8a6d3ebb9d6c
Denis Pereloma <den.pereloma@gmail.com>
2025-11-22 16:57:14 +0300

    YH-1463: fix edit profile tabs skeleton

--- src/shared/ui/Tabs/Tabs.skeleton.tsx
+++ src/shared/ui/Tabs/Tabs.skeleton.tsx
@@ -2,21 +2,19 @@ import classNames from 'classnames';
 import { Key } from 'react';
 
 import { Flex } from '@/shared/ui/Flex';
-import { TabsProps } from '@/shared/ui/Tabs/Tabs';
 import styles from '@/shared/ui/Tabs/Tabs.module.css';
 import { TextSkeleton } from '@/shared/ui/Text';
 
-export const TabsSkeleton = <T,>({ tabs }: Partial<TabsProps<T>>) => {
+interface TabsSkeleton {
+	tabs: string[];
+}
+
+export const TabsSkeleton = ({ tabs }: TabsSkeleton) => {
 	return (
 		<Flex direction="column" gap="28" className={styles['tab-container']}>
 			<Flex componentType="ul" gap="24" className={styles['tab-list']}>
 				{tabs?.map((tab) => (
-					<li
-						key={tab.id as Key}
-						className={classNames(styles['tab-item'])}
-						role="tab"
-						tabIndex={0}
-					>
+					<li key={tab as Key} className={classNames(styles['tab-item'])} role="tab" tabIndex={0}>
 						<TextSkeleton variant="body4" width={200} />
 					</li>
 				))}
