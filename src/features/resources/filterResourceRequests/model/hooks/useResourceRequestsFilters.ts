69dfa602f5e43bec166285a49fce2a2f39dcab24
Ubermensch <ubermensch1937@bk.ru>
2025-11-26 16:41:45 +0300

    YH-1522: fix error with hasFilters

--- src/features/resources/filterResourceRequests/model/hooks/useResourceRequestsFilters.ts
+++ src/features/resources/filterResourceRequests/model/hooks/useResourceRequestsFilters.ts
@@ -13,8 +13,8 @@ export const useResourceRequestsFilters = (initialParams: ResourceRequestsFilter
 		(filters.page || 1) > 1 ||
 		Boolean(filters.title) ||
 		filters.status !== 'all' ||
-		(filters?.skills || [])?.length > 0;
-	(filters?.types || [])?.length > 0;
+		(filters?.skills || [])?.length > 0 ||
+		(filters?.types || [])?.length > 0;
 
 	const onChangeTitle = (title: ResourceRequestsFilterParams['title']) => {
 		onFilterChange({ title, page: 1 });
