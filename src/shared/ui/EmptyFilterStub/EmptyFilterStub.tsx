5ffe1daf2072f7a797e36eeba71cf1606baf6c11
joyart78 <peta04033@gmail.com>
2025-12-01 21:34:22 +0300

    YH-1517: Add empty state for filtered applications

--- src/shared/ui/EmptyFilterStub/EmptyFilterStub.tsx
+++ src/shared/ui/EmptyFilterStub/EmptyFilterStub.tsx
@@ -1,10 +1,13 @@
 import { useTranslation } from 'react-i18next';
 
+import NotFound from '@/shared/assets/images/notFound.avif';
 import { i18Namespace } from '@/shared/config/i18n';
 import { Translation } from '@/shared/config/i18n/i18nTranslations';
 import { useScreenSize } from '@/shared/hooks';
 import { Button } from '@/shared/ui/Button';
+import { Card } from '@/shared/ui/Card';
 import { emptyStubTestIds } from '@/shared/ui/EmptyFilterStub/constants';
+import { Flex } from '@/shared/ui/Flex';
 import { Text } from '@/shared/ui/Text';
 
 import styles from './EmptyFilerStub.module.css';
@@ -19,23 +22,28 @@ export const EmptyFilterStub = ({ resetFilters, text }: EmptyStubProps) => {
 	const { isMobileS } = useScreenSize();
 
 	return (
-		<>
-			<Text
-				dataTestId={emptyStubTestIds.emptyStubText}
-				variant={isMobileS ? 'body2-accent' : 'body5-accent'}
-				className={styles.text}
-			>
-				{t(Translation.STUB_FILTER_TITLE, { text: text ? `“${text}”` : undefined })}
-			</Text>
-			<Button
-				dataTestId={emptyStubTestIds.emptyStubButton}
-				size="large"
-				variant="outline"
-				onClick={resetFilters}
-				className={styles.button}
-			>
-				{t(Translation.STUB_FILTER_SUBMIT)}
-			</Button>
-		</>
+		<Card withShadow={true} className={styles.container}>
+			<Flex justify={'center'} align={'center'}>
+				<Flex justify={'center'} direction={'column'} align={'center'} className={styles.card}>
+					<img src={NotFound} alt="Not Found" className={styles.img} />
+					<Text
+						dataTestId={emptyStubTestIds.emptyStubText}
+						variant={isMobileS ? 'body2-accent' : 'body5-accent'}
+						className={styles.text}
+					>
+						{t(Translation.STUB_FILTER_TITLE, { text: text ? `“${text}”` : undefined })}
+					</Text>
+					<Button
+						dataTestId={emptyStubTestIds.emptyStubButton}
+						size="large"
+						variant="outline"
+						onClick={resetFilters}
+						className={styles.button}
+					>
+						{t(Translation.STUB_FILTER_SUBMIT)}
+					</Button>
+				</Flex>
+			</Flex>
+		</Card>
 	);
 };
