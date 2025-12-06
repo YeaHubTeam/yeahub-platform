07cec7292aee041632c9947d4eb3683091635b9c
Anushavan <anushavan777@gmail.com>
2025-12-06 16:23:33 +0400

    YH-1572: fixed conflicts

--- src/features/specialization/createSpecialization/api/createSpecializationApi.ts
+++ src/features/specialization/createSpecialization/api/createSpecializationApi.ts
@@ -1,16 +1,6 @@
-<<<<<<< HEAD
 import { ROUTES, i18n, Translation, ApiTags, baseApi, ExtraArgument } from '@/shared/config';
 import { route } from '@/shared/libs';
-=======
-import { ApiTags } from '@/shared/config/api/apiTags';
-import { baseApi } from '@/shared/config/api/baseApi';
-import i18n from '@/shared/config/i18n/i18n';
-import { Translation } from '@/shared/config/i18n/i18nTranslations';
-import { ROUTES } from '@/shared/config/router/routes';
-import { ExtraArgument } from '@/shared/config/store/types';
-import { route } from '@/shared/helpers/route';
 import { handleApiError } from '@/shared/libs/query/handleApiError/handleApiError';
->>>>>>> 895ee717 (YH-1572: add translation and handlig errors)
 import { toast } from '@/shared/ui/Toast';
 
 import { createSpecializationApiUrls } from '../model/constants/createSpecializationConstants';
