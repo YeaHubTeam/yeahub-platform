c43fc1a535f719b3e3e0e94754db3d428e0f9751
Anushavan <anushavan777@gmail.com>
2025-12-06 12:54:01 +0400

    YH-1572: add translation and handlig errors

--- src/features/specialization/createSpecialization/api/createSpecializationApi.ts
+++ src/features/specialization/createSpecialization/api/createSpecializationApi.ts
@@ -1,8 +1,20 @@
+<<<<<<< HEAD
 import { ROUTES, i18n, Translation, ApiTags, baseApi, ExtraArgument } from '@/shared/config';
 import { route } from '@/shared/libs';
+=======
+import { ApiTags } from '@/shared/config/api/apiTags';
+import { baseApi } from '@/shared/config/api/baseApi';
+import i18n from '@/shared/config/i18n/i18n';
+import { Translation } from '@/shared/config/i18n/i18nTranslations';
+import { ROUTES } from '@/shared/config/router/routes';
+import { ExtraArgument } from '@/shared/config/store/types';
+import { route } from '@/shared/helpers/route';
+import { handleApiError } from '@/shared/libs/query/handleApiError/handleApiError';
+>>>>>>> 895ee717 (YH-1572: add translation and handlig errors)
 import { toast } from '@/shared/ui/Toast';
 
 import { createSpecializationApiUrls } from '../model/constants/createSpecializationConstants';
+import { getSpecializationsErrorMessage } from '../model/lib/utils/getApiErrorMessage';
 import {
 	CreateSpecializationBodyRequest,
 	CreateSpecializationResponse,
@@ -26,7 +38,7 @@ export const createSpecializationApi = baseApi.injectEndpoints({
 					typedExtra.navigate(route(ROUTES.admin.specializations.details.page, result.data.id));
 					toast.success(i18n.t(Translation.TOAST_SPECIALIZATION_CREATE_SUCCESS));
 				} catch (error) {
-					//const status = error.error?.status;
+					toast.error(i18n.t(handleApiError(error, getSpecializationsErrorMessage)));
 					console.error(error);
 				}
 			},
--- src/features/specialization/createSpecialization/index.ts
+++ src/features/specialization/createSpecialization/index.ts
@@ -1,3 +1,4 @@
 export { useCreateSpecializationMutation } from './api/createSpecializationApi';
 export { SpecializationCreateForm } from './ui/SpecializationCreateForm/SpecializationCreateForm';
 export { specializationCreateHandlers } from './api/__mock__';
+export { getSpecializationsErrorMessage } from './model/lib/utils/getApiErrorMessage';
