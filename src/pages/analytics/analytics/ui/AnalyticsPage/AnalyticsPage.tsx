7d2e92aa2519650ff749dbafce65e040bd56f580
Denis Pereloma <den.pereloma@gmail.com>
2025-12-21 09:40:03 +0300

    YH-1453: hide widget

--- src/pages/analytics/analytics/ui/AnalyticsPage/AnalyticsPage.tsx
+++ src/pages/analytics/analytics/ui/AnalyticsPage/AnalyticsPage.tsx
@@ -12,7 +12,7 @@ import { PopularQuestionsWidget } from '@/widgets/analytics/PopularQuestionsWidg
 import { PopularSkillsWidget } from '@/widgets/analytics/PopularSkillsWidget';
 import { SkillsProficiencyWidget } from '@/widgets/analytics/SkillsProficiencyWidget';
 import { SpecializationProgressWidget } from '@/widgets/analytics/SpecializationProgressWidget';
-import { UsersRatingWidget } from '@/widgets/analytics/UsersRatingWidget';
+// import { UsersRatingWidget } from '@/widgets/analytics/UsersRatingWidget';
 
 export const AnalyticsPage = () => {
 	const { isSmallScreen, isLaptop, isTablet } = useScreenSize();
@@ -28,7 +28,7 @@ export const AnalyticsPage = () => {
 	return (
 		<Flex wrap={isSmallScreen ? 'wrap' : 'nowrap'} gap="20">
 			<Flex direction="column" gap="20" maxWidth={isSmallScreen}>
-				<UsersRatingWidget />
+				{/*<UsersRatingWidget />*/}
 				<Flex direction={isTablet || isLaptop ? 'row' : 'column'} gap="20" maxWidth={isSmallScreen}>
 					<SkillsProficiencyWidget />
 					<MostDifficultQuestionsWidget />
