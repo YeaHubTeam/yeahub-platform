34989bd1e59b3162f0ef8c61f15dcbeadb21ae52
Daniil Leonov <142981918+Beyondofsoul@users.noreply.github.com>
2025-12-26 00:54:32 +0300

    YH-1539: Added filter button and Pagination (#1053)
    * YH-1539: Added filter button and Pagination
    
    * YH-1539: Fixed comments

---
+++ src/features/topic/filterTopics/index.ts
@@ -0,0 +1,2 @@
+export { TopicsFilters } from './ui/TopicsFilters/TopicsFilters';
+export { useTopicsFilters } from './model/hooks/useTopicsFilters';
--- src/pages/admin/topic/topics/ui/TopicsPage/TopicsPage.tsx
+++ src/pages/admin/topic/topics/ui/TopicsPage/TopicsPage.tsx
@@ -1,9 +1,12 @@
 import { SelectedAdminEntities, useAppDispatch, useAppSelector } from '@/shared/libs';
 import { Card } from '@/shared/ui/Card';
 import { Flex } from '@/shared/ui/Flex';
+import { TablePagination } from '@/shared/ui/TablePagination';
 
 import { useGetTopicsListQuery } from '@/entities/topic';
 
+
+import { TopicsFilters, useTopicsFilters } from '@/features/topic/filterTopics';
 import { DeleteTopicsButton } from '@/features/topics/deleteTopics';
 
 import { SearchSection } from '@/widgets/SearchSection';
@@ -11,11 +14,18 @@ import { TopicsTable } from '@/widgets/topic/TopicsTable';
 
 import { getSelectedTopics } from '../../model/selectors/topicsPageSelectors';
 import { topicsPageActions } from '../../model/slices/topicsPageSlice';
-
 const TopicsPage = () => {
 	const dispatch = useAppDispatch();
 
-	const { data: topics } = useGetTopicsListQuery({ page: 1 });
+	const { filters, hasFilters, onChangeTitle, onChangeSkillIds, onChangePage, onResetFilters } =
+		useTopicsFilters({ page: 1 });
+
+	const { data: topicsWithTitle } = useGetTopicsListQuery({
+		page: filters.page,
+		limit: 10,
+		title: filters.title,
+		skillIds: filters.skillIds,
+	});
 	const selectedTopics = useAppSelector(getSelectedTopics);
 
 	const onSelectTopics = (ids: SelectedAdminEntities) => {
@@ -28,22 +38,36 @@ const TopicsPage = () => {
 
 	return (
 		<Flex componentType="main" direction="column" gap="24">
-			<SearchSection
-				to="create"
-				showRemoveButton={selectedTopics.length > 0}
-				renderRemoveButton={() => (
-					<DeleteTopicsButton
-						topicsToRemove={selectedTopics}
-						onSuccess={() => clearSelectedTopics()}
-					/>
-				)}
+<SearchSection
+    to="create"
+    onSearch={onChangeTitle}
+    searchValue={filters.title}
+    renderFilter={() => <TopicsFilters filters={filters} onChangeSkillIds={onChangeSkillIds} />}
+    onResetFilters={onResetFilters}
+    showResetFilterButton={hasFilters}
+    hasFilters={Boolean((filters.skillIds || []).length)}
+    showRemoveButton={selectedTopics.length > 0}
+    renderRemoveButton={() => (
+        <DeleteTopicsButton
+            topicsToRemove={selectedTopics}
+            onSuccess={() => clearSelectedTopics()}
+        />
+    )}
+/>
+
 			/>
 			<Card>
 				<TopicsTable
-					topics={topics?.data}
+					topics={topicsWithTitle?.data}
 					selectedTopics={selectedTopics}
 					onSelectTopics={onSelectTopics}
 					onDeleteSuccess={() => clearSelectedTopics()}
+				/>
+				<TablePagination
+					page={filters.page || 1}
+					onChangePage={onChangePage}
+					limit={topicsWithTitle?.limit ?? 10}
+					total={topicsWithTitle?.total ?? 0}
 				/>
 			</Card>
 		</Flex>
