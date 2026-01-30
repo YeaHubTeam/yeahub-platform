c344b787f0d93b81ff0accdcfc5f9e990c6716dd
Pavel Henuz <mshenuz@gmail.com>
2026-01-30 11:58:29 +0300

    YH-1690: add character counter and max length limit to TextArea component.

--- src/shared/ui/TextArea/TextArea.tsx
+++ src/shared/ui/TextArea/TextArea.tsx
@@ -6,18 +6,32 @@ import styles from './TextArea.module.css';
 import { TextAreaProps } from './types';
 
 export const TextArea = forwardRef<HTMLTextAreaElement, TextAreaProps>(
-	({ className, state = 'default', isReadonly, disabled, ...otherProps }, ref): JSX.Element => {
+	(
+		{ className, state = 'default', isReadonly, disabled, limit, value, ...otherProps },
+		ref,
+	): JSX.Element => {
+		const valueLength = String(value ?? '').length;
+
 		return (
-			<textarea
-				ref={ref}
-				className={classnames(styles[textAreaClassName], className, {
-					[styles[`${textAreaClassName}-error`]]: state === 'error',
-					[styles[`${textAreaClassName}-valid`]]: state === 'valid',
-					[styles[`${textAreaClassName}-disabled`]]: isReadonly || disabled,
-				})}
-				disabled={isReadonly || disabled}
-				{...otherProps}
-			/>
+			<div className={styles.wrapper}>
+				<textarea
+					ref={ref}
+					maxLength={limit}
+					className={classnames(styles[textAreaClassName], className, {
+						[styles[`${textAreaClassName}-error`]]: state === 'error',
+						[styles[`${textAreaClassName}-valid`]]: state === 'valid',
+						[styles[`${textAreaClassName}-disabled`]]: isReadonly || disabled,
+					})}
+					disabled={isReadonly || disabled}
+					{...otherProps}
+				/>
+
+				{limit && (
+					<div className={styles.counter}>
+						{valueLength}/{limit}
+					</div>
+				)}
+			</div>
 		);
 	},
 );
--- src/shared/ui/TextArea/types.ts
+++ src/shared/ui/TextArea/types.ts
@@ -1,4 +1,5 @@
 export interface TextAreaProps extends React.ComponentPropsWithoutRef<'textarea'> {
 	isReadonly?: boolean;
 	state?: 'default' | 'error' | 'valid';
+	limit?: number;
 }
