fdc24f8f06dcf5e8997103411fec25f45428f483
Crouch <crouch365md@gmail.com>
2026-01-31 21:24:08 +0500

    YH-1581: resolved the conflict (part 2)

--- src/entities/resource/api/__mock__/index.ts
+++ src/entities/resource/api/__mock__/index.ts
@@ -1,5 +1,11 @@
 import { myRequestsResoursesMock } from './myRequestsResoursesMock';
 import { resourceByIdMock } from './resourceMock';
 import { resourcesListMock } from './resourcesListMock';
+import { resourcesRequestsMock } from './resourcesRequestsMock';
 
-export const resourcesHandlers = [resourcesListMock, resourceByIdMock, myRequestsResoursesMock];
+export const resourcesHandlers = [
+	resourcesListMock,
+	resourceByIdMock,
+	myRequestsResoursesMock,
+	resourcesRequestsMock,
+];
