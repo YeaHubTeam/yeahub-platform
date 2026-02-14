2746fd984ad65d1f1e51fe6e2f4bbad256a01290
Vitaly Lebedkov <lebedkov.2000@mail.ru>
2026-02-14 19:19:59 +0700

    YH-1726: fix password validation rules in passwordRecovery module

--- src/widgets/authentication/passwordRecovery/lib/validation/passwordRecoverySchema.ts
+++ src/widgets/authentication/passwordRecovery/lib/validation/passwordRecoverySchema.ts
@@ -6,11 +6,13 @@ import { passwordRules } from '@/shared/libs';
 export const passwordRecoverySchema = yup.object().shape({
 	password: yup
 		.string()
-		.required(i18n.t(Translation.VALIDATION_REQUIRED))
 		.min(8, ({ min }) => i18n.t(Translation.VALIDATION_LENGTH_MIN, { count: min }))
-		.matches(passwordRules, () => i18n.t(Translation.VALIDATION_PASSWORD_WEAK)),
+		.matches(passwordRules, () => i18n.t(Translation.VALIDATION_PASSWORD_WEAK))
+		.required(i18n.t(Translation.VALIDATION_REQUIRED)),
 	confirmPassword: yup
 		.string()
+		.min(8, ({ min }) => i18n.t(Translation.VALIDATION_LENGTH_MIN, { count: min }))
+		.matches(passwordRules, () => i18n.t(Translation.VALIDATION_PASSWORD_WEAK))
 		.oneOf([yup.ref('password')], () => i18n.t(Translation.VALIDATION_PASSWORD_SIMILAR))
 		.required(i18n.t(Translation.VALIDATION_REQUIRED)),
 });
