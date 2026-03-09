d64f37ae5e867faebfdd8383ffdeabe3f6dfb3b2
SoundTroniX <maks.yangirov@mail.ru>
2026-03-09 14:07:14 +0500

    YH-1773: add error handling for skill edit

--- src/features/skill/editSkill/model/types/skillEditPageTypes.ts
+++ src/features/skill/editSkill/model/types/skillEditPageTypes.ts
@@ -4,3 +4,10 @@ export type EditSkillFormValues = CreateOrEditSkillFormValues;
 
 export type EditSkillBodyRequest = EditSkillFormValues;
 export type EditSkillResponse = Skill;
+export type EditSkillError =
+	| 'auth.auth.unauthorized'
+	| 'auth.user.verified'
+	| 'skill.skill.not_found'
+	| 'skill.skill.title.conflict'
+	| 'tinify.tinify.compress_failed'
+	| 'tinify.tinify.resize_failed';
