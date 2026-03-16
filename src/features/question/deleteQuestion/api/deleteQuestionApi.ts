105085460bdc56717795f672a6f4721b2e022fa1
nickKor29 <nickkor2912@gmail.com>
2026-03-16 11:24:51 +0300

    YH-1794: added error handling for delete question (#1172)

--- src/features/question/deleteQuestion/api/deleteQuestionApi.ts
+++ src/features/question/deleteQuestion/api/deleteQuestionApi.ts
@@ -1,9 +1,10 @@
 import { i18n, Translation, ApiTags, baseApi, ROUTES, ExtraArgument } from '@/shared/config';
-import { route } from '@/shared/libs';
+import { route, handleApiError } from '@/shared/libs';
 import { toast } from '@/shared/ui/Toast';
 
 import { Question } from '@/entities/question';
 
+import { getDeleteQuestionApiErrorMessage } from '../lib/utils/getDeleteQuestionApiErrorMessage';
 import { deleteQuestionApiUrls } from '../model/constants/deleteQuestionConstants';
 
 const deleteQuestionApi = baseApi.injectEndpoints({
@@ -21,7 +22,7 @@ const deleteQuestionApi = baseApi.injectEndpoints({
 					toast.success(i18n.t(Translation.TOAST_QUESTIONS_DELETE_SINGLE_SUCCESS));
 					typedExtra.navigate(ROUTES.admin.questions.page);
 				} catch (error) {
-					toast.error(i18n.t(Translation.TOAST_QUESTIONS_DELETE_SINGLE_FAILED));
+					toast.error(i18n.t(handleApiError(error, getDeleteQuestionApiErrorMessage)));
 					// eslint-disable-next-line no-console
 					console.error(error);
 				}
