917ea7a39e38f46489455b7cb921347ad54cfb8b
PerelomaDenis <88330372+PerelomaDenis@users.noreply.github.com>
2026-05-10 11:08:11 +0300

    YH-1817: add parms mock list tasks (#1225)

--- src/entities/task/api/__mocks__/data/taskCategoriesMock.ts
+++ src/entities/task/api/__mocks__/data/taskCategoriesMock.ts
@@ -1,6 +1,4 @@
-import { programmingLanguagesMock } from '@/entities/programmingLanguage/@x/task';
-
-import type { TaskCategory, TaskSolution } from '../../../model/types/task';
+import { TaskCategory } from '../../../model/types/task';
 
 export const taskCategoriesMock: TaskCategory[] = [
 	{
@@ -299,23 +297,5 @@ export const taskCategoriesMock: TaskCategory[] = [
 		isActive: true,
 		parentCode: null,
 		childrenCodes: [],
-	},
-];
-
-export const tasksProfileSolutionsMock: TaskSolution[] = [
-	{
-		id: '1',
-		profileId: '101',
-		taskId: '201',
-		status: 'solved',
-		attemptsCount: 3,
-		lastAttemptAt: '2024-01-01T10:00:00.000Z',
-		solvedAt: '2024-01-01T10:30:00.000Z',
-		bestExecutionTime: 150,
-		bestMemoryUsage: 512,
-		solutionCode: 'function solution() {}',
-		solutionLanguage: programmingLanguagesMock.javascript,
-		createdAt: '2024-01-01T09:00:00.000Z',
-		updatedAt: '2024-01-01T10:30:00.000Z',
 	},
 ];
