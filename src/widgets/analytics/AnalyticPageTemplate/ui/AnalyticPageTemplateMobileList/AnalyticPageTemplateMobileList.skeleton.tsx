56dfaed3a275f24a266b42513ece9d26ad00d6a3
Gnarkill33 <gnarkill33@gmail.com>
2026-05-19 22:18:11 +0300

    YH-1564: add analytic page template mobile list skeleton and update index

---
+++ src/widgets/analytics/AnalyticPageTemplate/ui/AnalyticPageTemplateMobileList/AnalyticPageTemplateMobileList.skeleton.tsx
@@ -0,0 +1,41 @@
+import { Card } from '@/shared/ui/Card';
+import { Flex } from '@/shared/ui/Flex';
+import { ImageWithWrapperSkeleton } from '@/shared/ui/ImageWithWrapper';
+import { StatusChipSkeleton } from '@/shared/ui/StatusChip';
+import { TextSkeleton } from '@/shared/ui/Text';
+
+interface AnalyticPageTemplateMobileListSkeletonProps {
+	withChips?: boolean;
+	fieldsCount?: number;
+	withImage?: boolean;
+	rowsCount?: number;
+}
+
+export const AnalyticPageTemplateMobileListSkeleton = ({
+	withChips = false,
+	fieldsCount = 1,
+	rowsCount = 10,
+	withImage = false,
+}: AnalyticPageTemplateMobileListSkeletonProps) => {
+	return (
+		<Flex componentType="ul" direction="column" gap="16">
+			{Array.from({ length: rowsCount }).map((_, index) => (
+				<li key={index}>
+					<Card>
+						<Flex gap="12" direction="column">
+							{withChips && <StatusChipSkeleton />}
+							{withImage && <ImageWithWrapperSkeleton />}
+							<TextSkeleton variant="body3-accent" width="70%" />
+							{Array.from({ length: fieldsCount }).map((_, fieldIndex) => (
+								<Flex key={fieldIndex} justify="between">
+									<TextSkeleton variant="body3-accent" width="25%" />
+									<TextSkeleton variant="body3-accent" width="15%" />
+								</Flex>
+							))}
+						</Flex>
+					</Card>
+				</li>
+			))}
+		</Flex>
+	);
+};
