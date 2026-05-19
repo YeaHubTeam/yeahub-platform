070f85c6af137edd72f0bc5d1c21e3779649f724
Aleksandr <aleksandera9@gmail.com>
2026-05-19 11:30:49 +0300

    YH-1945: fix load url parameters for feature flags filters

--- src/features/featureFlag/model/hooks/useGetFeatureFlagFilterParams.ts
+++ src/features/featureFlag/model/hooks/useGetFeatureFlagFilterParams.ts
@@ -1,3 +1,5 @@
+import { ClientType } from '@/entities/featureFlag';
+
 import { FeatureFlagFiltersParams } from '../types/filters';
 
 export const useGetFeatureFlagFilterParams = (initialParams: FeatureFlagFiltersParams) => {
@@ -6,6 +8,12 @@ export const useGetFeatureFlagFilterParams = (initialParams: FeatureFlagFiltersP
 
 	const currentParams: FeatureFlagFiltersParams = {
 		page: parsedParams.page ? Number(parsedParams.page) : initialParams.page,
+		search: parsedParams.search,
+		enabled: parsedParams.enabled ? parsedParams.enabled === 'true' : initialParams.enabled,
+		selectedRoles: parsedParams.selectedRoles
+			? parsedParams.selectedRoles.split(',').map(Number)
+			: initialParams.selectedRoles,
+		clientType: parsedParams.clientType as ClientType,
 	};
 
 	return currentParams;
