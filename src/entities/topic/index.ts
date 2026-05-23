8a361485dab3f7a94721367ec2c17f3cc8e257db
Denis Pereloma <den.pereloma@gmail.com>
2026-05-23 23:38:40 +0300

    YH-1937: fix error

--- src/entities/topic/index.ts
+++ src/entities/topic/index.ts
@@ -8,12 +8,7 @@ export { useGetTopicsListQuery, useGetTopicByIdQuery } from './api/topicApi';
 export { TopicAdditionalInfo } from './ui/TopicAdditionalInfo/TopicAdditionalInfo';
 
 export { TopicCard } from './ui/TopicCard/TopicCard';
-export { TopicFilterField } from './ui/TopicFilterField/TopicFilterField';
-
-export { TopicFilterField } from './ui/TopicFilterField/TopicFilterField';
 
 export { topicHandlers } from './api/__mock__';
 
 export { topicsMocks } from './api/__mock__/data';
-
-export { TopicFilterField } from './ui/TopicFilterField/TopicFilterField';
