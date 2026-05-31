24ac6871199f926ac902aad685387bf5b8ea8636
Aleksandr <aleksandera9@gmail.com>
2026-05-31 15:56:43 +0300

    YH-2031: fix ReferralLinkCreatePage skeleton

--- src/pages/admin/referralLink/ReferralLinkCreate/ui/ReferralLinkCreatePage/ReferralLinkCreatePage.skeleton.tsx
+++ src/pages/admin/referralLink/ReferralLinkCreate/ui/ReferralLinkCreatePage/ReferralLinkCreatePage.skeleton.tsx
@@ -1,13 +1,5 @@
 import { ReferralLinkCreateFormSkeleton } from '@/features/referralLinks/createReferralLink';
 
-import { PageWrapper } from '@/widgets/PageWrapper';
-
 export const ReferralLinkCreatePageSkeleton = () => {
-	const content = <ReferralLinkCreateFormSkeleton />;
-
-	return (
-		<PageWrapper hasData stubs={{}} roles={['admin']} content={content}>
-			{({ content }) => content}
-		</PageWrapper>
-	);
+	return <ReferralLinkCreateFormSkeleton />;
 };
