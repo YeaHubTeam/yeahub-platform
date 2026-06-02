7f3fb1261cf3b6a503837b0d655ad0e82e98ae2e
Artem <89ufisum@gmail.com>
2026-06-02 23:51:01 +0300

    YH-2005: add mock for create feature flag (#1264)
    * YH-2005: add mock for create feature flag
    
    * YH-2005: fix: resolve review comments

--- src/app/msw/handlers.ts
+++ src/app/msw/handlers.ts
@@ -23,6 +23,7 @@ import { usersRatingHandlers } from '@/entities/user';
 
 import { createCollectionHandlers } from '@/features/collections/createCollection';
 import { createCompanyHandlers } from '@/features/company/createCompany';
+import { createFeatureFlagHandlers } from '@/features/featureFlag/createFeatureFlag';
 import { questionCreateHandlers } from '@/features/question/createQuestion';
 import { questionDeleteHandlers } from '@/features/question/deleteQuestion';
 import { questionEditHandlers } from '@/features/question/editQuestion';
@@ -75,6 +76,7 @@ export const handlers = [
 	...createCollectionHandlers,
 	...topicEditHandlers,
 	...createCompanyHandlers,
+	...createFeatureFlagHandlers,
 	...programmingLanguageHandlers,
 	...referralLinksHandlers,
 	...referralLinkDeleteHandlers,
