54a85ba413e2a1a263f55b34f92ce30b21f1d3ad
Denis Pereloma <den.pereloma@gmail.com>
2026-06-28 22:20:35 +0300

    YH-2074: fix

--- src/shared/ui/FormField/FormField.test.tsx
+++ src/shared/ui/FormField/FormField.test.tsx
@@ -2,14 +2,15 @@ import { screen } from '@testing-library/react';
 
 import { renderComponent } from '@/shared/libs';
 
-import { FlexProps } from '../Flex';
-
 import { formFieldTestIds } from './constants';
 import { FormField, FormFieldProps } from './FormField';
 import { FormFieldSkeleton } from './FormField.skeleton';
 
-const render = (props: FormFieldProps, dataTestId: keyof typeof formFieldTestIds) => {
-	renderComponent(<FormField {...props} />);
+const label = 'Название коллекции';
+const description = 'Добавьте название коллекции';
+
+const render = ({ label, ...props }: FormFieldProps, dataTestId: keyof typeof formFieldTestIds) => {
+	renderComponent(<FormField label={label} {...props} />);
 	const component = screen.getByTestId(formFieldTestIds[dataTestId]);
 	return component;
 };
@@ -20,14 +21,6 @@ const renderSkeleton = (props?: Partial<FormFieldProps>) => {
 	return skeleton;
 };
 
-jest.mock('@/shared/ui/Flex', () => ({
-	Flex: ({ gap, dataTestId, className, children }: FlexProps) => (
-		<div data-testid={dataTestId} data-gap={gap} className={className}>
-			{children}
-		</div>
-	),
-}));
-
 describe('FormField', () => {
 	test('constants are defined', () => {
 		expect(formFieldTestIds).toBeDefined();
@@ -36,40 +29,53 @@ describe('FormField', () => {
 		expect(formFieldTestIds.formFieldLabel).toBeDefined();
 	});
 
+	test('wrapper', () => {
+		const component = render(
+			{ label, children: <div data-testid="children">Дочерний компонент</div> },
+			'formFieldChildren',
+		);
+		expect(component).toBeInTheDocument();
+		expect(component).toHaveClass('form-field');
+	});
+
 	test('label', () => {
-		const label = 'Название коллекции';
 		const labelText = render({ label, children: null }, 'formFieldLabel');
 		expect(labelText.textContent).toBe(label);
+		expect(labelText).toHaveClass('body4');
+		expect(labelText).toHaveClass('text-black-800');
 	});
 
 	test('description', () => {
-		const label = 'Название коллекции';
-		const description = 'Добавьте название коллекции';
 		const descriptionText = render({ label, description, children: null }, 'formFieldDescription');
 		expect(descriptionText.textContent).toBe(description);
+		expect(descriptionText).toHaveClass('body2');
+		expect(descriptionText).toHaveClass('text-black-800');
 	});
 
 	test('gap column', () => {
-		const label = 'Название коллекции';
-		const description = 'Добавьте название коллекции';
 		const component = render(
 			{ label, description, children: null, direction: 'column' },
 			'formFieldChildren',
 		);
-		expect(component).toHaveAttribute('data-gap', '20');
+		expect(component).toHaveClass('gap20');
 	});
 
 	test('gap row', () => {
-		const label = 'Название коллекции';
-		const description = 'Добавьте название коллекции';
 		const component = render({ label, description, children: null }, 'formFieldChildren');
-		expect(component).toHaveAttribute('data-gap', '120');
+		expect(component).toHaveClass('gap120');
 	});
 
 	test('limit width', () => {
-		const label = 'Название коллекции';
 		const component = render({ label, children: null, isLimitWidth: true }, 'formFieldChildren');
 		expect(component.className).toContain('limit-width');
+	});
+
+	test('children', () => {
+		render(
+			{ label, children: <div data-testid="children">Дочерний компонент</div> },
+			'formFieldChildren',
+		);
+		expect(screen.getByTestId('children')).toBeInTheDocument();
 	});
 });
 
@@ -77,20 +83,5 @@ describe('FormFieldSkeleton', () => {
 	test('render', () => {
 		const skeleton = renderSkeleton();
 		expect(skeleton).toBeInTheDocument();
-	});
-
-	test('gap row', () => {
-		const skeleton = renderSkeleton();
-		expect(skeleton).toHaveAttribute('data-gap', '120');
-	});
-
-	test('gap column', () => {
-		const skeleton = renderSkeleton({ direction: 'column' });
-		expect(skeleton).toHaveAttribute('data-gap', '20');
-	});
-
-	test('limit width', () => {
-		const skeleton = renderSkeleton({ isLimitWidth: true });
-		expect(skeleton.className).toContain('limit-width');
 	});
 });
