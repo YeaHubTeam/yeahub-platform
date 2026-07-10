585db20e811f6a04ed09f6b18075085bf44e35ae
vely1012 <damyanovv228666@gmail.com>
2026-07-10 17:21:20 +0300

    YH-2197: improve skeletons for SpecializationDetailPage
    - Replace mistakenly left DeleteSpecializationButtonSkeleton with
    regular DeleteButtonSkeleton

--- src/pages/admin/specialization/specializationDetail/ui/SpecializationDetailPage/SpecializationDetailPage.skeleton.tsx
+++ src/pages/admin/specialization/specializationDetail/ui/SpecializationDetailPage/SpecializationDetailPage.skeleton.tsx
@@ -2,16 +2,15 @@ import React from 'react';
 
 import { BackHeaderSkeleton } from '@/shared/ui/BackHeader';
 import { ButtonSkeleton } from '@/shared/ui/Button';
+import { DeleteButtonSkeleton } from '@/shared/ui/DeleteButton';
 
 import { SpecializationCardSkeleton } from '@/entities/specialization';
-
-import { DeleteQuestionButtonSkeleton } from '@/features/question/deleteQuestion';
 
 export const SpecializationDetailPageSkeleton = () => {
 	return (
 		<>
 			<BackHeaderSkeleton>
-				<DeleteQuestionButtonSkeleton isDetailPage />
+				<DeleteButtonSkeleton isDetailPage />
 				<ButtonSkeleton width={180} />
 			</BackHeaderSkeleton>
 			<SpecializationCardSkeleton />
