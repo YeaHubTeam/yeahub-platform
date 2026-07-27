4c6a10702caf0f232ec46b3db936fd0bfae24bb7
MJ-learning <mrkrepton@mail.ru>
2026-07-27 23:12:36 +0500

    YH-2226: added emails domen validation for password recovery page

--- src/widgets/authentication/forgotPassword/lib/validation/forgotPasswordSchema.ts
+++ src/widgets/authentication/forgotPassword/lib/validation/forgotPasswordSchema.ts
@@ -6,5 +6,12 @@ export const forgotPasswordSchema = yup.object().shape({
 	username: yup
 		.string()
 		.email(i18n.t(Translation.VALIDATION_EMAIL))
+		.test('has-domain-zone', i18n.t(Translation.VALIDATION_EMAIL), (value) => {
+			if (!value) return true;
+			const parts = value.split('@');
+			if (parts.length !== 2) return false;
+			const domain = parts[1];
+			return domain.includes('.') && !domain.endsWith('.');
+		})
 		.required(i18n.t(Translation.VALIDATION_REQUIRED)),
 });
