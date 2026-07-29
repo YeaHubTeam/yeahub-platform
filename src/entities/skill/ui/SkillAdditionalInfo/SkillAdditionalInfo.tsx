8b27f0842ac4dbc57a186f7714c0b080c018ba65
Alevtina <wimoon@yandex.ru>
2026-07-29 23:13:47 +0400

    YH-2178: add skill additional info widget (entities)

---
+++ src/entities/skill/ui/SkillAdditionalInfo/SkillAdditionalInfo.tsx
@@ -0,0 +1,91 @@
+import classnames from 'classnames';
+import { useTranslation } from 'react-i18next';
+import { useNavigate } from 'react-router-dom';
+
+import { i18Namespace, Questions } from '@/shared/config';
+import { useCurrentProject, useScreenSize, formatDate, route } from '@/shared/libs';
+import { Author, AuthorInfo } from '@/shared/ui/AuthorInfo';
+import { BaseFilterSection } from '@/shared/ui/BaseFilterSection';
+import { Card } from '@/shared/ui/Card';
+import { Flex } from '@/shared/ui/Flex';
+import { Text } from '@/shared/ui/Text';
+
+// eslint-disable-next-line @conarti/feature-sliced/layers-slices
+import { Specialization } from '@/entities/specialization/@x/skill';
+
+import styles from './SkillAdditionalInfo.module.css';
+
+export interface SkillAdditionalInfoProps {
+	skillSpecializations: Specialization[];
+	updatedAt: string | null;
+	createdAt: string;
+	route: string;
+	createdBy: Author;
+	showAuthor?: boolean;
+	className?: string;
+}
+
+export const SkillAdditionalInfo = ({
+	skillSpecializations,
+	updatedAt,
+	createdAt,
+	route: baseRoute,
+	createdBy,
+	showAuthor = true,
+	className,
+}: SkillAdditionalInfoProps) => {
+	const navigate = useNavigate();
+	const { isMobile, isTablet } = useScreenSize();
+
+	const { t } = useTranslation(i18Namespace.questions);
+	const onMoveToSpecializationPage = (specializationId: number) => {
+		navigate(route(baseRoute, specializationId));
+	};
+
+	const project = useCurrentProject();
+
+	return (
+		<Card className={classnames(styles['normal-height'], className)}>
+			<Flex direction="column" gap="24">
+				{project === 'admin' && (
+					<BaseFilterSection
+						title={t(Questions.SPECIALIZATION_TITLE)}
+						data={skillSpecializations}
+						onClick={onMoveToSpecializationPage}
+					/>
+				)}
+				{updatedAt && (
+					<BaseFilterSection
+						title="Дата изменения:"
+						data={[
+							{
+								id: 'createdAt',
+								title: formatDate(new Date(updatedAt), 'dd.MM.yyyy'),
+							},
+						]}
+						isAllActive
+					/>
+				)}
+				<BaseFilterSection
+					title="Дата создания:"
+					data={[
+						{
+							id: 'createdAt',
+							title: formatDate(new Date(createdAt), 'dd.MM.yyyy'),
+						},
+					]}
+					isAllActive
+				/>
+
+				<Flex direction="column" gap="8">
+					<Text variant="body2" color="black-700">
+						Автор:{' '}
+						{showAuthor && createdBy && (isMobile || isTablet) && (
+							<AuthorInfo createdBy={createdBy || 'Не указан'} />
+						)}
+					</Text>
+				</Flex>
+			</Flex>
+		</Card>
+	);
+};
--- src/entities/skill/ui/SkillCard/SkillCard.tsx
+++ src/entities/skill/ui/SkillCard/SkillCard.tsx
@@ -1,11 +1,13 @@
 import { useTranslation } from 'react-i18next';
 
-import { Translation } from '@/shared/config';
+import { ROUTES, Translation } from '@/shared/config';
+import { useScreenSize } from '@/shared/libs';
 import { Card } from '@/shared/ui/Card';
 import { Flex } from '@/shared/ui/Flex';
 import { ImageWithWrapper } from '@/shared/ui/ImageWithWrapper';
 
 import { Skill } from '../../model/types/skill';
+import { SkillAdditionalInfo } from '../SkillAdditionalInfo/SkillAdditionalInfo';
 
 import styles from './SkillCard.module.css';
 
@@ -15,28 +17,44 @@ interface SkillCardProps {
 
 export const SkillCard = ({ skill }: SkillCardProps) => {
 	const { t } = useTranslation();
+	const { isMobile, isTablet } = useScreenSize();
+
+	const { createdAt, updatedAt, specializations, createdBy } = skill;
 
 	return (
-		<Flex direction="column" gap="24" className={styles.wrap}>
-			<Card withOutsideShadow>
-				<Flex gap="16">
-					<div className={styles['card-image-wrapper']}>
-						<ImageWithWrapper
-							src={skill.imageSrc}
-							alt={`${t(Translation.LOGO)} ${skill.title}`}
-							className={styles['card-image']}
-						/>
-					</div>
-					<h2>{skill.title}</h2>
-				</Flex>
-			</Card>
+		<Flex gap="20">
+			<Flex direction="column" gap="20" flex={1}>
+				<Card withOutsideShadow>
+					<Flex gap="16" direction="row">
+						<div className={styles['card-image-wrapper']}>
+							<ImageWithWrapper
+								src={skill.imageSrc}
+								alt={`${t(Translation.LOGO)} ${skill.title}`}
+								className={styles['card-image']}
+							/>
+						</div>
+						<h2>{skill.title}</h2>
+					</Flex>
+				</Card>
 
-			<Card withOutsideShadow expandable>
-				<Flex direction="column" gap="20">
-					<h3>{skill.title}</h3>
-					<p>{skill.description}</p>
+				<Card withOutsideShadow expandable>
+					<Flex direction="column" gap="20">
+						<h3>{skill.title}</h3>
+						<p>{skill.description}</p>
+					</Flex>
+				</Card>
+			</Flex>
+			{!isMobile && !isTablet && (
+				<Flex direction="column" gap="20" className={styles.additional}>
+					<SkillAdditionalInfo
+						skillSpecializations={specializations}
+						updatedAt={updatedAt}
+						createdAt={createdAt}
+						createdBy={createdBy}
+						route={ROUTES.admin.specializations.details.page}
+					/>
 				</Flex>
-			</Card>
+			)}
 		</Flex>
 	);
 };
