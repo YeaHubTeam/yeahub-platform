75988316c4bad969499a92b509e268c9c7e60e21
vely1012 <117396699+vely1012@users.noreply.github.com>
2026-08-02 19:17:59 +0300

    YH-2197: Add appropriate skeleton for SpecializationDetailPage (#1324)
    * YH-2197: improve skeletons for SpecializationDetailPage
    
    - Adjust SpecializationDetailPage skeleton to occupy same space as real content
    - Refine skeleton structure to better represent actual layout and content
    
    * YH-2197: improve skeletons for SpecializationDetailPage
    
    - Replace mistakenly left DeleteSpecializationButtonSkeleton with
    regular DeleteButtonSkeleton
    
    * YH-2197: Add skeleton for TopicDetailPage
    
    - Implement TopicDetailPageSkeleton component as requested per updated task requirements
    - Created skeletons for TopicCard and TopicAdditionalInfo
    
    * YH-2197: fix
    
    * YH-2197: fix
    
    * YH-2197: fix
    
    ---------
    
    Co-authored-by: Denis Pereloma <den.pereloma@gmail.com>

--- src/pages/admin/topic/topicDetail/ui/TopicDetailPage/TopicDetailPage.skeleton.tsx
+++ src/pages/admin/topic/topicDetail/ui/TopicDetailPage/TopicDetailPage.skeleton.tsx
@@ -3,17 +3,21 @@ import React from 'react';
 import { BackHeaderSkeleton } from '@/shared/ui/BackHeader';
 import { ButtonSkeleton } from '@/shared/ui/Button';
 import { DeleteButtonSkeleton } from '@/shared/ui/DeleteButton';
+import { Flex } from '@/shared/ui/Flex';
 
-import { SpecializationCardSkeleton } from '@/entities/specialization';
+import { TopicAdditionalInfoSkeleton, TopicCardSkeleton } from '@/entities/topic';
 
-export const SpecializationDetailPageSkeleton = () => {
+export const TopicDetailPageSkeleton = () => {
 	return (
 		<>
 			<BackHeaderSkeleton>
 				<DeleteButtonSkeleton isDetailPage />
 				<ButtonSkeleton width={180} />
 			</BackHeaderSkeleton>
-			<SpecializationCardSkeleton />
+			<Flex gap="24">
+				<TopicCardSkeleton />
+				<TopicAdditionalInfoSkeleton />
+			</Flex>
 		</>
 	);
 };
