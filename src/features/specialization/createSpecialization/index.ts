5517ff0379b22ac4f48137827645a6308d0d4aa6
Milo78k <132217962+Milo78k@users.noreply.github.com>
2026-08-02 16:25:24 +0300

    YH-2266: add specialization creation skeleton (#1351)

--- src/features/specialization/createSpecialization/index.ts
+++ src/features/specialization/createSpecialization/index.ts
@@ -1,4 +1,5 @@
 export { useCreateSpecializationMutation } from './api/createSpecializationApi';
 export { SpecializationCreateForm } from './ui/SpecializationCreateForm/SpecializationCreateForm';
+export { SpecializationCreateFormSkeleton } from './ui/SpecializationCreateForm/SpecializationCreateForm.skeleton';
 export { specializationCreateHandlers } from './api/__mock__';
 export { getCreateSpecializationErrorMessage } from './model/utils/getApiErrorMessage';
