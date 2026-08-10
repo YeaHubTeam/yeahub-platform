c0e95b5b0de0ef42c30b009ce9e9b909d559f281
OlgaBul <olgushka-ya@yandex.ru>
2026-08-10 13:19:05 +0300

    YH-2255: updated skeleton for questions page

--- src/shared/ui/Accordion/Accordion.skeleton.tsx
+++ src/shared/ui/Accordion/Accordion.skeleton.tsx
@@ -5,16 +5,20 @@ import styles from '@/shared/ui/Accordion/Accordion.module.css';
 import { IconSkeleton } from '@/shared/ui/Icon';
 import { TextSkeleton } from '@/shared/ui/Text';
 
-export const AccordionSkeleton = () => {
+interface AccordionSkeletonProps {
+	className?: string;
+}
+
+export const AccordionSkeleton = ({ className }: AccordionSkeletonProps) => {
 	const { isMobileS } = useScreenSize();
 
 	return (
-		<div className={styles.accordion}>
+		<div className={classNames(styles.accordion, className)}>
 			<div className={styles.heading}>
 				<button className={styles.button}>
 					<TextSkeleton
 						width="70%"
-						variant={isMobileS ? 'body2-accent' : 'body5-accent'}
+						variant={isMobileS ? 'body3-accent' : 'body5-accent'}
 						className={classNames(styles.title, styles.skeleton)}
 					/>
 					<IconSkeleton size={24} className={styles.icon} />
