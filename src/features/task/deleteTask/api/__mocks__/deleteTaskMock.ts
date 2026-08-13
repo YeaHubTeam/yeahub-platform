8ce9a86db91b5a83b5c751f8770ae3752d092f25
Visage <some@email.com>
2026-08-14 02:09:52 +0300

    YH-2310: address review comments (typing, status code, error format)

--- src/features/task/deleteTask/api/__mocks__/deleteTaskMock.ts
+++ src/features/task/deleteTask/api/__mocks__/deleteTaskMock.ts
@@ -1,20 +1,28 @@
-import { http, HttpResponse } from 'msw';
+import { http, HttpResponse, DefaultBodyType } from 'msw';
 
 import { tasksMock } from '@/entities/task';
 
 import { deleteTaskApiUrls } from '../../model/constants/deleteTaskConstants';
 
-export const deleteTaskMock = http.delete<{ taskId: string }>(
-	`${process.env.API_URL}${deleteTaskApiUrls.deleteTask}`,
-	({ params }) => {
-		const { taskId } = params;
-		const foundIndexTask = tasksMock.findIndex((task) => task.id === taskId);
+export const deleteTaskMock = http.delete<
+	{ taskId: string },
+	DefaultBodyType,
+	ApiErrorData<string>
+>(`${process.env.API_URL}${deleteTaskApiUrls.deleteTask}`, ({ params }) => {
+	const { taskId } = params;
+	const foundIndexTask = tasksMock.findIndex((task) => task.id === taskId);
 
-		if (foundIndexTask === -1) {
-			return HttpResponse.json({ message: 'Task is not found' }, { status: 404 });
-		}
+	if (foundIndexTask === -1) {
+		return HttpResponse.json(
+			{
+				message: 'task.task.not_found',
+				statusCode: 404,
+				description: 'Task not found',
+			},
+			{ status: 404 },
+		);
+	}
 
-		tasksMock.splice(foundIndexTask, 1);
-		return new HttpResponse(null, { status: 200 });
-	},
-);
+	tasksMock.splice(foundIndexTask, 1);
+	return new HttpResponse(null, { status: 204 });
+});
