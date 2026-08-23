e330eba5f3ca7629740f8cf71a75d8f2611bba65
Alevtina <wimoon@yandex.ru>
2026-08-23 12:29:25 +0400

    YH-1769: fix error stub to resource request edit page

--- src/pages/admin/resource/resourceRequestEdit/ui/ResourceRequestEditPage/ResourceRequestEditPage.tsx
+++ src/pages/admin/resource/resourceRequestEdit/ui/ResourceRequestEditPage/ResourceRequestEditPage.tsx
@@ -4,13 +4,36 @@ import { useGetResourceRequestByIdQuery } from '@/entities/resource';
 
 import { ResourceRequestEditForm } from '@/features/resources/editResourceRequest';
 
+import { PageWrapper, PageWrapperStubs } from '@/widgets/PageWrapper';
+
 const ResourceRequestEditPage = () => {
 	const { resourceId } = useParams<{ resourceId: string }>();
 
-	const { data: myResourceRequest } = useGetResourceRequestByIdQuery(resourceId || '');
+	const {
+		data: myResourceRequest,
+		isLoading,
+		isFetching,
+		isError,
+		refetch,
+	} = useGetResourceRequestByIdQuery(resourceId || '');
 
-	if (!myResourceRequest) return null;
-	return <ResourceRequestEditForm request={myResourceRequest} />;
+	const stubs: PageWrapperStubs = {
+		error: {
+			onClick: refetch,
+		},
+	};
+	return (
+		<PageWrapper
+			isLoading={isLoading || isFetching}
+			hasError={isError}
+			hasData={Boolean(myResourceRequest)}
+			stubs={stubs}
+			roles={['admin', 'author']}
+			content={myResourceRequest ? <ResourceRequestEditForm request={myResourceRequest} /> : null}
+		>
+			{({ content: resolvedContent }) => <>{resolvedContent}</>}
+		</PageWrapper>
+	);
 };
 
 export default ResourceRequestEditPage;
