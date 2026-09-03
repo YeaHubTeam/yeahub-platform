6c12bd537961a0f9b978f872d13f43e3e80cb0c7
Magomed Kuriev <96702005+oneOfThePeakyBlinders@users.noreply.github.com>
2026-09-03 22:26:12 +0300

    YH-2367: added error handling for topic-edit using toast and multilanguage support using i18n (#1397)
    Co-authored-by: Magomed The Great <magomedthegreat@MacBook-Pro-Magomed.local>
    Co-authored-by: PerelomaDenis <88330372+PerelomaDenis@users.noreply.github.com>

--- src/features/topics/editTopic/model/types/topicEditErrorTypes.ts
+++ src/features/topics/editTopic/model/types/topicEditErrorTypes.ts
@@ -3,4 +3,6 @@ export type TopicEditError =
 	| 'auth.user.verified'
 	| 'auth.roles.author_can_change_only_own'
 	| 'topic.topic.title.conflict'
-	| 'topic.topic.not_found';
+	| 'topic.topic.not_found'
+	| 'tinify.tinify.compress_failed'
+	| 'tinify.tinify.resize_failed';
