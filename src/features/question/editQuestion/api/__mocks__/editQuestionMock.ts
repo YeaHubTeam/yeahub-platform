1f69bc5b9b897a448d9eccc796f36a4e33294776
Magomed The Great <magomedthegreat@MacBook-Pro-Magomed.local>
2026-09-07 02:59:30 +0300

    YH-2369: added error-handling for edit mock question

--- src/features/question/editQuestion/api/__mocks__/editQuestionMock.ts
+++ src/features/question/editQuestion/api/__mocks__/editQuestionMock.ts
@@ -1,5 +1,6 @@
 import { http, HttpResponse, PathParams } from 'msw';
 
+import { getMockAuthProfile } from '@/entities/auth';
 import { questionsMock } from '@/entities/question';
 
 import { editQuestionApiUrls } from '../../model/constants/editQuestionConstants';
@@ -11,23 +12,85 @@ import {
 export const editQuestionMock = http.patch<
 	PathParams,
 	EditQuestionBodyRequest,
-	EditQuestionResponse | { error: string }
+	EditQuestionResponse | ApiErrorData<string>
 >(process.env.API_URL + editQuestionApiUrls.editQuestion, async ({ request }) => {
 	const formData = await request.json();
 
-	const questionId = questionsMock.data.findIndex((question) => question.id === formData.id);
+	const questionIndex = questionsMock.data.findIndex((question) => question.id === formData.id);
 
-	if (questionId !== -1) {
-		const updateQuestion = {
-			...questionsMock.data[questionId],
-			...formData,
-			updatedAt: new Date().toISOString(),
-		};
+	const profileMockResponse = getMockAuthProfile(request);
 
-		questionsMock.data[questionId] = updateQuestion;
+	const isAdmin = profileMockResponse?.userRoles.some((role) => role.name === 'admin') ?? false;
+	const isAuthor = profileMockResponse?.userRoles.some((role) => role.name === 'author') ?? false;
+	const isAuthorOfThisQuestion =
+		questionsMock.data[questionIndex]?.createdBy?.id === profileMockResponse?.id;
 
-		return HttpResponse.json(updateQuestion);
+	if (!profileMockResponse) {
+		return HttpResponse.json(
+			{
+				message: 'auth.auth.unauthorized',
+				statusCode: 401,
+				description: 'Authentication failed',
+			},
+			{ status: 401 },
+		);
 	}
 
-	return HttpResponse.json({ error: 'Question not found' }, { status: 404 });
+	if (!profileMockResponse.isVerified) {
+		return HttpResponse.json(
+			{
+				message: 'auth.user.verified',
+				statusCode: 403,
+				description: 'Route is available for verified users!',
+			},
+			{ status: 403 },
+		);
+	}
+
+	if (!isAdmin && !(isAuthor && isAuthorOfThisQuestion)) {
+		return HttpResponse.json(
+			{
+				message: 'auth.roles.admin_or_author_required',
+				statusCode: 403,
+				description: 'Admin or author required',
+			},
+			{ status: 403 },
+		);
+	}
+
+	if (questionIndex === -1) {
+		return HttpResponse.json(
+			{
+				message: 'question.question.not_found',
+				statusCode: 404,
+				description: 'Question not found',
+			},
+			{ status: 404 },
+		);
+	}
+
+	const isTitleConflict = questionsMock.data.some(
+		(question) => question.title === formData.title && question.id !== formData.id,
+	);
+
+	if (isTitleConflict) {
+		return HttpResponse.json(
+			{
+				message: 'question.question.title.conflict',
+				statusCode: 409,
+				description: 'Question with same title already exists',
+			},
+			{ status: 409 },
+		);
+	}
+
+	const updateQuestion = {
+		...questionsMock.data[questionIndex],
+		...formData,
+		updatedAt: new Date().toISOString(),
+	};
+
+	questionsMock.data[questionIndex] = updateQuestion;
+
+	return HttpResponse.json(updateQuestion);
 });
--- src/features/question/editQuestion/model/types/questionEditPageTypes.ts
+++ src/features/question/editQuestion/model/types/questionEditPageTypes.ts
@@ -8,6 +8,8 @@ export type EditQuestionResponse = Question;
 export type EditQuestionError =
 	| 'auth.auth.unauthorized'
 	| 'auth.user.verified'
+	| 'auth.roles.admin_or_author_required'
 	| 'auth.roles.author_can_change_only_own'
+	| 'question.question.title.conflict'
 	| 'question.question.not_found'
 	| 'question.question.internal';
