df4922939ff5c3f9b5814245b448cda0dc897e8c
belovroman90 <belov.roman.90@mail.ru>
2026-09-08 10:52:05 +0700

    YH-2339: fix review comments (part 1)

--- src/pages/career/resumeAnalyzer/api/resumeAnalyzeApi.ts
+++ src/pages/career/resumeAnalyzer/api/resumeAnalyzeApi.ts
@@ -1,12 +1,22 @@
-import { ApiTags, baseApi } from '@/shared/config';
-import { route } from '@/shared/libs';
+import toast from 'react-hot-toast';
 
-import { resumeAnalyzeApiUrls } from '../model/constants/resumeAnalyzeConstants';
-import { ResumeAnalyzeBodyRequest, ResumeAnalyzeResponse } from '../model/types/resumeAnalyze';
+import { ApiTags, baseApi, i18n } from '@/shared/config';
+import { route, setToLS } from '@/shared/libs';
+import { LS_RESUME_ANALYSIS_KEY } from '@/shared/libs';
+
+import { resumeAnalyzeApiUrls } from '../model/constants/resumeAnalyzerConstants';
+import { resumeAnalyzerPageActions } from '../model/slices/resumeAnalyzerPageSlice';
+import {
+	ResumeAnalyzeByPortraitBodyRequest,
+	ResumeAnalyzeByPortraitResponse,
+} from '../model/types/resumeAnalyzer';
 
 export const resumeAnalyzeApi = baseApi.injectEndpoints({
 	endpoints: (build) => ({
-		resumeAnalyze: build.mutation<ResumeAnalyzeResponse, ResumeAnalyzeBodyRequest>({
+		resumeAnalyzeByPortrait: build.mutation<
+			ResumeAnalyzeByPortraitResponse,
+			ResumeAnalyzeByPortraitBodyRequest
+		>({
 			query: ({ specializationId, file }) => {
 				return {
 					url: route(resumeAnalyzeApiUrls.resumeAnalyzeByPortrait, specializationId),
@@ -14,9 +24,24 @@ export const resumeAnalyzeApi = baseApi.injectEndpoints({
 					body: file,
 				};
 			},
+
+			async onQueryStarted(_, { queryFulfilled, dispatch }) {
+				try {
+					const { data } = await queryFulfilled;
+					dispatch(resumeAnalyzerPageActions.setResumeAnalysis(data));
+
+					setToLS(LS_RESUME_ANALYSIS_KEY, {
+						response: data,
+						analyzedAt: new Date().toISOString(),
+					});
+				} catch {
+					toast.error(i18n.t('file.uploaded_resume.analyses.failed'));
+				}
+			},
+
 			invalidatesTags: [ApiTags.RESUME_ATS],
 		}),
 	}),
 });
 
-export const { useResumeAnalyzeMutation } = resumeAnalyzeApi;
+export const { useResumeAnalyzeByPortraitMutation } = resumeAnalyzeApi;
--- src/pages/career/resumeAnalyzer/ui/UploadedResume/UploadedResume.tsx
+++ src/pages/career/resumeAnalyzer/ui/UploadedResume/UploadedResume.tsx
@@ -1,6 +1,8 @@
 import { useTranslation } from 'react-i18next';
+import { useSelector } from 'react-redux';
 
-import { i18Namespace, Translation } from '@/shared/config';
+import { i18Namespace, Resume } from '@/shared/config';
+import { useAppDispatch } from '@/shared/libs';
 import { Button } from '@/shared/ui/Button';
 import { Card } from '@/shared/ui/Card';
 import { Flex } from '@/shared/ui/Flex';
@@ -8,25 +10,45 @@ import { Icon } from '@/shared/ui/Icon';
 import { IconButton } from '@/shared/ui/IconButton';
 import { Text } from '@/shared/ui/Text';
 
+import { getSpecializationId } from '@/entities/profile';
+
+import { useResumeAnalyzeByPortraitMutation } from '../../api/resumeAnalyzeApi';
+import { useGetUploadedFileDate } from '../../model/hooks/useGetUploadedFileDate';
+import {
+	getResumeFileName,
+	getResumeUploadedAt,
+} from '../../model/selectors/resumeAnalyzerPageSelectors';
+import { resumeAnalyzerPageActions } from '../../model/slices/resumeAnalyzerPageSlice';
+
 import { UploadedFileIcon } from './UploadedFileIcon/UploadedFileIcon';
 import styles from './UploadedResume.module.css';
 
 export interface UploadedResumeProps {
-	fileName: string;
-	isLoading: boolean;
-	onUploadResume: () => void;
-	onDeleteResume: () => void;
-	uploadedAt?: string | null;
+	file: FormData;
+	setFile: (file: FormData | null) => void;
 }
 
-export const UploadedResume = ({
-	fileName,
-	isLoading,
-	onUploadResume,
-	onDeleteResume,
-	uploadedAt,
-}: UploadedResumeProps) => {
-	const { t } = useTranslation(i18Namespace.translation);
+export const UploadedResume = ({ file, setFile }: UploadedResumeProps) => {
+	const dispatch = useAppDispatch();
+	const fileName = useSelector(getResumeFileName);
+	const uploadedAt = useSelector(getResumeUploadedAt);
+	const formattedDate = useGetUploadedFileDate(uploadedAt);
+	const { t } = useTranslation(i18Namespace.resume);
+	const specializationId = useSelector(getSpecializationId);
+	const [uploadResume, { isLoading }] = useResumeAnalyzeByPortraitMutation({
+		fixedCacheKey: 'resume-analysis',
+	});
+
+	const onUploadResume = async () => {
+		if (file) {
+			await uploadResume({ specializationId, file }).unwrap();
+		}
+	};
+	const onResetAnalyzerState = () => {
+		setFile(null);
+		dispatch(resumeAnalyzerPageActions.resetResumeAnalyzerState());
+	};
+
 	return (
 		<Flex direction="column" gap="20" className={styles['uploaded-file-wrapper']}>
 			<Card withOutsideShadow>
@@ -34,12 +56,12 @@ export const UploadedResume = ({
 					<Flex gap="12">
 						<UploadedFileIcon />
 						<Flex gap="8" direction="column" align="start">
-							<Text variant="body2-accent">{t(Translation.FILE_UPLOADED_RESUME_TITLE)}</Text>
-							<Text variant="body3-strong-compact" className={styles['file-name']}>
+							<Text variant="body2-accent">{t(Resume.FILE_UPLOADED_RESUME_TITLE)}</Text>
+							<Text variant="body3-strong" className={styles['file-name']}>
 								{fileName}
 							</Text>
 							<Text variant="body3-accent" color="black-500">
-								{t(Translation.FILE_UPLOADED_RESUME_UPLOADED_AT, { uploadedAt })}
+								{t(Resume.FILE_UPLOADED_RESUME_UPLOADED_AT, { uploadedAt: formattedDate })}
 							</Text>
 						</Flex>
 					</Flex>
@@ -48,7 +70,7 @@ export const UploadedResume = ({
 						variant="tertiary"
 						size="small"
 						className={styles['delete-button']}
-						onClick={onDeleteResume}
+						onClick={onResetAnalyzerState}
 					/>
 				</Flex>
 			</Card>
@@ -59,7 +81,7 @@ export const UploadedResume = ({
 				onClick={onUploadResume}
 				className={styles['upload-button']}
 			>
-				Проверить резюме
+				{t(Resume.FILE_UPLOADED_RESUME_CHECK)}
 			</Button>
 		</Flex>
 	);
