ccc5f1d2b1719f9375ee31b548acfbd038de050e
Denis Pereloma <den.pereloma@gmail.com>
2026-09-10 23:11:54 +0300

    YH-2281: fix skeleton

--- src/pages/interview/createQuiz/ui/CreateQuizPage/CreateQuizPage.skeleton.tsx
+++ src/pages/interview/createQuiz/ui/CreateQuizPage/CreateQuizPage.skeleton.tsx
@@ -1,57 +1,19 @@
-import { Card } from '@/shared/ui/Card';
-import { Flex } from '@/shared/ui/Flex';
-import { Skeleton } from '@/shared/ui/Skeleton';
+import { ButtonSkeleton } from '@/shared/ui/Button';
+import { CardSkeleton } from '@/shared/ui/Card';
+import { TextSkeleton } from '@/shared/ui/Text';
+
+import { CreateQuizFiltersSkeleton } from '@/widgets/interview/CreateQuizFilters';
 
 import styles from './CreateQuizPage.module.css';
 
 export const CreateQuizPageSkeleton = () => {
 	return (
 		<section>
-			<Card className={styles.container}>
-				<Skeleton width={200} height={33} className={styles.title} />
-				<Flex
-					direction="row"
-					justify="between"
-					style={{ width: '100%' }}
-					className={styles.wrapper}
-					gap="40"
-				>
-					<Flex direction="column" gap="8" style={{ flex: '1 1 60%' }}>
-						<Skeleton width={137} height={19} />
-						<Flex gap="8" style={{ flexWrap: 'wrap' }}>
-							{[...Array(5)].map((_, j) => (
-								<Skeleton key={j} width={120} height={40} style={{ flex: '0 1 130px' }} />
-							))}
-						</Flex>
-						<Skeleton width={110} height={20} />
-					</Flex>
-					<Flex direction="column" gap="24" style={{ flex: '1 1 40%' }}>
-						<Flex gap="8" direction="column">
-							<Skeleton width={137} height={19} />
-							<Flex gap="8" style={{ flexWrap: 'wrap' }}>
-								{[...Array(4)].map((_, j) => (
-									<Skeleton key={j} width={60} height={40} />
-								))}
-							</Flex>
-						</Flex>
-						<Flex gap="8" direction="column">
-							<Skeleton width={120} height={19} />
-							<Flex gap="8" style={{ flexWrap: 'wrap' }}>
-								{[...Array(3)].map((_, j) => (
-									<Skeleton key={j} width={120} height={40} />
-								))}
-							</Flex>
-						</Flex>
-						<Flex gap="8" direction="column">
-							<Skeleton width={145} height={19} />
-							<Flex gap="8">
-								<Skeleton width={115} height={40} />
-							</Flex>
-						</Flex>
-					</Flex>
-				</Flex>
-				<Skeleton width={152} height={40} borderRadius={12} className={styles.button} />
-			</Card>
+			<CardSkeleton className={styles.container}>
+				<TextSkeleton variant="body6" className={styles.title} width={200} />
+				<CreateQuizFiltersSkeleton />
+				<ButtonSkeleton width={150} className={styles.button} />
+			</CardSkeleton>
 		</section>
 	);
 };
