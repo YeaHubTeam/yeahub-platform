64adde3e8091862b931313d12e14166d7e5a1272
ShavelMaxim <54544092+ShavelMaxim@users.noreply.github.com>
2026-09-11 15:14:40 +0800

    YH-2391: fix comments

--- src/shared/ui/TableV2/TableBody.tsx
+++ src/shared/ui/TableV2/TableBody.tsx
@@ -73,7 +73,7 @@ export const TableBody = <T,>({
 									<TableActions
 										entity={entity}
 										actions={actions}
-										onDelete={() => onDelete?.(rowId)}
+										onDelete={onDelete ? () => onDelete(rowId) : undefined}
 										id={rowId}
 										disabled={disabled}
 										disabledTooltipTitle={disabledActionsTooltipTitle}
