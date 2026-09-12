a8c073c9abca3a1dae310ccded34561c8750de19
AleksandrovIgor1 <alexigore@yandex.ru>
2026-09-13 00:42:09 +0500

    YH-2387: replaced specialization table with TableV2 (#1425)
    * YH-2387: replaced specialization table with TableV2
    
    * YH-2387: formatted table data for default cell rendering
    
    ---------
    
    Co-authored-by: PerelomaDenis <88330372+PerelomaDenis@users.noreply.github.com>

--- src/features/specialization/deleteSpecialization/index.ts
+++ src/features/specialization/deleteSpecialization/index.ts
@@ -1,4 +1,5 @@
 export { DeleteSpecializationButton } from './ui/DeleteSpecializationButton/DeleteSpecializationButton';
 export { DeleteSpecializationButtonSkeleton } from './ui/DeleteSpecializationButton/DeleteSpecializationButton.skeleton';
+export { useDeleteSpecializationMutation } from './api/deleteSpecializationApi';
 
 export { specializationDeleteHandlers } from './api/__mocks__';
