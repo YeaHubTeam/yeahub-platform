e349b6c4bde9916e28df308b1e0b920d7b262d46
AleksandrovIgor1 <alexigore@yandex.ru>
2026-09-13 14:25:38 +0500

    YH-2412: fix mock to return unique user data per userId

--- src/entities/user/__mocks__/userByIdMock.ts
+++ src/entities/user/__mocks__/userByIdMock.ts
@@ -3,31 +3,14 @@ import { DefaultBodyType, http, HttpResponse } from 'msw';
 import { userApiUrls } from '../model/constants/userConstants';
 import { GetUserByIdResponse } from '../model/types/user';
 
+import { usersDataMock } from './data/usersDataMock';
+
 export const userByIdMock = http.get<Record<string, string>, DefaultBodyType, GetUserByIdResponse>(
 	`${process.env.API_URL}${userApiUrls.getUserById}`,
 	({ params }) => {
 		const { userId } = params;
+		const user = usersDataMock.data.find((u) => String(u.id) === userId);
 
-		return HttpResponse.json({
-			id: userId,
-			username: 'Mock user',
-			email: 'Mock@yeahub.ru',
-			country: 'Russia',
-			city: 'Moscow',
-			address: 'Zemlyanoy Val Street, 72',
-			avatarUrl: '',
-			birthday: '1995-06-15',
-			updatedAt: new Date().toISOString(),
-			createdAt: '2023-01-10T08:00:00.000Z',
-			userRoles: [
-				{
-					id: 1,
-					name: 'guest',
-					permissions: [],
-				},
-			],
-			telegramUsername: 'mock_user_tg',
-			isVerified: false,
-		});
+		return HttpResponse.json(user);
 	},
 );
