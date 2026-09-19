d887e61b900214d6ac2536e763d46e935a765fdc
Pavel Prigoda <rotares@disroot.org>
2026-09-19 17:12:29 +0300

    YH-2287: modified structure of the faqitem skeleton component

--- src/widgets/FaqList/ui/FaqItem/FaqItem.skeleton.tsx
+++ src/widgets/FaqList/ui/FaqItem/FaqItem.skeleton.tsx
@@ -1,20 +1,14 @@
 import classNames from 'classnames';
 
-import { useScreenSize } from '@/shared/libs';
-
-import { ButtonSkeleton } from '../../../../shared/ui/Button';
-import { TextSkeleton } from '../../../../shared/ui/Text';
+import { ButtonSkeleton } from '@/shared/ui/Button';
+import { TextSkeleton } from '@/shared/ui/Text';
 
 import styles from './FaqItem.module.css';
 
 export const FaqItemSkeleton = () => {
-	const { isMobile } = useScreenSize();
 	return (
 		<li className={styles['faq-item']}>
-			<ButtonSkeleton className={styles['faq-header']}>
-				<TextSkeleton width={150} variant={isMobile ? 'body3-strong' : 'body5-accent'} />
-				<div className={classNames(styles['faq-show'])}></div>
-			</ButtonSkeleton>
+			<ButtonSkeleton className={styles['faq-header']} />
 			<div className={classNames(styles['faq-collapse'])}>
 				<div className={styles['faq-body']}>
 					<TextSkeleton variant="body3" width={150} />
