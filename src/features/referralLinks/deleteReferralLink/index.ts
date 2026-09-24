e7b78810d47c40c2092b314568b198341bacc4a9
Лёша <134238215+AlexKhabibov@users.noreply.github.com>
2026-09-24 14:54:12 +0300

    YH-2397: refactor ReferralLinksTable(Table->TableV2)

--- src/features/referralLinks/deleteReferralLink/index.ts
+++ src/features/referralLinks/deleteReferralLink/index.ts
@@ -1,2 +1,3 @@
 export { DeleteReferralLinkButton } from './ui/DeleteReferralLinkButton/DeleteReferralLinkButton';
 export { referralLinkDeleteHandlers } from './api/__mocks__/index';
+export { useDeleteReferralLinkMutation } from './api/deleteReferralLinkApi';
